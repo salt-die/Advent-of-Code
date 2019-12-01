c497000f178f6dea8f1db1ed9ed82695fee04478
salt-die <naclmolecule@gmail.com>
2019-11-30 23:23:33 -0600

    optimize slightly

--- day_01.py
+++ day_01.py
@@ -1,13 +1,13 @@
 with open('day_01_input.txt', 'r') as numbers:
-    numbers = [int(line) for line in numbers.readlines()]
+    numbers = list(map(int, numbers.readlines()))
 
-print(sum([number//3 - 2 for number in numbers]))
+print(sum(number//3 - 2 for number in numbers))
 
-grand_total = 0
+total = 0
 for number in numbers:
     fuel = number // 3 - 2
     while fuel > 0:
-        grand_total += fuel
+        total += fuel
         fuel = fuel // 3 - 2
 
-print(grand_total)
+print(total)
