95f1b3516304e5a8a5a9524de81ba45853677aa8
salt-die <naclmolecule@gmail.com>
2019-12-01 04:18:40 -0600

    reformatted

--- day_01.py
+++ day_01.py
@@ -15,6 +15,6 @@ print(total)
 #Alternate solution -- one-line non-recursive for-loop
 from math import log
 print(sum(sum(j for j in [number]
-                for i in range(int(log(number, 3))+1)
-                for j in [j//3 -2] if j > 0)
+                for i in range(int(log(number, 3)) + 1)
+                for j in [j // 3 - 2] if j > 0)
           for number in numbers))
