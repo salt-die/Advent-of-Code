23985ada67c68021548bdacfd4e7bf57ed3ce0b6
salt-die <naclmolecule@gmail.com>
2019-12-01 04:11:13 -0600

    one-liner added

--- day_01.py
+++ day_01.py
@@ -11,3 +11,10 @@ for number in numbers:
         fuel = fuel // 3 - 2
 
 print(total)
+
+#Alternate solution -- one-line non-recursive for-loop
+from math import log
+print(sum(sum(j for j in [number]
+                for i in range(int(log(number, 3))+1)
+                for j in [j//3 -2] if j > 0)
+          for number in numbers))
