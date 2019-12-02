352be36b82161673a1df02cf2e62b65dd2b71ba4
salt-die <naclmolecule@gmail.com>
2019-12-02 16:27:44 -0600

    keywords added to parameters

--- day_02.py
+++ day_02.py
@@ -5,11 +5,11 @@ with open('input', 'r') as data:
     data = list(map(int, data.read().split(',')))
 
 #Part1
-tape = Computer(data)
-print(tape.compute(12, 2))
+tape = Computer(int_code=data)
+print(tape.compute(noun=12, verb=2))
 
 #Part2
 for i, j in product(range(100), repeat=2):
-    if tape.compute(i, j) == 19690720: #Date of moon landing
+    if tape.compute(noun=i, verb=j) == 19690720: #Date of moon landing
         print(100 * i + j)
         break
