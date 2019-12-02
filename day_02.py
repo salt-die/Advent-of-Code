2ed0b35145c1b4ca15f9a0e7fc3cf505ee0bf2f6
salt-die <naclmolecule@gmail.com>
2019-12-01 23:37:00 -0600

    cleaned up code

--- day_02.py
+++ day_02.py
@@ -1,27 +1,34 @@
+from itertools import product
+
 with open('input', 'r') as data:
     data = list(map(int, data.read().split(',')))
 
-from itertools import product
-pristine_data = data.copy()
+def compute_data(memory):
+    head_position = 0
+    while True:
+        try:
+            op_code = memory[head_position]
+        except IndexError:
+            return -1
+
+        if op_code not in (1, 2, 99):
+            return -1
+
+        if op_code == 99:
+            return memory[0]
+
+        try:
+            address_1, address_2, output_address = data[head_position + 1:head_position + 4]
+        except IndexError:
+            return -1
+
+        value_1, value_2 = memory[address_1], memory[address_2]
+        memory[output_address] = value_1 + value_2 if op_code == 1 else value_1 * value_2
+        head_position += 4
 
 for i,j in product(range(100), repeat=2):
-    data = pristine_data.copy()
-    data[1:3] = [i, j]
-
-    c = 0
-    try:
-        while 1:
-            op = data[c]
-            if op == 99:
-                break
-            pos1, pos2, out = data[c + 1:c + 4]
-            val1, val2 = data[pos1], data[pos2]
-            data[out] = val1 + val2 if op == 1 else val1 * val2
-            c += 4
-
-    except IndexError:
-        continue
-
-    if data[0] == 19690720:
-        print(100*i + j)
+    new_data = data.copy()
+    new_data[1], new_data[2] = i, j
+    if compute_data(new_data) == 19690720:
+        print(100 * i + j)
         break
