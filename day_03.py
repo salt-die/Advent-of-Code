6002d33416c64c8ace34ba5df3598c665cd1a851
salt-die <naclmolecule@gmail.com>
2019-12-03 02:42:49 -0600

    optimize last lines

--- day_03.py
+++ day_03.py
@@ -3,15 +3,15 @@ import numpy as np
 with open('input', 'r') as data:
     data = data.readlines()
 
-directions = dict(zip('UDRL', map(np.array,([0, 1], [0, -1], [1, 0], [-1, 0]))))
+directions = dict(zip('UDRL', map(np.array, ([0, 1], [0, -1], [1, 0], [-1, 0]))))
 
 wires = [[(directions[d], int(''.join(num))) for d, *num in wire.split(',')] for wire in data]
 
 visits = []
 for wire in wires:
     visited = []
-    current_location = np.array([0, 0])
-    visited.append(tuple(current_location))
+    current_location = (0, 0)
+    visited.append(current_location)
     for direction, number in wire:
         visited.extend(tuple(current_location + i * direction) for i in range(1, number + 1))
         current_location += number * direction
@@ -21,9 +21,7 @@ intersections = set(visits[0]) & set(visits[1])
 intersections.remove((0, 0))
 
 #Part 1
-closest = min(intersections, key=lambda arr:sum(np.abs(arr)))
-print(sum(np.abs(closest)))
+print(min(sum(np.abs(location)) for location in intersections))
 
 #Part 2
-closest = min(intersections, key=lambda arr:sum(wire.index(arr) for wire in visits))
-print(sum(wire.index(closest) for wire in visits))
+print(min(sum(wire.index(location) for wire in visits) for location in intersections))
