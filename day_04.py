300b8986c40ea885763b838a3ce43742309fe9a3
salt-die <naclmolecule@gmail.com>
2019-12-03 23:51:00 -0600

    optimized

--- day_04.py
+++ day_04.py
@@ -1,11 +1,7 @@
 from itertools import groupby
 
 def has_adjacent(number):
-    number = str(number)
-    for first, second in zip(number, number[1:]):
-        if first == second:
-            return True
-    return False
+    return any(len(tuple(n)) > 1 for _, n in groupby(str(number)))
 
 def has_adjacent_pair(number):
     return any(len(tuple(n)) == 2 for _, n in groupby(str(number)))
@@ -20,11 +16,10 @@ def is_non_decreasing(number):
 start, end = 134564, 585159
 
 # Part 1
-passwords = [number for number in range(start, end + 1)
-             if has_adjacent(number) and is_non_decreasing(number)]
+nondecreasing = filter(is_non_decreasing, range(start, end + 1))
+passwords = tuple(filter(has_adjacent, nondecreasing))
 print(len(passwords))
 
 # Part 2
-passwords = [number for number in passwords
-             if has_adjacent_pair(number) and is_non_decreasing(number)]
+passwords = tuple(filter(has_adjacent_pair, passwords))
 print(len(passwords))
