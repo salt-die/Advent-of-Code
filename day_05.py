a1b470e7161b955601f5a76a972070b0aa07cc73
salt-die <naclmolecule@gmail.com>
2019-12-05 07:02:50 -0600

    answer added to comments

--- day_05.py
+++ day_05.py
@@ -4,5 +4,5 @@ with open('input05', 'r') as data:
     data = list(map(int, data.read().split(',')))
 
 tape = Computer(int_code=data, verbose=True)
-tape.compute() #Type '1'
-tape.compute() #Type '5'
+tape.compute() #'1': 4887191
+tape.compute() #'5': 3419022
