bb4a05c238872b8feb2ed01f833218be40e51dad
salt-die <naclmolecule@gmail.com>
2019-12-04 19:18:58 -0600

    removed commented code

--- day_04.py
+++ day_04.py
@@ -1,6 +1,5 @@
-from itertools import groupby, tee
+from itertools import groupby
 from glen import glen # generator length
-
 
 def non_decreasing(start, end):
     number = list(str(start))
@@ -17,7 +16,7 @@ def non_decreasing(start, end):
         yield number
         for i, digit in enumerate(reversed(number), start=1):
             if digit != '9':
-                number = number[:6-i]
+                number = number[:6 - i]
                 number.extend(str(int(digit) + 1) for _ in range(i))
                 break
         else:
@@ -29,17 +28,11 @@ def has_adjacent(number):
 def has_pair(number):
     return any(glen(n) == 2 for _, n in groupby(number))
 
-#def is_non_decreasing(number):
-#    number = str(number)
-#    return all(first <= second for first, second in zip(number, number[1:]))
-
 start, end = 134564, 585159
 
 # Part 1
-#nondecreasing = filter(is_non_decreasing, range(start, end + 1))
-nondecreasing = non_decreasing(start, end)
-passwords, passwords_copy = tee(filter(has_adjacent, nondecreasing))
-print(glen(passwords_copy))
+passwords = tuple(filter(has_adjacent, non_decreasing(start, end)))
+print(len(passwords))
 
 # Part 2
 passwords = filter(has_pair, passwords)
