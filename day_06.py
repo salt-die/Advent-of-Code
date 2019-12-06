fcdab3155a626eb42cda7340bd0bc467b5861793
salt-die <naclmolecule@gmail.com>
2019-12-06 00:02:44 -0600

    comments

--- day_06.py
+++ day_06.py
@@ -3,6 +3,6 @@ import networkx as nx
 with open('input06', 'r') as data:
     G = nx.Graph(item.strip().split(')') for item in data.readlines())
 
-print(sum(nx.shortest_path_length(G, node, 'COM') for node in G))
+print(sum(nx.shortest_path_length(G, node, 'COM') for node in G)) # Part 1
 
-print(nx.shortest_path_length(G, 'YOU', 'SAN') - 2)
+print(nx.shortest_path_length(G, 'YOU', 'SAN') - 2) # Part 2
