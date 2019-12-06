f20888ef5dde058ddd1a6f3c23973a6ff7b859d5
salt-die <naclmolecule@gmail.com>
2019-12-06 00:00:00 -0600

    space removed

--- day_06.py
+++ day_06.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
 with open('input06', 'r') as data:
-    G = nx.Graph( item.strip().split(')') for item in data.readlines())
+    G = nx.Graph(item.strip().split(')') for item in data.readlines())
 
 print(sum(nx.shortest_path_length(G, node, 'COM') for node in G))
 
