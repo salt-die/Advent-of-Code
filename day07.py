1c145230c78d21fe0c54af1155b261977e4b43b1
salt-die <naclmolecule@gmail.com>
2019-12-07 06:25:18 -0600

    rewrote with a feed system -- will change to a deque for FIFO operations at a later date

--- day07.py
+++ day07.py
@@ -9,8 +9,8 @@ for permutation in permutations('01234'):
     out = 0
     for i in range(5):
         tape = Computer(int_code=data)
-        tape.compute(std_in=(int(permutation[i]), out))
-        out = int(tape.out)
+        tape.compute(feed=(int(permutation[i]), out))
+        out = tape.out
     outs.append(out)
 
 print(max(outs)) # Part 1
@@ -18,24 +18,21 @@ print(max(outs)) # Part 1
 
 outs = []
 for permutation in permutations('56789'):
-    feed = 0
     computers = [Computer(int_code=data) for _ in range(5)]
-    amps = [c.compute_iter(std_in=int(digit)) for c, digit in zip(computers, permutation)]
-
-    for comp in amps: # Push inputs
-        next(comp)
+    amps = [c.compute_iter(feed=int(digit)) for c, digit in zip(computers, permutation)]
+    feed = [0]
 
     for amp, computer in cycle(zip(amps, computers)):
-        computer.feed = feed
-
-        try:
-            while True:
-                _, op, __ = next(amp)
-                if op == '04':
-                    feed = int(computer.out)
-                    break
-        except:
-            outs.append(feed)
+        for _, op, __, after in amp:
+            if op == '03' and not (computer.feed or after): #accepting input
+                if feed: #feed input
+                    computer.feed.append(feed.pop())
+            elif op == '04':
+                next(amp)
+                feed.append(computer.out)
+                break
+        else:
+            outs.append(feed.pop())
             break
 
 print(max(outs)) # Part 2
