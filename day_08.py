dc23a12f41d63a62ab9314efdd6b2e524f0e6356
salt-die <naclmolecule@gmail.com>
2019-12-08 00:16:00 -0600

    formatting

--- day_08.py
+++ day_08.py
@@ -4,13 +4,13 @@ with open('input08', 'r') as data:
     data = np.array(list(data.read().strip())).reshape((-1, 6, 25))
 
 fewest_zeros = min(data, key=lambda layer:np.sum(layer=='0'))
-print(np.sum(fewest_zeros=='1') * np.sum(fewest_zeros=='2')) #Part 1
+print(np.sum(fewest_zeros=='1') * np.sum(fewest_zeros=='2')) # Part 1
 
 decoded = data[0]
 for layer in data:
     decoded = np.where(decoded != '2', decoded, layer)
 
-decoded = np.where(decoded == '1','M',' ') # To make it easier to see
+decoded = np.where(decoded == '1', 'M', ' ') # To make it easier to see
 for row in decoded:
     print(*row) # Part 2
 
