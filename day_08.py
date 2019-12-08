302d9c8f34d3bdc1b8f183f59189492a4c91594d
salt-die <naclmolecule@gmail.com>
2019-12-08 01:37:11 -0600

    avoiding more python loops

--- day_08.py
+++ day_08.py
@@ -1,22 +1,12 @@
 import numpy as np
+from functools import reduce
+import matplotlib.pyplot as plt
 
 with open('input08', 'r') as data:
-    data = np.array(list(data.read().strip())).reshape((-1, 6, 25))
+    data = np.fromiter(data.read().strip(), int).reshape((-1, 6, 25))
 
-fewest_zeros = min(data, key=lambda layer:np.sum(layer == '0'))
-print(np.sum(fewest_zeros == '1') * np.sum(fewest_zeros == '2')) # Part 1
+fewest_zeros = data[np.argmin((data == 0).sum(axis=(1, 2)))]
+print((fewest_zeros == 1).sum() * (fewest_zeros == 2).sum()) # Part 1
 
-decoded = data[0]
-for layer in data:
-    decoded = np.where(decoded != '2', decoded, layer)
-
-decoded = np.where(decoded == '1', '█', ' ') # To make it easier to see
-for row in decoded: # Part 2
-    print(*row, sep='')
-
-# ████ █   ████  █    █  █
-#    █ █   ██  █ █    █  █
-#   █   █ █ ███  █    ████
-#  █     █  █  █ █    █  █
-# █      █  █  █ █    █  █
-# ████   █  ███  ████ █  █
+decoded = reduce(lambda top, bottom: np.where(top != 2, top, bottom), data)
+plt.axis('off'); plt.tight_layout(pad=0); plt.imshow(decoded); plt.show() # Part 2
