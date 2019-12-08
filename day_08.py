814494b990df0ff0a525bc4504fae552248b3242
salt-die <naclmolecule@gmail.com>
2019-12-08 00:11:44 -0600

    better numpy solution

--- day_08.py
+++ day_08.py
@@ -3,17 +3,16 @@ import numpy as np
 with open('input08', 'r') as data:
     data = np.array(list(data.read().strip())).reshape((-1, 6, 25))
 
-fewest_zeros = min(data, key=lambda layer:''.join(map(''.join,layer)).count('0'))
-digits = ''.join(map(''.join, fewest_zeros))
-print(digits.count('1') * digits.count('2')) # Part 1
+fewest_zeros = min(data, key=lambda layer:np.sum(layer=='0'))
+print(np.sum(fewest_zeros=='1') * np.sum(fewest_zeros=='2'))
 
 decoded = data[0]
 for layer in data:
-    decoded = np.where(decoded!='2', decoded, layer)
-decoded = np.where(decoded=='1','M',' ') # To make it easier to see
+    decoded = np.where(decoded != '2', decoded, layer)
+decoded = np.where(decoded == '1','M',' ') # To make it easier to see
 
-for i in decoded:
-    print(*i) # Part 2
+for row in decoded:
+    print(*row) # Part 2
 
 #   M M M M   M       M M M M     M         M     M
 #         M   M       M M     M   M         M     M
