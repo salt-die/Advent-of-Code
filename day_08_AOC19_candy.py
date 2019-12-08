89b072e0a2f32873041d83fd5fa3c4424379b6dd
salt-die <naclmolecule@gmail.com>
2019-12-08 06:05:17 -0600

    updated gif

--- day_08_AOC19_candy.py
+++ day_08_AOC19_candy.py
@@ -8,7 +8,7 @@ import imageio
 l = np.array([[0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
               [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1],
               [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1],
-              [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+              [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1],
               [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
               [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0]])
 
@@ -17,8 +17,11 @@ random_indices=np.dstack(np.where(l==1)).tolist()[0]
 shuffle(random_indices)
 counter = count()
 while random_indices:
+    if np.random.random() > .9:
+        start[tuple(random_indices.pop())] = 1
     if round(np.random.random()):
         start[tuple(random_indices.pop())] = 1
+
     cv2.imwrite(f'frames/{next(counter):03d}.png',
                 cv2.resize(np.where(start != 1, 0, 255), (250, 60), interpolation=cv2.INTER_NEAREST))
 
