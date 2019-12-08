f549286385aa7fb709cb66ed02bdf0cecc8d01aa
salt-die <naclmolecule@gmail.com>
2019-12-08 00:04:11 -0600

    a space

--- day_08.py
+++ day_08.py
@@ -7,7 +7,7 @@ fewest_zeros = min(data, key=lambda layer:''.join(map(''.join,layer)).count('0')
 digits = ''.join(map(''.join, fewest_zeros))
 print(digits.count('1') * digits.count('2')) # Part 1
 
-decoded= data[0]
+decoded = data[0]
 for layer in data:
     decoded = np.where(decoded!='2', decoded, layer)
 decoded = np.where(decoded=='1','M',' ') # To make it easier to see
