5f63a3d0abd0e4457ddfaa20343b20cc11b7f52e
salt-die <naclmolecule@gmail.com>
2019-12-07 23:56:19 -0600

    consistent quotes

--- day_08.py
+++ day_08.py
@@ -3,8 +3,8 @@ import numpy as np
 with open('input08', 'r') as data:
     data = np.array(list(data.read().strip())).reshape((-1, 6, 25))
 
-fewest_zeros = min(data, key=lambda layer:"".join(map(''.join,layer)).count('0'))
-digits = "".join(map(''.join, fewest_zeros))
+fewest_zeros = min(data, key=lambda layer:''.join(map(''.join,layer)).count('0'))
+digits = ''.join(map(''.join, fewest_zeros))
 print(digits.count('1') * digits.count('2')) # Part 1
 
 decoded= data[0]
