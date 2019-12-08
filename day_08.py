e3f5f9425d66a5d8486755924189b0dbf191eab0
salt-die <naclmolecule@gmail.com>
2019-12-08 00:23:21 -0600

    formmating

--- day_08.py
+++ day_08.py
@@ -3,16 +3,16 @@ import numpy as np
 with open('input08', 'r') as data:
     data = np.array(list(data.read().strip())).reshape((-1, 6, 25))
 
-fewest_zeros = min(data, key=lambda layer:np.sum(layer=='0'))
-print(np.sum(fewest_zeros=='1') * np.sum(fewest_zeros=='2')) # Part 1
+fewest_zeros = min(data, key=lambda layer:np.sum(layer == '0'))
+print(np.sum(fewest_zeros == '1') * np.sum(fewest_zeros == '2')) # Part 1
 
 decoded = data[0]
 for layer in data:
     decoded = np.where(decoded != '2', decoded, layer)
 
 decoded = np.where(decoded == '1', '█', ' ') # To make it easier to see
-for row in decoded:
-    print(*row) # Part 2
+for row in decoded: # Part 2
+    print(*row)
 
 # █ █ █ █   █       █ █ █ █     █         █     █
 #       █   █       █ █     █   █         █     █
