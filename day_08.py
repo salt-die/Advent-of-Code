6c2366c4fdf0366a75bca3a96d279f326760295c
salt-die <naclmolecule@gmail.com>
2019-12-08 03:07:59 -0600

    comment out animation section

--- day_08.py
+++ day_08.py
@@ -10,18 +10,18 @@ print((fewest_zeros == 1).sum() * (fewest_zeros == 2).sum()) # Part 1
 decoded = reduce(lambda top, bottom: np.where(top != 2, top, bottom), data) # Part 2
 print(*map(''.join, decoded.astype(str)), sep='\n')
 
-#Alternative Part 2 -- animation
-import cv2
-import imageio
-
-decoded = data[-1]
-for i, layer in enumerate(data[::-1]):
-    decoded = np.where(layer != 2, layer, decoded)
-    cv2.imwrite(f'frames/{i:03d}.png',
-                cv2.resize(np.where(decoded != 0, 255, 0),
-                           (250, 60),
-                           interpolation=cv2.INTER_NEAREST))
-
-frames = [imageio.imread(f'frames/{i:03d}.png') for i in range(len(data))]
-frames[-1:] += frames[-1:] * 20 # Show last frame for longer duration
-imageio.mimsave('frames/transmission.gif', frames, duration=.1)
+## Alternative Part 2 -- animation
+#import cv2
+#import imageio
+#
+#decoded = data[-1]
+#for i, layer in enumerate(data[::-1]):
+#    decoded = np.where(layer != 2, layer, decoded)
+#    cv2.imwrite(f'frames/{i:03d}.png',
+#                cv2.resize(np.where(decoded != 0, 255, 0),
+#                           (250, 60),
+#                           interpolation=cv2.INTER_NEAREST))
+#
+#frames = [imageio.imread(f'frames/{i:03d}.png') for i in range(len(data))]
+#frames[-1:] += frames[-1:] * 20 # Show last frame for longer duration
+#imageio.mimsave('frames/transmission.gif', frames, duration=.1)
