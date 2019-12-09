ab16b39016f9f13ef0e40375522d09344abd4598
salt-die <naclmolecule@gmail.com>
2019-12-09 02:28:45 -0600

    day 5 solution updated to take advantage of new Computer methods

--- day_05.py
+++ day_05.py
@@ -3,6 +3,5 @@ from computer import Computer
 with open('input05', 'r') as data:
     data = list(map(int, data.read().split(',')))
 
-tape = Computer(int_code=data, verbose=True)
-tape.compute() #'1': 4887191
-tape.compute() #'5': 3419022
+tape = Computer(int_code=data)
+print(*tape.compute_n(niter=2, feed=(1, 5)))
