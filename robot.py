4e941f6489ff24a2aefabc969f7ea8774fdee328
salt-die <naclmolecule@gmail.com>
2019-12-11 02:29:54 -0600

    comment

--- robot.py
+++ robot.py
@@ -14,7 +14,7 @@ class Robot:
         self.colors = defaultdict(int)
 
         self.animate = animate
-        if animate: # Only works for part two
+        if animate: # Only works for part two (We know how large the array needs to be.)
             self.counter = count()
             self.array = np.zeros((6, 43))
 
