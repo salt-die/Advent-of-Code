7e151a8e9bf7f1e722beb049a76f310be0414b9b
salt-die <naclmolecule@gmail.com>
2019-12-12 05:24:38 -0600

    more verbose, but we can do part one all at once

--- day_12.py
+++ day_12.py
@@ -8,35 +8,31 @@ Callisto = [8, 7, -6]
 planets = [Io, Europa, Ganymede, Callisto]
 
 combs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
-intermediate = np.zeros((6, 2), dtype=int) # For doing numpy operations in place.
-
-def apply_gravity_along(axis):
-    np.subtract(np.fliplr(positions[combs, axis]), positions[combs, axis], out=intermediate)
-    np.sign(intermediate, out=intermediate)
-    np.add.at(velocities[:, axis], combs, intermediate)
-
-def apply_velocity_along(axis):
-    positions[:, axis] += velocities[:, axis]
 
 positions = np.array(planets)
 velocities = np.zeros((4, 3), dtype=int)
+intermediate = np.zeros((6, 2, 3), dtype=int) # For doing numpy operations in place.
 for _ in range(1000):
-    for axis in range(3):
-        apply_gravity_along(axis)
-        apply_velocity_along(axis)
+    np.subtract(np.fliplr(positions[combs]), positions[combs], out=intermediate)
+    np.sign(intermediate, out=intermediate)
+    np.add.at(velocities, combs, intermediate)
+    positions += velocities
 
-print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum()) # Part 1
+print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum())
 
 positions = np.array(planets)
 velocities = np.zeros((4, 3), dtype=int)
+intermediate = np.zeros((6, 2), dtype=int)
 cycle_length = []
 for axis in range(3):
     initial_pos = positions[:, axis].copy()
     initial_vel = velocities[:, axis].copy()
     cycle = 0 # System is reversible, initial state will be the first repeated
     while True:
-        apply_gravity_along(axis)
-        apply_velocity_along(axis)
+        np.subtract(np.fliplr(positions[combs, axis]), positions[combs, axis], out=intermediate)
+        np.sign(intermediate, out=intermediate)
+        np.add.at(velocities[:, axis], combs, intermediate)
+        positions[:, axis] += velocities[:, axis]
         cycle += 1
 
         if (np.array_equal(velocities[:, axis], initial_vel) and
