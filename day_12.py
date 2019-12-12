5287e56125849e6d017e9761464c6630f8718100
salt-die <naclmolecule@gmail.com>
2019-12-12 06:28:49 -0600

    reduce velocity and position arrays into a single state

--- day_12.py
+++ day_12.py
@@ -6,37 +6,34 @@ Ganymede = [14, -4, 1]
 Callisto = [8, 7, -6]
 
 planets = [Io, Europa, Ganymede, Callisto]
-
 combs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
 
-positions = np.array(planets)
-velocities = np.zeros((4, 3), dtype=int)
+state = np.zeros((4, 6), dtype=int)
+state[:, :3] = np.array(planets)
 intermediate = np.zeros((6, 2, 3), dtype=int) # For doing numpy operations in place.
 for _ in range(1000):
-    np.subtract(np.fliplr(positions[combs]), positions[combs], out=intermediate)
+    np.subtract(np.fliplr(state[combs, :3]), state[combs, :3], out=intermediate)
     np.sign(intermediate, out=intermediate)
-    np.add.at(velocities, combs, intermediate)
-    positions += velocities
+    np.add.at(state[:, 3:], combs, intermediate)
+    state[:, :3] += state[:, 3:]
 
-print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum())
+print((np.abs(state[:, :3]).sum(axis=1) * np.abs(state[:, 3:]).sum(axis=1)).sum())
 
-positions = np.array(planets)
-velocities = np.zeros((4, 3), dtype=int)
+state = np.zeros((4, 6), dtype=int)
+state[:, :3] = np.array(planets)
 intermediate = np.zeros((6, 2), dtype=int)
 cycle_length = []
 for axis in range(3):
-    initial_pos = positions[:, axis].copy()
-    initial_vel = velocities[:, axis].copy()
+    initial_state = state[:, [axis, axis + 3]].copy()
     cycle = 0 # System is reversible, initial state will be the first repeated
     while True:
-        np.subtract(np.fliplr(positions[combs, axis]), positions[combs, axis], out=intermediate)
+        np.subtract(np.fliplr(state[combs, axis]), state[combs, axis], out=intermediate)
         np.sign(intermediate, out=intermediate)
-        np.add.at(velocities[:, axis], combs, intermediate)
-        positions[:, axis] += velocities[:, axis]
+        np.add.at(state[:, axis + 3], combs, intermediate)
+        state[:, axis] += state[:, axis + 3]
         cycle += 1
 
-        if (np.array_equal(velocities[:, axis], initial_vel) and
-            np.array_equal(positions[:, axis], initial_pos)):
+        if np.array_equal(state[:, [axis, axis + 3]], initial_state):
             cycle_length.append(cycle)
             break
 
