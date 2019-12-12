faeedfb4bf043d95a326f3be9a49b9c107b14186
salt-die <naclmolecule@gmail.com>
2019-12-12 02:38:47 -0600

    numpy indexing

--- day_12.py
+++ day_12.py
@@ -10,12 +10,11 @@ Callisto = [8, 7, -6]
 planets = [Io, Europa, Ganymede, Callisto]
 
 def apply_gravity_along(axis):
-    for one, two in combinations(range(4),r=2):
-        if positions[one, axis] == positions[two, axis]:
+    for comb in combinations(range(4), r=2):
+        if not np.ptp(positions[comb, axis]):
             continue
         else:
-            velocities[one, axis] += (-1)**(comp := (positions[one, axis] > positions[two, axis]))
-            velocities[two, axis] += (-1)**(not comp)
+            velocities[comb, axis] += (-1)**((arr := positions[comb, axis]) - arr[::-1] > 0)
 
 def apply_velocity_along(axis):
     positions[:, axis] += velocities[:, axis]
@@ -29,20 +28,20 @@ for _ in range(1000):
 
 print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum()) # Part 1
 
-
-positions = np.array(planets)
-velocities = np.zeros((4, 3), dtype=int)
-cycle_length = []
-for axis in range(3):
-    initial_state = tuple(chain(positions[:,axis], velocities[:, axis]))
-    cycle = 0 # System is reversible, initial state will be the first repeated
-    while True:
-        if initial_state == tuple(chain(positions[:,axis], velocities[:, axis])) and cycle:
-            cycle_length.append(cycle)
-            break
-
-        apply_gravity_along(axis)
-        apply_velocity_along(axis)
-        cycle += 1
-
-print(np.lcm.reduce(cycle_length)) # Part 2
+#
+#positions = np.array(planets)
+#velocities = np.zeros((4, 3), dtype=int)
+#cycle_length = []
+#for axis in range(3):
+#    initial_state = tuple(chain(positions[:,axis], velocities[:, axis]))
+#    cycle = 0 # System is reversible, initial state will be the first repeated
+#    while True:
+#        if initial_state == tuple(chain(positions[:,axis], velocities[:, axis])) and cycle:
+#            cycle_length.append(cycle)
+#            break
+#
+#        apply_gravity_along(axis)
+#        apply_velocity_along(axis)
+#        cycle += 1
+#
+#print(np.lcm.reduce(cycle_length)) # Part 2
