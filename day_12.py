760959b8a5f83a7193ce805b28001ab059d65267
salt-die <naclmolecule@gmail.com>
2019-12-12 01:51:18 -0600

    revised to numpy solution

--- day_12.py
+++ day_12.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
-from functools import reduce
-from itertools import combinations
+from itertools import combinations, chain
 import numpy as np
 
 Io = [19, -10, 7]
@@ -12,42 +11,33 @@ planets = [Io, Europa, Ganymede, Callisto]
 
 def apply_gravity_along(axis):
     for one, two in combinations(range(4),r=2):
-        if positions[one][axis] == positions[two][axis]:
+        if positions[one, axis] == positions[two, axis]:
             continue
         else:
-            velocities[one][axis] += (-1)**(positions[one][axis] > positions[two][axis])
-            velocities[two][axis] += (-1)**(positions[one][axis] < positions[two][axis])
+            velocities[one, axis] += (-1)**(comp := (positions[one, axis] > positions[two, axis]))
+            velocities[two, axis] += (-1)**(not comp)
 
 def apply_velocity_along(axis):
-    for planet in range(4):
-        positions[planet][axis] += velocities[planet][axis]
+    positions[:, axis] += velocities[:, axis]
 
-def energy():
-    return sum(sum(map(abs, position)) * sum(map(abs, velocity))
-               for position, velocity in zip(positions,velocities))
-
-
-positions = deepcopy(planets)
-velocities = [[0] * 3 for _ in range(4)]
-
+positions = np.array(planets)
+velocities = np.zeros((4, 3), dtype=int)
 for _ in range(1000):
     for axis in range(3):
         apply_gravity_along(axis)
         apply_velocity_along(axis)
 
-print(energy()) # Part 1
+print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum()) # Part 1
 
 
-positions = deepcopy(planets)
-velocities = [[0] * 3 for _ in range(4)]
+positions = np.array(planets)
+velocities = np.zeros((4, 3), dtype=int)
 intervals = []
-
 for axis in range(3):
     previous_states = set()
     cycle = 0
     while True:
-        state = tuple((position[axis], velocity[axis])
-                      for position, velocity in zip(positions, velocities))
+        state = tuple(value for value in chain(positions[:,axis], velocities[:, axis]))
         if state in previous_states:
             intervals.append(cycle)
             break
@@ -58,4 +48,4 @@ for axis in range(3):
         apply_velocity_along(axis)
         cycle += 1
 
-print(reduce(np.lcm, intervals)) # Part 2
+print(np.lcm.reduce(intervals)) # Part 2
