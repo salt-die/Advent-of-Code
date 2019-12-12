6b45b45a51df5ea8fac3cf8bb480887e3cde5887
salt-die <naclmolecule@gmail.com>
2019-12-12 03:20:04 -0600

    ptp for equality check

--- day_12.py
+++ day_12.py
@@ -1,3 +1,8 @@
+"""
+Warning: This solution is slow due to the number of arrays we create.  Std library solutions
+recommended, but this was to practice numpy implementation.  We can also pass all combinations
+of indices at once in apply_gravity_along, but I haven't figured out the masking needed yet.
+"""
 from copy import deepcopy
 from itertools import combinations, chain
 import numpy as np
@@ -11,9 +16,7 @@ planets = [Io, Europa, Ganymede, Callisto]
 
 def apply_gravity_along(axis):
     for comb in combinations(range(4), r=2):
-        if not np.ptp(positions[comb, axis]):
-            continue
-        else:
+        if np.ptp(positions[comb, axis]):
             velocities[comb, axis] += (-1)**((arr := positions[comb, axis]) - arr[::-1] > 0)
 
 def apply_velocity_along(axis):
@@ -28,7 +31,6 @@ for _ in range(1000):
 
 print((np.abs(positions).sum(axis=1) * np.abs(velocities).sum(axis=1)).sum()) # Part 1
 
-
 positions = np.array(planets)
 velocities = np.zeros((4, 3), dtype=int)
 cycle_length = []
@@ -36,7 +38,9 @@ for axis in range(3):
     initial_state = tuple(chain(positions[:,axis], velocities[:, axis]))
     cycle = 0 # System is reversible, initial state will be the first repeated
     while True:
-        if initial_state == tuple(chain(positions[:,axis], velocities[:, axis])) and cycle:
+        if (all(init_val == state_val
+                for init_val, state_val in zip(initial_state, chain(positions[:, axis], velocities[:, axis])))
+           and cycle):
             cycle_length.append(cycle)
             break
 
