3acac9441b51a47bcfecf0af6457d83780c8a5ce
salt-die <naclmolecule@gmail.com>
2019-12-12 07:30:34 -0600

    removed outer python loop

--- day_12.py
+++ day_12.py
@@ -21,20 +21,21 @@ print((np.abs(state[:, :3]).sum(axis=1) * np.abs(state[:, 3:]).sum(axis=1)).sum(
 
 state = np.zeros((4, 6), dtype=int)
 state[:, :3] = np.array(planets)
-intermediate = np.zeros((6, 2), dtype=int)
-cycle_length = []
-for axis in range(3):
-    initial_state = state[:, axis::3].copy()
-    cycle = 0 # System is reversible, initial state will be the first repeated
-    while True:
-        np.subtract(np.fliplr(state[combs, axis]), state[combs, axis], out=intermediate)
-        np.sign(intermediate, out=intermediate)
-        np.add.at(state[:, axis + 3], combs, intermediate)
-        state[:, axis] += state[:, axis + 3]
-        cycle += 1
 
-        if np.array_equal(state[:, axis::3], initial_state):
-            cycle_length.append(cycle)
-            break
+flags = [True] * 3
+cycle_lengths = []
+initial_state = state.copy()
+cycle = 0 # System is reversible, initial state will be the first repeated
+while any(flags):
+    np.subtract(np.fliplr(state[combs, :3]), state[combs, :3], out=intermediate)
+    np.sign(intermediate, out=intermediate)
+    np.add.at(state[:, 3:], combs, intermediate)
+    state[:, :3] += state[:, 3:]
+    cycle += 1
 
-print(np.lcm.reduce(cycle_length)) # Part 2
+    for axis, flag in enumerate(flags):
+        if flag and np.array_equal(state[:, axis::3], initial_state[:, axis::3]):
+            cycle_lengths.append(cycle)
+            flags[axis] = False
+
+print(np.lcm.reduce(cycle_lengths)) # Part 2
