efe1aedb863b96e581a597b3c467e00d2c98d0da
salt-die <naclmolecule@gmail.com>
2019-12-16 05:07:47 -0600

    throwaway made explicit

--- day_16.py
+++ day_16.py
@@ -19,11 +19,11 @@ print(''.join(map(str, number[:8]))) # Part 1
 
 number = (list(map(int, data)) * 10000)[int(data[:7]):] # Slice at offset
 
-for i in range(100):
+for _ in range(100):
     sum_ = 0
-    for j, digit in enumerate(reversed(number), start=1):
+    for i, digit in enumerate(reversed(number), start=1):
         sum_ += digit
         sum_ %= 10
-        number[-j] = sum_
+        number[-i] = sum_
 
 print(''.join(map(str, number[:8]))) # Part 2
