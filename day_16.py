3453c70c5e008eaed1a644fa663d770df0302292
salt-die <naclmolecule@gmail.com>
2019-12-16 01:13:07 -0600

    renamed variables

--- day_16.py
+++ day_16.py
@@ -8,17 +8,16 @@ BASE = (0,1,0,-1)
 def phase(number):
     new_number = []
     for i, digit in enumerate(number, start=1):
-        multiplier = (repeated_factor for factor in cycle(BASE)
-                                      for repeated_factor in repeat(factor, i))
-        next(multiplier)
-        new_number.append(abs(sum(digit * next(multiplier) for digit in number)) % 10)
+        coefficients = (coef for factor in cycle(BASE) for coef in repeat(factor, i))
+        next(coefficients)
+        new_number.append(abs(sum(digit * next(coefficients) for digit in number)) % 10)
     return new_number
-#
-#number = list(map(int, data))
-#for _ in range(100):
-#    number = phase(number)
-#
-#print(''.join(map(str, number[:8]))) # Part 1
+
+number = list(map(int, data))
+for _ in range(100):
+    number = phase(number)
+
+print(''.join(map(str, number[:8]))) # Part 1
 
 number = (list(map(int, data)) * 10000)[int(data[:7]):] # Slice at offset
 
