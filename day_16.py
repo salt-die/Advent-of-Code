eb0673618314fffbd725aa5b4141ab79233f6ce0
salt-die <naclmolecule@gmail.com>
2019-12-16 04:38:30 -0600

    optimized

--- day_16.py
+++ day_16.py
@@ -3,19 +3,19 @@ from itertools import cycle, repeat
 with open('input16', 'r') as data:
     data = data.read().strip()
 
-#def phase(number):
-#    new_number = []
-#    for i, digit in enumerate(number, start=1):
-#        coefficients = (coef for base in cycle((0,1,0,-1)) for coef in repeat(base, i))
-#        next(coefficients)
-#        new_number.append(abs(sum(digit * next(coefficients) for digit in number)) % 10)
-#    return new_number
-#
-#number = list(map(int, data))
-#for _ in range(100):
-#    number = phase(number)
-#
-#print(''.join(map(str, number[:8]))) # Part 1
+def phase(number):
+    new_number = []
+    for i, digit in enumerate(number, start=1):
+        coefficients = (coef for base in cycle((0,1,0,-1)) for coef in repeat(base, i))
+        next(coefficients)
+        new_number.append(abs(sum(digit * next(coefficients) for digit in number)) % 10)
+    return new_number
+
+number = list(map(int, data))
+for _ in range(100):
+    number = phase(number)
+
+print(''.join(map(str, number[:8]))) # Part 1
 
 number = (list(map(int, data)) * 10000)[int(data[:7]):] # Slice at offset
 
