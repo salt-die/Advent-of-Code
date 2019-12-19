8f31f22b3df13758a926b36ddad9107d650dd92b
salt-die <naclmolecule@gmail.com>
2019-12-19 06:39:58 -0600

    simplifiy part 1

--- day_19.py
+++ day_19.py
@@ -1,26 +1,21 @@
 from computer import Computer
-from display import Display
-import numpy as np
+from itertools import product
 
 with open('input19', 'r') as data:
     data = list(map(int, data.read().split(',')))
 
 tape = Computer(int_code=data)
-screen = Display()
-pixels = np.zeros((50, 50), dtype=int)
-coordinates = np.nditer(pixels, flags=['multi_index'])
+area = 0
 
-for _ in coordinates:
-    tape << (coordinate := coordinates.multi_index)
-    tape.compute()
-    pixels[coordinate] = tape.pop()
-    screen(pixels.T)
+for coordinate in product(range(50), repeat=2):
+    tape.compute(feed=coordinate)
+    area += tape.pop()
 
-screen.text(f'Number of pixels is: {pixels.sum()}') # Part 1
-screen.stop()
+print(area) # Part 1: 114
 
-# By inspection the slope of the outer lines is roughly 11/7 and 10/7, so x-coordinate should
-# be roughly in the 700 range to fit a 100x100 box in the tractor beam, begin search there:
+# By inspection the slope of the outer lines is roughly 11/7 and 10/7 (original solution was
+# plotted on a numpy array), so x-coordinate should be roughly in the 700 range to fit a
+# 100x100 box in the tractor beam, begin search there:
 
 def is_beam(x, y):
     tape.compute(feed=(x, y))
