17d273ea7ca56992624a51a59d191102d883e7fe
salt-die <naclmolecule@gmail.com>
2019-12-20 09:46:37 -0600

    day 20 initial

---
+++ day_20.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+import networkx as nx
+import numpy as np
+
+with open('input20', 'r') as data:
+    maze = np.array([list(row.strip('\n')) for row in data])
+
+height, width = maze.shape
+G = nx.grid_graph([width, height])
+
+walls = np.vstack(np.where(maze != '.')).T
+G.remove_nodes_from(map(tuple, walls)) # Remove walls from graph
+
+def is_portal(portal):
+    A, B, C = portal
+    return A.isupper() and B.isupper() and C == '.'
+
+mapping = defaultdict(tuple)
+maze_iter = np.nditer(maze, flags=['multi_index'])
+for char in maze_iter: # This awful loop is just to find indices of portals.
+    if str(char).isupper():
+        y, x = maze_iter.multi_index
+        # Check down:
+        if y < height - 2 and is_portal((portal := maze[y: y + 3, x])):
+            mapping[''.join(portal[:-1])] += ((y + 2, x), )
+        # Check left:
+        elif x > 1 and is_portal((portal := maze[y, x: x - 3: -1])):
+             mapping[''.join(portal[1::-1])] += ((y, x - 2), )
+        # Check right:
+        elif x < width - 2 and is_portal((portal := maze[y, x: x + 3])):
+            mapping[''.join(portal[:-1])] += ((y, x + 2), )
+        # Check up:
+        elif y > 1 and is_portal((portal := maze[y: y - 3: -1, x])):
+            mapping[''.join(portal[1::-1])] += ((y - 2, x), )
+
+(AA, ), (ZZ, ) = mapping.pop('AA'), mapping.pop('ZZ')
+G.add_edges_from(mapping.values())
+print(nx.shortest_path_length(G, AA, ZZ)) # Part 1
+
+G.remove_edges_from(mapping.values())
+inner = {}
+outer = {}
+for name, locations in mapping.items():
+    for location in locations:
+        y, x = location
+        if any((y == 2, y == height - 3, x == 2, x == width - 3)):
+            outer[name] = location
+        else:
+            inner[name] = location
+
+H = nx.Graph()
+for level in range(26): # Levels increased by hand until a path was found.
+    for start, end in G.edges:
+        H.add_edge((*start, level), (*end, level))
+    for name, location in inner.items():
+        H.add_edge((*location, level), (*outer[name], level + 1))
+print(nx.shortest_path_length(H, (*AA, 0), (*ZZ, 0))) # Part 2
