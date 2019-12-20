f61ba946f541c60af59d9302ef9ffaa08a6ac890
salt-die <naclmolecule@gmail.com>
2019-12-20 16:48:38 -0600

    better variable names

--- day_20.py
+++ day_20.py
@@ -16,33 +16,34 @@ def is_portal(portal):
     A, B, C = portal
     return A.isupper() and B.isupper() and C == '.'
 
-mapping = defaultdict(tuple)
+portals = defaultdict(tuple)
 maze_iter = np.nditer(maze, flags=['multi_index'])
 for char in maze_iter: # This awful loop is just to find indices of portals.
     if str(char).isupper():
         y, x = maze_iter.multi_index
         if y < height - 2 and is_portal((portal := maze[y: y + 3, x])): # Check down
-            mapping[''.join(portal[:-1])] += ((y + 2, x), )
+            portals[''.join(portal[:-1])] += ((y + 2, x), )
         elif y > 1 and is_portal((portal := maze[y: y - 3: -1, x])): # Check up
-            mapping[''.join(portal[1::-1])] += ((y - 2, x), )
+            portals[''.join(portal[1::-1])] += ((y - 2, x), )
         elif x < width - 2 and is_portal((portal := maze[y, x: x + 3])): # Check right
-            mapping[''.join(portal[:-1])] += ((y, x + 2), )
+            portals[''.join(portal[:-1])] += ((y, x + 2), )
         elif x > 1 and is_portal((portal := maze[y, x: x - 3: -1])): # Check left
-             mapping[''.join(portal[1::-1])] += ((y, x - 2), )
+             portals[''.join(portal[1::-1])] += ((y, x - 2), )
 
-portals = set(chain(*mapping.values()))
-while True: # Prune dead-ends and isolated nodes that aren't portals.
-    dead = [node for node, degree in nx.degree(G) if node not in portals and degree <= 1]
+portal_locations = set(chain(*portals.values()))
+while True: # Prune dead-ends and isolated nodes that aren't portal_locations.
+    dead = [node for node, degree in nx.degree(G) if node not in portal_locations and degree <= 1]
     if not dead:
         break
     G.remove_nodes_from(dead)
 
-(AA, ), (ZZ, ) = mapping.pop('AA'), mapping.pop('ZZ')
-G.add_edges_from(mapping.values())
+(AA, ), (ZZ, ) = portals.pop('AA'), portals.pop('ZZ')
+G.add_edges_from(portals.values()) # Connect portal_locations
+
 nx.set_edge_attributes(G, 1, name='weight')
 while True: # Contract paths, adding adjacent weights.
     for node, degree in nx.degree(G):
-        if degree == 2 and node not in portals:
+        if degree == 2 and node not in portal_locations:
             (*_, w1), (*_, w2) = G.edges(node, data='weight')
             G.add_edge(*G.neighbors(node), weight=w1 + w2)
             G.remove_node(node)
@@ -51,10 +52,10 @@ while True: # Contract paths, adding adjacent weights.
         break
 
 print(nx.shortest_paths.dijkstra_path_length(G, AA, ZZ)) # Part 1
-G.remove_edges_from(mapping.values())
+G.remove_edges_from(portals.values()) # Un-connect portals
 
 inner, outer, outer_coords = {}, {}, set((2, height - 3, width - 3))
-for portal, nodes in mapping.items():
+for portal, nodes in portals.items():
     for node in nodes:
         (inner if set(node).isdisjoint(outer_coords) else outer)[portal] = node
 
