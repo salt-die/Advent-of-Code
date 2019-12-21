bb5e77a1374379e970663112f245b66b4d1d7ffe
salt-die <naclmolecule@gmail.com>
2019-12-21 09:54:03 -0600

    day 21 initial commit

---
+++ day_21.py
@@ -0,0 +1,50 @@
+from computer import Computer
+
+with open('input21', 'r') as data:
+    data = list(map(int, data.read().split(',')))
+
+brain = Computer(int_code=data)
+
+instructions = '''NOT A J
+NOT B T
+AND D T
+OR T J
+NOT C T
+AND D T
+OR T J
+WALK
+'''
+# NOT A OR (NOT B AND D) OR (NOT C AND D)
+brain << map(ord, instructions)
+
+for _, op, _, _, _ in brain:
+    if brain and brain.out[0] == ord('\n'):
+        print(''.join(map(chr, reversed(brain.out))), end='')
+        brain.out.clear()
+
+print(brain.last()) # Part 1
+
+instructions = '''NOT C T
+AND D T
+OR H J
+OR E J
+AND T J
+OR B T
+OR E T
+NOT T T
+AND D T
+OR T J
+NOT A T
+OR T J
+RUN
+'''
+# (NOT C AND D AND (H OR E)) OR (NOT (B OR E) AND D) OR NOT A
+brain << map(ord, instructions)
+
+for _, op, _, _, _ in brain:
+    if brain and brain.out[0] == ord('\n'):
+        print(''.join(map(chr, reversed(brain.out))), end='')
+        brain.out.clear()
+
+print(brain.last()) # Part 2
+
