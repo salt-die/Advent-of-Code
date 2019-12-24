479178ba7ca83f5f3bb513e70cac7ec981a37ce2
salt-die <naclmolecule@gmail.com>
2019-12-24 14:30:37 -0600

    logspace used for bitmasking

--- day_24.py
+++ day_24.py
@@ -18,8 +18,7 @@ while True:
         break
     states.add(as_string)
 
-powers = np.nditer(bugs, flags=['c_index'])
-print(sum(cooef * 2**powers.index for cooef in powers)) # Part 1: 18842609
+print((bugs.flatten() * np.logspace(0, 24, 25, base=2, dtype=int)).sum())
 
 levels = defaultdict(lambda:np.zeros_like(bugs), {0: np.array(data)})
 levels[-1]; levels[1] # First outer and inner level
