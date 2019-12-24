058b8b4f067ee8d3f0c0ff247e543fad79a91969
salt-die <naclmolecule@gmail.com>
2019-12-24 17:01:50 -0600

    a space

--- day_02.py
+++ day_02.py
@@ -14,6 +14,6 @@ print(tape.read(0))
 for i, j in product(range(100), repeat=2):
     tape.int_code[1:3]= i, j
     tape.compute()
-    if  tape.read(0) == 19690720: #Date of moon landing
+    if tape.read(0) == 19690720: #Date of moon landing
         print(100 * i + j)
         break
