70155a29923f28089a51d54ed3797dc4b2a2b8c4
salt-die <naclmolecule@gmail.com>
2019-12-24 13:51:42 -0600

    np.where replaced with astype(int) conversion in .convolve

--- day_24.py
+++ day_24.py
@@ -5,14 +5,14 @@ import scipy.ndimage as nd
 KERNEL = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
 with open('input24', 'r') as data:
-    data = [[int(char == '#') for char in line] for line in data.read().splitlines()]
+    data = [[char == '#' for char in line] for line in data.read().splitlines()]
 
 bugs = np.array(data)
 states = set((bugs.tostring(), ))
 while True:
-    neighbor_count = nd.convolve(bugs, KERNEL, mode="constant")
-    still_alive = np.where(bugs & (neighbor_count == 1), 1, 0)
-    new_borns = np.where(~bugs & ((neighbor_count == 2) | (neighbor_count == 1)), 1, 0)
+    neighbor_count = nd.convolve(bugs.astype(int), KERNEL, mode="constant")
+    still_alive = bugs & (neighbor_count == 1)
+    new_borns = ~bugs & ((neighbor_count == 2) | (neighbor_count == 1))
     bugs = still_alive + new_borns
     if (as_string := bugs.tostring()) in states:
         break
@@ -26,14 +26,14 @@ levels[-1]; levels[1] # First outer and inner level
 for _ in range(200):
     new = {}
     for level, bugs in tuple(levels.items()):
-        neighbor_count = nd.convolve(bugs, KERNEL, mode="constant")
+        neighbor_count = nd.convolve(bugs.astype(int), KERNEL, mode="constant")
         for outer, inner in ((0, (1, 2)), ((..., 0), (2, 1)),
                              (4, (3, 2)), ((..., 4), (2, 3))):
             neighbor_count[outer] += levels[level - 1][inner]
             neighbor_count[inner] += levels[level + 1][outer].sum()
 
-        still_alive = np.where(bugs & (neighbor_count == 1), 1, 0)
-        new_borns = np.where(~bugs & ((neighbor_count == 2) | (neighbor_count == 1)), 1, 0)
+        still_alive = bugs & (neighbor_count == 1)
+        new_borns = ~bugs & ((neighbor_count == 2) | (neighbor_count == 1))
         new[level] = still_alive + new_borns
         new[level][2, 2] = 0 # Center stays empty
     levels.update(new)
