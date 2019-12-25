bf11ecda8c0bd3417a72703eccea21dd60c78d1d
salt-die <naclmolecule@gmail.com>
2019-12-24 22:53:35 -0600

    signal convolve instead of ndimage so we don't have to convert boolean arrays to integer arrays before we convolve

--- day_24.py
+++ day_24.py
@@ -1,17 +1,16 @@
 import numpy as np
-import scipy.ndimage as nd
+from scipy.signal import convolve
 
 KERNEL = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
 with open('input24', 'r') as data:
-    data = [[int(char == '#') for char in line] for line in data.read().splitlines()]
+    data = [[char == '#' for char in line] for line in data.read().splitlines()]
 
 bugs = np.array(data)
 states = set((bugs.tostring(), ))
 while True:
-    neighbor_count = nd.convolve(bugs, KERNEL, mode="constant")
-    bugs = ((bugs & (neighbor_count == 1)) |
-            (~bugs & np.isin(neighbor_count, [1, 2]))).astype(int)
+    neighbor_count = convolve(bugs, KERNEL, mode='same')
+    bugs = (bugs & (neighbor_count == 1)) | (~bugs & np.isin(neighbor_count, [1, 2]))
     if (as_string := bugs.tostring()) in states:
         break
     states.add(as_string)
@@ -23,14 +22,13 @@ KERNEL3D = np.pad(KERNEL[None], [(1,), (0,), (0,)])
 UP, dn = slice(1, None), slice(None, -1)
 
 for _ in range(200):
-    neighbor_count = nd.convolve(levels, KERNEL3D, mode='constant')
+    neighbor_count = convolve(levels, KERNEL3D, mode='same')
     neighbor_count[:, 2, 2] = 0
     for outer, inner in ((( 0,), (1, 2)), ((...,  0), (2, 1)),
                          ((-1,), (3, 2)), ((..., -1), (2, 3))):
         neighbor_count[(UP, *outer)] += levels[(dn, *inner, None)]
         neighbor_count[(dn, *inner)] += levels[(UP, *outer)].sum(axis=1)
 
-    levels = ((levels & (neighbor_count == 1)) |
-              (~levels & np.isin(neighbor_count, [1, 2]))).astype(int)
+    levels = (levels & (neighbor_count == 1)) | (~levels & np.isin(neighbor_count, [1, 2]))
 
 print(levels.sum()) # Part 2: 2059
