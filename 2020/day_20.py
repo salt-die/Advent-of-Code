31576173fa51f89627b1bee70622b2153825eeac
salt-die <53280662+salt-die@users.noreply.github.com>
2020-11-30 03:38:43 -0600

    use walrus in template

--- 2020/day_20.py
+++ 2020/day_20.py
@@ -12,10 +12,8 @@ def part_two():
     ...
 
 if __name__ = "__main__":
-    a = part_one()
-    if a is not None:
+    if a := part_one():
         aoc_helper.submit(day=20, part=1, a)
 
-    b = part_two()
-    if b is not None:
+    if b := part_two():
         aoc_helper.submit(day=20, part=2, b)
