be0a4c21520fb40dabbcbf2300774b28d7dc53eb
salt-die <53280662+salt-die@users.noreply.github.com>
2020-11-30 23:20:49 -0600

    day 1 solution done!

--- 2020/day_01.py
+++ 2020/day_01.py
@@ -1,18 +1,22 @@
 import aoc_helper
+from itertools import combinations
 
 raw = aoc_helper.day(1)
-print(raw)
 
 def parse_raw():
-    ...
+    return [int(line) for line in raw.splitlines()]
 
 data = parse_raw()
 
 def part_one():
-    ...
+    for a, b in combinations(data, 2):
+        if a + b == 2020:
+            return a * b
 
 def part_two():
-    ...
+    for a, b, c in combinations(data, 3):
+        if a + b + c == 2020:
+            return a * b * c
 
 aoc_helper.submit(day=1, solv_func=part_one)
 aoc_helper.submit(day=1, solv_func=part_two)
