fbff945f0068f8518dc31c87c3cdecff505d63de
salt-die <53280662+salt-die@users.noreply.github.com>
2020-11-30 23:34:04 -0600

    order of magnitude optimization

--- 2020/day_01.py
+++ 2020/day_01.py
@@ -4,18 +4,18 @@ from itertools import combinations
 raw = aoc_helper.day(1)
 
 def parse_raw():
-    return [int(line) for line in raw.splitlines()]
+    return set(int(line) for line in raw.splitlines())
 
 data = parse_raw()
 
 def part_one():
-    for a, b in combinations(data, 2):
-        if a + b == 2020:
+    for a in data:
+        if (b := (2020 - a)) in data:
             return a * b
 
 def part_two():
-    for a, b, c in combinations(data, 3):
-        if a + b + c == 2020:
+    for a, b in combinations(data, 2):
+        if (c := (2020 - a - b)) in data:
             return a * b * c
 
 aoc_helper.submit(day=1, solv_func=part_one)
