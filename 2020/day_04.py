51a36cb71b2a02dee9ec9fb407ab02696f40741c
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-04 06:05:21 -0600

    re only matching!

--- 2020/day_04.py
+++ 2020/day_04.py
@@ -4,24 +4,22 @@ import re
 
 raw = aoc_helper.day(4)
 passports = [adict(re.findall(r"(?!cid)([a-z]{3}):(\S+)", line)) for line in raw.split("\n\n")]
+FIELDS = "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
 
 def part_one():
     return sum(len(pp) == 7 for pp in passports)
 
 def part_two():
-    HCL_RE = re.compile(r"^#[0-9a-f]{6}$")
-    PID_RE = re.compile(r"^\d{9}$")
-
-    return sum(     len(pp) == 7
-               and  1920 <= int(pp.byr) <= 2002
-               and  2010 <= int(pp.iyr) <= 2020
-               and  2020 <= int(pp.eyr) <= 2030
-               and  (   pp.hgt.endswith("cm") and 150 <= int(pp.hgt[:-2]) <= 193
-                     or pp.hgt.endswith("in") and  59 <= int(pp.hgt[:-2]) <= 76)
-               and bool(HCL_RE.match(pp.hcl))
-               and pp.ecl in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-               and bool(PID_RE.match(pp.pid))
-                for pp in passports)
+    RES = (
+        r"19[2-9][\d]|200[0-2]",                           # byr
+        r"201[\d]|2020",                                   # iyr
+        r"202[\d]|2030",                                   # eyr
+        r"1(?:[5-8][\d]|9[0-3])cm|(?:59|6[\d]|7[0-6])in",  # hgt
+        r"#[0-9a-f]{6}",                                   # hcl
+        r"amb|blu|brn|gry|grn|hzl|oth",                    # ecl
+        r"\d{9}",                                          # pid
+    )
+    return sum(len(pp) == 7 and all(map(bool, map(re.fullmatch, RES, map(pp.__getitem__, FIELDS)))) for pp in passports)
 
 aoc_helper.submit(4, part_one)
 aoc_helper.submit(4, part_two)
