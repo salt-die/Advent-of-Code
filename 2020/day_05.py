50bdbe887c582b5c29c17310c498a610278dd520
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-05 00:49:20 -0600

    part 5 done!

--- 2020/day_05.py
+++ 2020/day_05.py
@@ -1,18 +1,22 @@
 import aoc_helper
 
 raw = aoc_helper.day(5)
-print(raw)
 
 def parse_raw():
-    ...
+    trans = str.maketrans('FBRL', '0110')
 
-data = parse_raw()
+    for line in raw.splitlines():
+        line = line.translate(trans)
+        yield int(line[:-3], 2) * 8 + int(line[-3:], 2)
+
+data = set(parse_raw())
 
 def part_one():
-    ...
+    return max(data)
 
 def part_two():
-    ...
+    l, m, s = len(data) + 1, min(data) - 1, sum(data)
+    return (l * (l + 1) // 2 + l * m) - s
 
 aoc_helper.submit(5, part_one)
 aoc_helper.submit(5, part_two)
