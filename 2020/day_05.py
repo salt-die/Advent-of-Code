bf1963e4821523f944cb7c585ee9781935997243
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-05 01:02:22 -0600

    * 8 is just a bit shift by 3! no need to split the string!

--- 2020/day_05.py
+++ 2020/day_05.py
@@ -2,14 +2,8 @@ import aoc_helper
 
 raw = aoc_helper.day(5)
 
-def parse_raw():
-    trans = str.maketrans('FBRL', '0110')
-
-    for line in raw.splitlines():
-        line = line.translate(trans)
-        yield int(line[:-3], 2) * 8 + int(line[-3:], 2)
-
-data = list(parse_raw())
+trans = str.maketrans('FBRL', '0110')
+data = [int(line.translate(trans), 2) for line in raw.splitlines()]
 
 def part_one():
     return max(data)
@@ -18,5 +12,6 @@ def part_two():
     l, m = len(data) + 1, min(data) - 1,
     return l * (l + 1) // 2 + l * m - sum(data)
 
+print(part_one(), part_two())
 aoc_helper.submit(5, part_one)
 aoc_helper.submit(5, part_two)
