72378c88bd6915ae65454e922e958b888f024ef2
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-05 23:40:00 -0600

    abstracted logic into a separate function

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -2,13 +2,16 @@ import aoc_helper
 from functools import reduce
 
 raw = aoc_helper.day(6)
-data = raw.split('\n\n')
+data = [list(map(set, group.splitlines())) for group in raw.split('\n\n')]
+
+def combine_with(func):
+    return sum(len(reduce(func, group)) for group in data)
 
 def part_one():
-    return sum(len(set(filter(str.isalpha, group))) for group in data)
+    return combine_with(set.union)
 
 def part_two():
-    return sum(len(reduce(set.intersection, map(set, group.splitlines()))) for group in data)
+    return combine_with(set.intersection)
 
 aoc_helper.submit(6, part_one)
 aoc_helper.submit(6, part_two)
