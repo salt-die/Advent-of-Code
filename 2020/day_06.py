5315e87cdf75834110a97d3787749c9b086f663d
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-05 23:29:07 -0600

    day 6 done!

--- 2020/day_06.py
+++ 2020/day_06.py
@@ -1,18 +1,14 @@
 import aoc_helper
+from functools import reduce
 
 raw = aoc_helper.day(6)
-print(raw)
-
-def parse_raw():
-    ...
-
-data = parse_raw()
+data = raw.split('\n\n')
 
 def part_one():
-    ...
+    return sum(len(set(filter(str.isalpha, group))) for group in data)
 
 def part_two():
-    ...
+    return sum(len(reduce(set.intersection, map(set, group.splitlines()))) for group in data)
 
 aoc_helper.submit(6, part_one)
 aoc_helper.submit(6, part_two)
