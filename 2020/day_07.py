a5a86bbe5f60c248bee3346150e6af12dc199b54
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-07 13:12:32 -0600

    toy solution

--- 2020/day_07.py
+++ 2020/day_07.py
@@ -26,11 +26,36 @@ def has_shiny(bag):
 def count(bag):
     return 1 + sum(n * count(inner) for inner, n in formulas[bag].items())
 
+
+#  How about another stack solution, but one that builds an very long arithmetic expression to eval?!
+# from collections import deque
+#
+# def count(bag):
+#     tokens = deque(formulas[bag].items())
+#     stack = []
+#     while tokens:
+#         current, amt = tokens.popleft()
+#         stack.append(str(amt))
+#         if formulas[current]:
+#             stack.extend("*(")
+#             tokens.appendleft(")")
+#             tokens.extendleft(formulas[current].items())
+#             continue
+
+#         while tokens:
+#             if tokens[0] == ")":
+#                 stack.extend("+1")
+#                 stack.append(tokens.popleft())
+#             else:
+#                 stack.append("+")
+#                 break
+#     return eval("".join(stack))
+
 def part_one():
     return sum(map(has_shiny, formulas))
 
 def part_two():
     return count("shiny gold") - 1  # -1 since we don't count the shiny gold bag itself!
-
+print(part_two())
 aoc_helper.submit(7, part_one)
 aoc_helper.submit(7, part_two)
