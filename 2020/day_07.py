0f04dccf7a4630f5bb1e9246b2acec94fbd1b943
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-07 02:45:30 -0600

    conditional not needed, just use or

--- 2020/day_07.py
+++ 2020/day_07.py
@@ -9,11 +9,9 @@ def parse_raw():
     return {bag: {inner: int(n) for n, inner in formula.findall(contents)} for bag, contents in bags}
 
 formulas = parse_raw()
-GOLD_BAG = "shiny gold"
 
 def has_shiny(bag):
-    if GOLD_BAG in formulas[bag]: return True
-    return any(map(has_shiny, formulas[bag]))
+    return "shiny gold" in formulas[bag] or any(map(has_shiny, formulas[bag]))
 
 def count(bag):
     return 1 + sum(n * count(inner) for inner, n in formulas[bag].items())
@@ -22,7 +20,7 @@ def part_one():
     return sum(map(has_shiny, formulas))
 
 def part_two():
-    return count(GOLD_BAG) - 1  # -1 since we don't count the GOLD_BAG itself!
+    return count("shiny gold") - 1  # -1 since we don't count the GOLD_BAG itself!
 
 aoc_helper.submit(7, part_one)
 aoc_helper.submit(7, part_two)
