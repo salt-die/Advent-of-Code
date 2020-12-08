ed8e95a20c180493eb330b31e3772a202e3f6b09
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-08 00:47:00 -0600

    day 8 done!

--- 2020/day_08.py
+++ 2020/day_08.py
@@ -1,18 +1,24 @@
 import aoc_helper
+import intcode as ic
 
 raw = aoc_helper.day(8)
-print(raw)
 
 def parse_raw():
-    ...
+    out = []
+    for line in raw.splitlines():
+        out.append((line.split()[0], int(line.split()[1])))
+    return out
 
-data = parse_raw()
+boot_program = ic.Computer(parse_raw())
 
 def part_one():
-    ...
+    try:
+        boot_program.run()
+    except ic.CycleError as e:
+        return e.args[0]
 
 def part_two():
-    ...
+    return boot_program.fsck()
 
 aoc_helper.submit(8, part_one)
 aoc_helper.submit(8, part_two)
