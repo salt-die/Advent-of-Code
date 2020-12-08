0a6178b05d5644c884407452bcfca187b883ac88
GlassToeStudio <GlassToeStudio@gmail.com>
2020-12-07 20:39:11 -0600

    Update tree visualization
    usage: python 2020/visuals/trees_day_07.py "shiny gold"

--- 2020/visuals/trees_day_07.py
+++ 2020/visuals/trees_day_07.py
@@ -1,10 +1,13 @@
 import re
+import sys
+
 
 def prefix(first, rest, lines):
     it = iter(lines.splitlines())
     yield first + next(it)
     for line in it:
         yield rest + line
+
 
 class Bag:
     def __init__(self, name, bags):
@@ -19,23 +22,32 @@ class Bag:
         return '\n'.join(
             (
                 self.name,
-                *(line for bag, n in body for line in prefix(' ├─', ' | ', f"{n} {bag}")),
-                *prefix(" ╰─", "   ", f"{last[1]} {last[0]}"),
+                *(line for bag, n in body for line in prefix(' ├─', ' │ ', f"{n} {bag}")),
+                *prefix(" └─", "   ", f"{last[1]} {last[0]}"),
             )
         )
 
-with open("day_07_input.txt") as f:
-    raw = f.read()
 
-def parse_raw():
-    bag_description = r"([a-z]+ [a-z]+) bags contain (.+)"
-    formula = re.compile(r"(\d+) ([a-z]+ [a-z]+) bag")
-    bags = {bag: Bag(bag, contents) for bag, contents in re.findall(bag_description, raw)}
-    for bag in bags.values():
-        bag.bags = {bags[inner]: n for n, inner in formula.findall(bag.bags)}
-    return bags
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        bag_name = sys.argv[1]
+    else:
+        bag_name = 'shiny gold'
 
-bags = parse_raw()
+    with open("2020/visuals/day_07_input.txt") as f:
+        raw = f.read()
 
-def pp(bag):
-    print(bags[bag])
+    def parse_raw():
+        bag_description = r"([a-z]+ [a-z]+) bags contain (.+)"
+        formula = re.compile(r"(\d+) ([a-z]+ [a-z]+) bag")
+        bags = {bag: Bag(bag, contents) for bag, contents in re.findall(bag_description, raw)}
+        for bag in bags.values():
+            bag.bags = {bags[inner]: n for n, inner in formula.findall(bag.bags)}
+        return bags
+
+    bags = parse_raw()
+
+    def pp(bag):
+        print(bags[bag])
+
+    pp(bag_name)
