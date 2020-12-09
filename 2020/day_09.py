33287d4fcc3097365570b9ccdfc89d9957a75ecb
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-08 23:32:24 -0600

    day 9 done!

--- 2020/day_09.py
+++ 2020/day_09.py
@@ -1,18 +1,34 @@
 import aoc_helper
+from itertools import combinations
 
 raw = aoc_helper.day(9)
-print(raw)
 
 def parse_raw():
-    ...
+    return [int(line) for line in raw.splitlines()]
 
 data = parse_raw()
 
 def part_one():
-    ...
+    for i, n in enumerate(data[25:]):
+        if n not in {x + y for x, y in combinations(data[i: i + 25], 2)}:
+            return n
 
 def part_two():
-    ...
+    target = 3199139634
+    i = -1
+    while i < len(data) - 2:
+        i += 1       # start index
+        j = i + 1    # end index
+        n = data[i]  # n is our sum
+        while n < target:
+            n += data[j]
+            if n > target:
+                break
+            elif n == target:
+                r = data[i: j + 1]
+                return max(r) + min(r)
+            j += 1
 
+print(part_two())
 aoc_helper.submit(9, part_one)
 aoc_helper.submit(9, part_two)
