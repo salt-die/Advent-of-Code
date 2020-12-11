88299875bff3b8df4bf7aff7e00ac30565b8b6dc
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-11 02:09:06 -0600

    formatting

--- 2020/day_11.py
+++ 2020/day_11.py
@@ -21,10 +21,12 @@ def part_one():
               [1, 1, 1]]
     last = None
     seats = data
+
     while (as_bytes := seats.tobytes()) != last:
         last = as_bytes
         neighbors = convolve(np.where(floor, 0, seats), KERNEL, mode="constant")
         seats = np.where(floor, FLOOR, np.where(neighbors >= 4, EMPTY, np.where(neighbors == 0, OCCUPIED, seats)))
+
     return (seats == OCCUPIED).sum()
 
 def check_line(y, x, y_step, x_step, seats):
@@ -33,7 +35,7 @@ def check_line(y, x, y_step, x_step, seats):
 
     for i in count(1):
         cell_y, cell_x = y + i * y_step, x + i * x_step
-        if cell_y not in range(0, h) or cell_x not in range(0, w):
+        if cell_y not in range(0, h) or cell_x not in range(0, w):  # Out-of-bounds
             return 0
         if (cell := seats[cell_y, cell_x]) != FLOOR:
             return cell
@@ -43,14 +45,14 @@ def part_two():
     seats = data
     while (as_bytes := seats.tobytes()) != last:
         last = as_bytes
-        neighbors = np.zeros_like(data)
+        neighbors = data.copy()
+
         it = np.nditer(data, flags=["multi_index"])
         for seat in it:
             y, x = it.multi_index
-            if seat == FLOOR:
-                neighbors[y, x] = FLOOR
-            else:
+            if seat != FLOOR:
                 neighbors[y, x] = sum(check_line(y, x, i, j, seats) for i, j in product((-1, 1, 0), repeat=2))
+
         seats = np.where(neighbors >= 5, EMPTY, np.where(neighbors == 0, OCCUPIED, seats))
 
     return (seats == OCCUPIED).sum()
