96ef3ab39cb274fcee14fa3cb0a3fcf1a7128c0d
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-11 06:17:41 -0600

    clean up

--- 2020/day_11.py
+++ 2020/day_11.py
@@ -1,8 +1,9 @@
 from itertools import product, count
+from functools import partial
 
 import aoc_helper
-from scipy.ndimage import convolve
 import numpy as np
+from scipy.ndimage import convolve
 
 raw = aoc_helper.day(11)
 FLOOR, EMPTY, OCCUPIED = -1, 0, 1
@@ -12,7 +13,7 @@ def parse_raw():
     return np.array([[trans[char] for char in line] for line in raw.splitlines()])
 
 data = parse_raw()
-floor = data == -1
+floor, no_floor = data == FLOOR, data != FLOOR
 h, w = data.shape
 
 def part_one():
@@ -29,30 +30,34 @@ def part_one():
 
     return (seats == OCCUPIED).sum()
 
-def check_line(y, x, y_step, x_step, seats):
-    if y_step == x_step == 0:
-        return 0
+@partial(np.vectorize, excluded=[2])
+def seen(y, x, seats):
+    total = 0
+    for y_step, x_step in product((-1, 0, 1), repeat=2):
+        if y_step == x_step == 0:
+            continue
 
-    for i in count(1):
-        cell_y, cell_x = y + i * y_step, x + i * x_step
-        if cell_y not in range(0, h) or cell_x not in range(0, w):  # Out-of-bounds
-            return 0
-        if (cell := seats[cell_y, cell_x]) != FLOOR:
-            return cell
+        for i in count(1):
+            cell_y, cell_x = y + i * y_step, x + i * x_step
+            if cell_y not in range(0, h) or cell_x not in range(0, w):  # Out-of-bounds
+                break
+            if (cell := seats[cell_y, cell_x]) != FLOOR:
+                total += cell
+                break
+
+        if total == 5:
+            return total
+
+    return total
 
 def part_two():
     last = None
-    seats = data
+    seats = neighbors = data.copy()
+    ys, xs = np.mgrid[:h, :w]
+    ys, xs = ys[no_floor], xs[no_floor]
     while (as_bytes := seats.tobytes()) != last:
         last = as_bytes
-        neighbors = data.copy()
-
-        it = np.nditer(data, flags=["multi_index"])
-        for seat in it:
-            y, x = it.multi_index
-            if seat != FLOOR:
-                neighbors[y, x] = sum(check_line(y, x, i, j, seats) for i, j in product((-1, 1, 0), repeat=2))
-
+        neighbors[no_floor] = seen(ys, xs, seats)
         seats = np.where(neighbors >= 5, EMPTY, np.where(neighbors == 0, OCCUPIED, seats))
 
     return (seats == OCCUPIED).sum()
