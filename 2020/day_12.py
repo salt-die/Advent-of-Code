cdc708ac6448d922b1c9d68670216a4767fe0d5b
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-12 07:26:42 -0600

    day 12 done!

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -1,18 +1,40 @@
 import aoc_helper
+import re
 
 raw = aoc_helper.day(12)
-print(raw)
 
 def parse_raw():
-    ...
+    pattern = re.compile(r"([NEWSLRF])(\d+)")
+    return [(dir_, int(val)) for dir_, val in pattern.findall(raw)]
 
 data = parse_raw()
 
+cards = dict(zip("NEWS", (1j, 1, -1, -1j)))
+turns = dict(zip("LR", (1j, -1j)))
+
 def part_one():
-    ...
+    facing = 1 + 0j
+    loc = 0 + 0j
+    for dir_, val in data:
+        if dir_ in cards:
+            loc += cards[dir_] * val
+        elif dir_ in turns:
+            facing *= turns[dir_] ** (val // 90)
+        elif dir_ == "F":
+            loc += facing * val
+    return round(abs(loc.real) + abs(loc.imag))
 
 def part_two():
-    ...
+    wp = 10 + 1j
+    loc = 0 + 0j
+    for dir_, val in data:
+        if dir_ in cards:
+            wp += cards[dir_] * val
+        elif dir_ in turns:
+            wp *= turns[dir_] ** (val // 90)
+        elif dir_ == "F":
+            loc += wp * val
+    return round(abs(loc.real) + abs(loc.imag))
 
 aoc_helper.submit(12, part_one)
 aoc_helper.submit(12, part_two)
