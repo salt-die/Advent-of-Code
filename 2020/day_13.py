61ddd3a0ae5bd419843f4012d8a24b9418a1fd24
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-13 07:13:58 -0600

    day 13 done!

--- 2020/day_13.py
+++ 2020/day_13.py
@@ -1,18 +1,26 @@
 import aoc_helper
+from sympy.ntheory.modular import crt
 
 raw = aoc_helper.day(13)
-print(raw)
 
 def parse_raw():
-    ...
+    departure_time, ids = raw.splitlines()
+    moduli = []
+    residues = []
+    for residue, modulus in enumerate(ids.split(",")):
+        if modulus.isdigit():
+            moduli.append(int(modulus))
+            residues.append(-residue)
+    return int(departure_time), moduli, residues
 
-data = parse_raw()
+departure, moduli, residues = parse_raw()
 
 def part_one():
-    ...
+    bus_id = min(moduli, key=lambda bus_id: -departure % bus_id)
+    return (-departure % bus_id) * bus_id
 
 def part_two():
-    ...
+    return crt(moduli, residues)[0]  # Chinese remainder theorem
 
 aoc_helper.submit(13, part_one)
 aoc_helper.submit(13, part_two)
