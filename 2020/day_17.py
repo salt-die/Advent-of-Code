7c308bf6bff53d14302991ed8a80fb477f41af77
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-16 23:42:51 -0600

    generalize

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -5,29 +5,23 @@ from scipy.ndimage import convolve
 raw = aoc_helper.day(17)
 data = np.array([[char=="#" for char in line] for line in raw.splitlines()])
 
-def part_one():
-    KERNEL = np.ones((3, 3, 3))
-    KERNEL[1, 1, 1] = 0
+def convolve_dimension_(n):
+    KERNEL = np.ones(tuple(3 for _ in range(n)))
+    KERNEL[tuple(1 for _ in range(n))] = 0
 
-    universe = np.zeros((13, 20, 20), dtype=int)
-    universe[6] = np.pad(data, 6)
+    universe = np.zeros((*(13 for _ in range(n - 2)), 20, 20), dtype=int)
+    universe[tuple(6 for _ in range(n - 2))] = np.pad(data, 6)
 
     for _ in range(6):
         neighbors = convolve(universe, KERNEL, mode="constant")
         universe = np.where((neighbors == 3) | (universe & (neighbors==2)), 1, 0)
     return universe.sum()
+
+def part_one():
+    return convolve_dimension_(3)
 
 def part_two():
-    KERNEL = np.ones((3, 3, 3, 3))
-    KERNEL[1, 1, 1, 1] = 0
-
-    universe = np.zeros((13, 13, 20, 20), dtype=int)
-    universe[6, 6] = np.pad(data, 6)
-
-    for _ in range(6):
-        neighbors = convolve(universe, KERNEL, mode="constant")
-        universe = np.where((neighbors == 3) | (universe & (neighbors==2)), 1, 0)
-    return universe.sum()
+    return convolve_dimension_(4)
 
 aoc_helper.submit(17, part_one)
 aoc_helper.submit(17, part_two)
