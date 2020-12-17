0bc36c164e44093efde810114a16e2188b6cfb68
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-17 01:20:12 -0600

    use signal.convolve to automatically adjust size of convolved array

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -1,20 +1,20 @@
 import aoc_helper
 import numpy as np
-from scipy.ndimage import convolve
+from scipy.signal import convolve
 
 raw = aoc_helper.day(17)
 data = np.array([[char=="#" for char in line] for line in raw.splitlines()])
 
 def convolve_dimension_(n):
-    KERNEL = np.ones(tuple(3 for _ in range(n)))
+    KERNEL = np.ones(tuple(3 for _ in range(n)), dtype=int)
     KERNEL[tuple(1 for _ in range(n))] = 0
 
-    universe = np.zeros((*(13 for _ in range(n - 2)), 20, 20), dtype=int)
-    universe[tuple(6 for _ in range(n - 2))] = np.pad(data, 6)
+    universe = np.zeros((*(1 for _ in range(n - 2)), *data.shape), dtype=int)
+    universe[...,:] = data
 
     for _ in range(6):
-        neighbors = convolve(universe, KERNEL, mode="constant")
-        universe = np.where((neighbors == 3) | (universe & (neighbors==2)), 1, 0)
+        neighbors = convolve(universe, KERNEL)
+        universe = np.where((neighbors == 3) | (np.pad(universe, 1) & (neighbors==2)), 1, 0)
     return universe.sum()
 
 def part_one():
