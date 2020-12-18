605ff0e736e1648bd49b282bb8a86825296221d9
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-17 23:45:06 -0600

    day 18 done!

--- 2020/day_18.py
+++ 2020/day_18.py
@@ -1,18 +1,93 @@
+from itertools import tee
+
 import aoc_helper
+from more_itertools import unzip
+from sly import Lexer, Parser
 
+
+class MathLexer(Lexer):
+    tokens = {NUMBER}
+    ignore = ' '
+    literals = {'+', '*', '(', ')'}
+
+    NUMBER = r'\d+'
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+
+class MathParser(Parser):
+    tokens = MathLexer.tokens
+
+    precedence = (
+        ('left', '+', '*'),
+        ('left', '(', ')'),
+    )
+
+    @_('expr')
+    def statement(self, p):
+        return p.expr
+
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return p.expr
+
+    @_('expr "+" expr')
+    def expr(self, p):
+        return p.expr0 + p.expr1
+
+    @_('expr "*" expr')
+    def expr(self, p):
+        return p.expr0 * p.expr1
+
+    @_('NUMBER')
+    def expr(self, p):
+        return p.NUMBER
+
+
+class AdvancedMathParser(Parser):
+    tokens = MathLexer.tokens
+
+    precedence = (
+        ('left', '*'),
+        ('left', '+'),
+        ('left', '(', ')'),
+    )
+
+    @_('expr')
+    def statement(self, p):
+        return p.expr
+
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return p.expr
+
+    @_('expr "+" expr')
+    def expr(self, p):
+        return p.expr0 + p.expr1
+
+    @_('expr "*" expr')
+    def expr(self, p):
+        return p.expr0 * p.expr1
+
+    @_('NUMBER')
+    def expr(self, p):
+        return p.NUMBER
+
+
+lexer = MathLexer()
 raw = aoc_helper.day(18)
-print(raw)
-
-def parse_raw():
-    ...
-
-data = parse_raw()
+tokens_a, tokens_b = unzip(tee(lexer.tokenize(line)) for line in raw.splitlines())
 
 def part_one():
-    ...
+    parser = MathParser()
+    return sum(map(parser.parse, tokens_a))
 
 def part_two():
-    ...
+    parser = AdvancedMathParser()
+    return sum(map(parser.parse, tokens_b))
 
 aoc_helper.submit(18, part_one)
 aoc_helper.submit(18, part_two)
