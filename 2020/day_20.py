97f662d300dbb1faf6305a822d1a5366d009a0de
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-20 05:37:40 -0600

    day 20 done!

--- 2020/day_20.py
+++ 2020/day_20.py
@@ -1,18 +1,84 @@
+from collections import defaultdict
+from itertools import product
+from math import prod
+
 import aoc_helper
+import networkx as nx
+import numpy as np
+from scipy.ndimage import convolve
 
 raw = aoc_helper.day(20)
-print(raw)
 
 def parse_raw():
-    ...
+    tiles = {}
+    for tile in raw.split("\n\n"):
+        it = iter(tile.splitlines())
+        number = int(next(it)[5:-1])
+        tiles[number] = np.array([[char == "#" for char in line ] for line in it])
+    return tiles
 
-data = parse_raw()
+tiles = parse_raw()
+
+def borders(tile):
+    for i in (0, (..., -1), -1, (..., 0)):
+        yield min(tile[i].tobytes(), tile[i][::-1].tobytes())
+
+border_to_tiles = defaultdict(list)  # Build a network of connected tiles from this
 
 def part_one():
-    ...
+    # Build dict of connections between tiles
+    for n, tile in tiles.items():
+        for border in borders(tile):
+            border_to_tiles[border].append(n)
+
+    return prod(n for n, tile in tiles.items() if sum(len(border_to_tiles[border]) for border in borders(tile)) == 6)
+
+def orientations(tile):
+    for _ in range(4):
+        tile = np.rot90(tile)
+        yield tile
+        yield tile.T
 
 def part_two():
-    ...
+    G = nx.Graph(edge for edge in border_to_tiles.values() if len(edge) == 2)
+
+    # Get a corner
+    for node in G:
+        if len(G[node]) == 2:
+            break
+    # Orient the corner
+    for tile in orientations(tiles[node]):
+        if [len(border_to_tiles[border]) for border in borders(tile)] == [1, 2, 2, 1]:
+            del tiles[node]
+            break
+
+    grid = {}
+    grid[0, 0] = node, tile
+    # Fill in the rest of the grid by correctly orienting the neighbors
+    for y, x in product(range(12), repeat=2):
+        node, tile = grid[y, x]
+        for neighbor in G[node]:
+            if neighbor not in tiles:
+                continue
+            for neighbor_tile in orientations(tiles[neighbor]):
+                if np.array_equal(tile[-1], neighbor_tile[0]):
+                    grid[y + 1, x] = neighbor, neighbor_tile
+                    break
+                if np.array_equal(tile[:, -1], neighbor_tile[:, 0]):
+                    grid[y, x + 1] = neighbor, neighbor_tile
+                    break
+            del tiles[neighbor]
+
+    image = np.block([[grid[y, x][1][1: -1, 1: -1] for x in range(12)] for y in range(12)])
+    lochness = np.array([[char == "#" for char in line] for line in
+        "                  #  \n"
+        "#    ##    ##    ### \n"
+        " #  #  #  #  #  #    \n".splitlines()
+    ])
+    for lochness in orientations(lochness):
+        if count := (convolve(image, lochness, output=int, mode="constant") == lochness.sum()).sum():
+            return image.sum() - count * lochness.sum()
 
 aoc_helper.submit(20, part_one)
+part_one() # Must be run for part two to work.
 aoc_helper.submit(20, part_two)
