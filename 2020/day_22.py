89c94e91edf6130076b7728a88615c0df47934cb
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-22 02:41:50 -0600

    more trimming

--- 2020/day_22.py
+++ 2020/day_22.py
@@ -10,17 +10,12 @@ enum = np.arange(len(alice) + len(bob), 0, -1)
 
 def game(alice=alice, bob=bob, recurse=True):
     seen = set()
-    while alice and bob:
-        if (s := (tuple(alice), tuple(bob))) in seen:
-            return True
+    while alice and bob and (s := (tuple(alice), tuple(bob))) not in seen:
         seen.add(s)
 
         a, b = alice.popleft(), bob.popleft()
-        if recurse and a <= len(alice) and b <= len(bob):
-            sub_alice, sub_bob = deque(islice(alice, a)), deque(islice(bob, b))
-            alice.extend((a, b)) if game(sub_alice, sub_bob) else bob.extend((b, a))
-        else:
-            alice.extend((a, b)) if a > b else bob.extend((b, a))
+        alice_wins = game(deque(islice(alice, a)), deque(islice(bob, b))) if recurse and a <= len(alice) and b <= len(bob) else a > b
+        alice.extend((a, b)) if alice_wins else bob.extend((b, a))
 
     return bool(alice)
 
