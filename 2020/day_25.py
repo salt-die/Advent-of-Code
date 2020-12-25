67ccf2985f7f2b9011a01e71a360863a31edb5db
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-24 23:36:51 -0600

    trimming

--- 2020/day_25.py
+++ 2020/day_25.py
@@ -2,14 +2,13 @@ import aoc_helper
 
 raw = aoc_helper.day(25)
 card_key, door_key = aoc_helper.extract_ints(raw)
+MOD = 20201227
 
 def part_one():
-    for n in range(1, 20201227):
-        s = pow(7, n, 20201227)
-
-        if  s == card_key:
-            return pow(door_key, n, 20201227)
+    for n in range(1, MOD):
+        if  (s := pow(7, n, MOD)) == card_key:
+            return pow(door_key, n, MOD)
         elif s == door_key:
-            return pow(card_key, n, 20201227)
+            return pow(card_key, n, MOD)
 
 aoc_helper.submit(25, part_one)
