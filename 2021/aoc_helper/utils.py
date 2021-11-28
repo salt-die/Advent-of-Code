5a4774a170be63173d6628bd91a71a2bce916e57
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-28 17:54:25 -0600

    Utils moved to separate file. Direction enum and crt added.

---
+++ 2021/aoc_helper/utils.py
@@ -0,0 +1,88 @@
+"""
+Useful functions for AoC. Note deferred imports.
+
+Requirements:
+    * networkx
+    * numpy
+"""
+
+__all__ = (
+    "extract_ints",
+    "extract_maze",
+    "maximum_matching",
+    "get_direction_enum",
+    "chinese_remainder_theorem",
+)
+
+def extract_ints(raw: str):
+    """
+    Extract integers from a string.
+    """
+    import re
+
+    for match in re.findall(r'(\d+)', raw):
+        yield int(match)
+
+def extract_maze(raw: str, empty_cell=".", largest_component=False):
+    """
+    Parse an ascii maze into a networkx graph. Return a tuple `(np.array, nx.Graph)`.
+    """
+    import numpy as np
+    import networkx as nx
+
+    maze = np.array(
+        [[*line] for line in raw.splitlines()]
+    )
+
+    G = nx.grid_graph(maze.shape[::-1])
+
+    walls = np.stack(np.where(maze != empty_cell)).T
+    G.remove_nodes_from(map(tuple, walls))
+
+    if largest_component:
+        G.remove_nodes_from(G.nodes - max(nx.connected_components(G), key=lambda g: len(g)))
+
+    return maze, G
+
+def maximum_matching(items: dict[list]):
+    """
+    Return a maximum matching from a dict of lists.
+    """
+    import networkx as nx
+
+    G = nx.from_dict_of_lists(items)
+
+    for k, v in nx.bipartite.maximum_matching(G, top_nodes=items).items():
+        if k in items:  # Filter edges pointing the wrong direction.
+            yield k, v
+
+def get_direction_enum():
+    """
+    Return an enum for Directions with a rotate method.
+    """
+    from enum import IntFlag
+
+    class Direction(IntFlag):
+        EAST  = E = 0
+        NORTH = N = 1
+        WEST  = W = 2
+        SOUTH = S = 3
+
+        def rotate(self, steps=1, clockwise=True):
+            if clockwise:
+                return Direction((self - steps) % 4)
+            return Directions((self + steps) % 4)
+
+    return Direction
+
+def chinese_remainder_theorem(moduli, residues):
+    from math import prod
+
+    N = prod(moduli)
+
+    X = sum(
+        (div := (N // modulus)) * pow(div, -1, modulus) * residue
+        for modulus, residue in zip(moduli, residues)
+    )
+
+    return X % N
