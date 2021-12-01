c1cc6ae36a03f466fa60386229f72bc31b8ef9a2
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-01 00:25:28 -0600

    Simplification.

--- 2021/day_01.py
+++ 2021/day_01.py
@@ -1,14 +1,16 @@
 import aoc_helper
 
-raw = aoc_helper.day(1)
+RAW  = aoc_helper.day(1)
+DATA = list(aoc_helper.utils.extract_ints(RAW))
 
-data = list(aoc_helper.utils.extract_ints(raw))
+def compare(n):
+    return sum(x < y for x, y in zip(DATA, DATA[n:]))
 
 def part_one():
-    return sum(x < y for x, y in zip(data, data[1:]))
+    return compare(1)
 
 def part_two():
-    return sum(x < y for x, y in zip(data, data[3:]))
+    return compare(3)
 
 aoc_helper.submit(1, part_one)
 aoc_helper.submit(1, part_two)
