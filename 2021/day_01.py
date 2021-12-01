409ab6a6983de4e6b6145846f63c800be630627e
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-30 23:34:40 -0600

    Day 1 complete!

--- 2021/day_01.py
+++ 2021/day_01.py
@@ -1,18 +1,14 @@
 import aoc_helper
 
 raw = aoc_helper.day(1)
-print(raw)
 
-def parse_raw():
-    ...
-
-data = parse_raw()
+data = list(aoc_helper.utils.extract_ints(raw))
 
 def part_one():
-    ...
+    return sum(x < y for x, y in zip(data, data[1:]))
 
 def part_two():
-    ...
+    return sum(x < y for x, y in zip(data, data[3:]))
 
 aoc_helper.submit(1, part_one)
 aoc_helper.submit(1, part_two)
