731b7466be789e3474620c84d1c779d6240d45af
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-01 23:23:17 -0600

    Day 2 done!

--- 2021/day_02.py
+++ 2021/day_02.py
@@ -1,18 +1,41 @@
+import re
+
 import aoc_helper
 
 RAW = aoc_helper.day(2)
-print(RAW)
-
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+DATA = [
+    (direction, int(amount))
+    for direction, amount in re.findall(r"(\w+) (\d+)", RAW)
+]
 
 def part_one():
-    ...
+    depth = x = 0
+
+    for direction, amount in DATA:
+        match direction:
+            case "forward":
+                x += amount
+            case "down":
+                depth += amount
+            case "up":
+                depth -= amount
+
+    return depth * x
 
 def part_two():
-    ...
+    depth = x = aim = 0
+
+    for direction, amount in DATA:
+        match direction:
+            case "forward":
+                x += amount
+                depth += aim * amount
+            case "down":
+                aim += amount
+            case "up":
+                aim -= amount
+
+    return depth * x
 
 aoc_helper.submit(2, part_one)
 aoc_helper.submit(2, part_two)
