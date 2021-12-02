e0ac7c66ee4fd48791731c8204713d33c41a2770
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-02 14:39:22 -0600

    Solution uses utility function.

--- 2021/day_01.py
+++ 2021/day_01.py
@@ -1,10 +1,11 @@
 import aoc_helper
+from aoc_helper.utils import pairwise
 
 RAW  = aoc_helper.day(1)
 DATA = list(aoc_helper.utils.extract_ints(RAW))
 
 def compare(n):
-    return sum(x < y for x, y in zip(DATA, DATA[n:]))
+    return sum(x < y for x, y in pairwise(DATA, offset=n))
 
 def part_one():
     return compare(1)
