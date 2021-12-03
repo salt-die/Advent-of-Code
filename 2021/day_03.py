6c2746a0fe0f6693115e854fea478e066e2e6f31
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-03 01:59:01 -0600

    Day 3 done!

--- 2021/day_03.py
+++ 2021/day_03.py
@@ -1,18 +1,37 @@
+import numpy as np
+
 import aoc_helper
 
 RAW = aoc_helper.day(3)
-print(RAW)
+DATA = np.array([list(map(int, line)) for line in RAW.splitlines()])
+h, w = DATA.shape
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+BIN_POWERS = np.geomspace(2**(w - 1), 1, w, dtype=int)
 
 def part_one():
-    ...
+    gamma_digits = DATA.sum(axis=0) > h >> 1
+
+    gamma = gamma_digits @ BIN_POWERS
+    episilon = ~gamma_digits @ BIN_POWERS
+
+    return gamma * episilon
 
 def part_two():
-    ...
+    oxy_rating = co2_rating = DATA
+
+    for i in range(w):
+        if half_sum := len(oxy_rating) >> 1:
+            mask = oxy_rating[:, i]
+            oxy_rating = oxy_rating[mask == (mask.sum() >= half_sum)]
+
+        if half_sum := len(co2_rating) >> 1:
+            mask = co2_rating[:, i] == 0
+            co2_rating = co2_rating[mask == (mask.sum() <= half_sum)]
+
+    oxy_rating = oxy_rating[0] @ BIN_POWERS
+    co2_rating = co2_rating[0] @ BIN_POWERS
+
+    return oxy_rating * co2_rating
 
 aoc_helper.submit(3, part_one)
 aoc_helper.submit(3, part_two)
