0d93aae7dcae3ef73cb73529879b8615a8d572bf
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-04 05:02:07 -0600

    Newlines added to initial input and submission files.

--- 2021/aoc_helper/setup.py
+++ 2021/aoc_helper/setup.py
@@ -4,10 +4,10 @@ Run once to set up AoC directory.
 from constants import *
 
 if not INPUTS_FILE.exists():
-    INPUTS_FILE.write_text("{}")
+    INPUTS_FILE.write_text("{}\n")
 
 if not SUBMISSIONS_FILE.exists():
-    SUBMISSIONS_FILE.write_text("{}")
+    SUBMISSIONS_FILE.write_text("{}\n")
 
 template = TEMPLATE_FILE.read_text()
 
