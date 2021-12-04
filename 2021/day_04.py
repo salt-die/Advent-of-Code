86a1de6d7a02b19a2a76e98120b5c353c4d5338d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-04 02:56:31 -0600

    Removed inner loops.

--- 2021/day_04.py
+++ 2021/day_04.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import numpy as np
 
 import aoc_helper
@@ -6,13 +7,11 @@ from aoc_helper.utils import extract_ints
 RAW = aoc_helper.day(4)
 
 def parse_raw():
-    numbers, *boards = RAW.split("\n\n")
+    numbers, boards = RAW.split("\n\n", 1)
 
     return (
         tuple(extract_ints(numbers)),
-        np.array(
-            [np.fromiter(extract_ints(board), dtype=int) for board in boards]
-        ).reshape(-1, 5, 5),
+        np.loadtxt(StringIO(boards), dtype=int).reshape(-1, 5, 5),
     )
 
 NUMBERS, BOARDS = parse_raw()
@@ -23,14 +22,12 @@ def part_one():
     for number in NUMBERS:
         boards[boards == number] = -1
 
-        for i in range(10):
-            winners = np.all(boards[:, i//2] == -1, axis=1)
+        mask = boards == -1
+        winners = (mask.all(1) | mask.all(2)).any(1)
 
-            if winners.any():
-                boards[boards == -1] = 0
-                return boards[winners.argmax()].sum() * number
-
-            boards = np.swapaxes(boards, 1, 2)
+        if winners.any():
+            boards[boards == -1] = 0
+            return boards[winners].sum() * number
 
 def part_two():
     boards = BOARDS.copy()
@@ -38,16 +35,14 @@ def part_two():
     for number in NUMBERS:
         boards[boards == number] = -1
 
-        for i in range(10):
-            winners = np.all(boards[:, i//2] == -1, axis=1)
+        mask = boards == -1
+        winners = (mask.all(1) | mask.all(2)).any(1)
 
-            if len(boards) > 1:
-                boards = boards[~winners]
-            elif winners[0]:
-                boards[boards == -1] = 0
-                return boards.sum() * number
-
-            boards = np.swapaxes(boards, 1, 2)
+        if len(boards) > 1:
+            boards = boards[~winners]
+        elif winners[0]:
+            boards[boards == -1] = 0
+            return boards.sum() * number
 
 aoc_helper.submit(4, part_one)
 aoc_helper.submit(4, part_two)
