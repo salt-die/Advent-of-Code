19b859e299bc9e12df4f1fa583a97acf09841bbe
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-04 21:18:31 -0600

    Refactor.

--- 2021/day_04.py
+++ 2021/day_04.py
@@ -1,48 +1,45 @@
-from io import StringIO
-
 import numpy as np
 
 import aoc_helper
+from aoc_helper.utils import extract_ints
 
 RAW = aoc_helper.day(4)
 
 def parse_raw():
-    numbers, boards = RAW.split("\n\n", 1)
+    draws, boards = RAW.split("\n\n", 1)
 
     return (
-        tuple(aoc_helper.utils.extract_ints(numbers)),
-        np.loadtxt(StringIO(boards), dtype=int).reshape(-1, 5, 5),
+        np.fromiter(extract_ints(draws), dtype=int),
+        np.fromiter(extract_ints(boards), dtype=int).reshape(-1, 5, 5),
     )
 
-NUMBERS, BOARDS = parse_raw()
+DRAWS, BOARDS = parse_raw()
+
+def score_ith_winner(i):
+    """
+    Return the score of the ith winner.
+    """
+    draw_indices = DRAWS.argsort()[BOARDS]
+
+    win_turns = np.minimum(
+        draw_indices.max(1).min(1),  # min win turn for columns
+        draw_indices.max(2).min(1),  # min win turn for rows
+    )
+
+    ith_win_index = win_turns.argsort()[i]
+
+    ith_win_turn = win_turns[ith_win_index]
+    ith_win_board = BOARDS[ith_win_index]
+
+    ith_win_board[np.isin(ith_win_board, DRAWS[:ith_win_turn + 1])] = 0
+
+    return ith_win_board.sum() * DRAWS[ith_win_turn]
 
 def part_one():
-    boards = BOARDS.copy()
-
-    for number in NUMBERS:
-        boards[boards == number] = -1
-
-        mask = boards == -1
-        winners = (mask.all(1) | mask.all(2)).any(1)
-
-        if winners.any():
-            boards[boards == -1] = 0
-            return boards[winners].sum() * number
+    return score_ith_winner(0)
 
 def part_two():
-    boards = BOARDS.copy()
-
-    for number in NUMBERS:
-        boards[boards == number] = -1
-
-        mask = boards == -1
-        winners = (mask.all(1) | mask.all(2)).any(1)
-
-        if len(boards) > 1:
-            boards = boards[~winners]
-        elif winners[0]:
-            boards[boards == -1] = 0
-            return boards.sum() * number
+    return score_ith_winner(-1)
 
 aoc_helper.submit(4, part_one)
 aoc_helper.submit(4, part_two)
