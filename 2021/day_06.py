224423d5699ac08bc2db41d147f922f51f0b1134
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-05 23:49:38 -0600

    Day 6 done!

--- 2021/day_06.py
+++ 2021/day_06.py
@@ -1,18 +1,29 @@
+import numpy as np
+
 import aoc_helper
 
 RAW = aoc_helper.day(6)
-print(RAW)
 
-def parse_raw():
-    ...
+FISH = np.full(9, 0, dtype=object)
 
-DATA = parse_raw()
+for n in aoc_helper.utils.extract_ints(RAW):
+    FISH[n] += 1
+
+def nfish(steps):
+    all_fish = FISH.copy()
+
+    for i in range(steps):
+        all_fish[(i + 7) % 9] += all_fish[i % 9]
+
+    return all_fish.sum()
 
 def part_one():
-    ...
+    return nfish(80)
 
 def part_two():
-    ...
+    return nfish(256)
 
 aoc_helper.submit(6, part_one)
 aoc_helper.submit(6, part_two)
+
+print(part_one(), part_two())
