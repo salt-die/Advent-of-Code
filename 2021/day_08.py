6372cf80c9f5a6aa79cc5a6fd3718abff7d62f1f
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-08 03:26:42 -0600

    Refactor.

--- 2021/day_08.py
+++ 2021/day_08.py
@@ -1,55 +1,48 @@
+import re
+from itertools import chain
+
 import aoc_helper
 
 RAW = aoc_helper.day(8)
+DIGIT_RE = re.compile(r"[a-g]+")
+DATA = [DIGIT_RE.findall(line) for line in RAW.splitlines()]
+DECODE = {
+    (6, 2, 3): "0",
+    (2, 2, 2): "1",
+    (5, 1, 2): "2",
+    (5, 2, 3): "3",
+    (4, 2, 4): "4",
+    (5, 1, 3): "5",
+    (6, 1, 3): "6",
+    (3, 2, 2): "7",
+    (7, 2, 4): "8",
+    (6, 2, 4): "9",
+}
 
-DATA = [
-    [patterns.split() for patterns in line.split("|")]
-    for line in RAW.splitlines()
-]
+def decode():
+    for patterns in DATA:
+        one, _, four, *_ = sorted(patterns[:-4], key=len)
+        one = set(one)
+        four = set(four)
+
+        yield "".join(
+            DECODE[
+                len(digit),
+                len(one.intersection(digit)),
+                len(four.intersection(digit)),
+            ]
+            for digit in patterns[-4:]
+        )
 
 def part_one():
     return sum(
-        sum(len(digit) in {2, 3, 4, 7} for digit in output_digits)
-        for _, output_digits in DATA
-    )
-
-DECODE = {
-    (6, 2, 3): 0,
-    (2, 2, 2): 1,
-    (5, 1, 2): 2,
-    (5, 2, 3): 3,
-    (4, 2, 4): 4,
-    (5, 1, 3): 5,
-    (6, 1, 3): 6,
-    (3, 2, 2): 7,
-    (7, 2, 4): 8,
-    (6, 2, 4): 9,
-}
-
-def decode(data):
-    signal_patterns, output_digits = data
-
-    one = four = None
-    for pattern in signal_patterns:
-        if len(pattern) == 2:
-            one = set(pattern)
-        elif len(pattern) == 4:
-            four = set(pattern)
-
-        if one and four:
-            break
-
-    return sum(
-        10**(3 - i) * DECODE[
-            len(digit),
-            len(one.intersection(digit)),
-            len(four.intersection(digit)),
-        ]
-        for i, digit in enumerate(output_digits)
+        digit in {"1", "4", "7", "8"}
+        for output in decode()
+        for digit in output
     )
 
 def part_two():
-    return sum(map(decode, DATA))
+    return sum(map(int, decode()))
 
 aoc_helper.submit(8, part_one)
 aoc_helper.submit(8, part_two)
