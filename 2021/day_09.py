19713318cbaf0ce2be2d9dfcaa7ac050f6dd1e9d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-09 01:18:39 -0600

    Refactor.

--- 2021/day_09.py
+++ 2021/day_09.py
@@ -1,8 +1,5 @@
 import re
-from heapq import nlargest
-from math import prod
 
-import cv2
 import numpy as np
 from scipy.ndimage import label
 
@@ -11,23 +8,23 @@ import aoc_helper
 RAW = aoc_helper.day(9)
 
 CAVE_MAP = np.fromiter(map(int, re.findall(r"\d", RAW)), dtype=int).reshape(100, 100)
-BORDER_MAP = cv2.copyMakeBorder(CAVE_MAP, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=9)
 
 def part_one():
+    border_map = np.pad(CAVE_MAP, 1, mode="constant", constant_values=9)
+
     mask = (
-          (CAVE_MAP < BORDER_MAP[2:   , 1: -1])
-        & (CAVE_MAP < BORDER_MAP[ : -2, 1: -1])
-        & (CAVE_MAP < BORDER_MAP[1: -1, 2:   ])
-        & (CAVE_MAP < BORDER_MAP[1: -1,  : -2])
+          (CAVE_MAP < border_map[2:   , 1: -1])
+        & (CAVE_MAP < border_map[ : -2, 1: -1])
+        & (CAVE_MAP < border_map[1: -1, 2:   ])
+        & (CAVE_MAP < border_map[1: -1,  : -2])
     )
     return (CAVE_MAP[mask] + 1).sum()
 
 def part_two():
-    labels, nbins = label(CAVE_MAP != 9)
+    labels, nlabels = label(CAVE_MAP != 9)
+    labels = labels.reshape(-1)
 
-    bin_sizes = ((labels == i).sum() for i in range(1, nbins + 1))
-
-    return prod(nlargest(3, bin_sizes))
+    return np.partition(np.bincount(labels, labels != 0), nlabels - 3)[-3:].prod().astype(int)
 
 aoc_helper.submit(9, part_one)
 aoc_helper.submit(9, part_two)
