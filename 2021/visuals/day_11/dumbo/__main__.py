0dea398ffe752a4d524c2ca287df2d9e7318a9c5
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-11 07:00:30 -0600

    Colors updated on sliders. Extra widget to hold sliders and labels added.

--- 2021/visuals/day_11/dumbo/__main__.py
+++ 2021/visuals/day_11/dumbo/__main__.py
@@ -1,30 +1,44 @@
 from nurses_2.app import App
 from nurses_2.colors import color_pair, WHITE, BLACK
-from nurses_2.widgets.behaviors import Anchor
+from nurses_2.widgets.behaviors import Anchor, AutoPositionBehavior
 from nurses_2.widgets.slider import Slider
 from nurses_2.widgets import Widget
 
 from .automata import Automata, BLUISH
 
 WHITE_ON_BLUISH = color_pair(WHITE, BLUISH)
+BLACK_ON_BLUISH = color_pair(BLACK, BLUISH)
+
+
+class AutoPositionWidget(AutoPositionBehavior, Widget):
+    ...
 
 
 class Dumbo(App):
     async def on_start(self):
         automata = Automata(pos_hint=(.5, .5), anchor=Anchor.CENTER)
 
-        label = Widget((2, 11), default_color_pair=WHITE_ON_BLUISH)
+        container = AutoPositionWidget(
+            size=(4, 63),
+            pos_hint=(1.0, .5),
+            default_color_pair=WHITE_ON_BLUISH,
+            anchor=Anchor.BOTTOM_CENTER,
+        )
+        container.add_border()
+
+        label = Widget(size=(2, 11), pos=(1, 1), default_color_pair=WHITE_ON_BLUISH)
         label.add_text("STATES:")
         label.add_text("ENERGY:", row=1)
 
         nstates = Slider(
             width=50,
-            pos=(0, 11),
+            pos=(1, 12),
             min=5,
             max=100,
             proportion=.06,
-            fill_color=BLACK,
-            default_color_pair=WHITE_ON_BLUISH,
+            fill_color=WHITE,
+            default_color_pair=BLACK_ON_BLUISH,
+            handle_color=WHITE,
             callback=lambda n: (
                 setattr(automata, "nstates", int(n)),
                 label.add_text(f"{int(n):>3}", 0, 8)
@@ -33,24 +47,22 @@ class Dumbo(App):
 
         energy = Slider(
             width=50,
-            pos=(1, 11),
+            pos=(2, 12),
             min=-24,
             max=25,
             proportion=.52,
-            fill_color=BLACK,
-            default_color_pair=WHITE_ON_BLUISH,
+            fill_color=WHITE,
+            default_color_pair=BLACK_ON_BLUISH,
+            handle_color=WHITE,
             callback=lambda n: (
                 setattr(automata, "energy", int(n)),
                 label.add_text(f"{int(n):>3}", 1, 8),
             ),
         )
 
-        self.root.add_widgets(
-            automata,
-            label,
-            nstates,
-            energy,
-        )
+        container.add_widgets(label, nstates, energy)
+
+        self.root.add_widgets(automata, container)
 
 
 Dumbo().run()
