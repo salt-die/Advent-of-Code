4fe0888bfaf1b7d4353b79f865034f2ba0189ba6
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-11 01:32:38 -0600

    Using convolutions.

--- 2021/day_11.py
+++ 2021/day_11.py
@@ -1,47 +1,38 @@
 from itertools import count
 
 import numpy as np
+from scipy.ndimage import convolve
 
 import aoc_helper
 
 OCTOPI = aoc_helper.utils.int_grid(aoc_helper.day(11))
 
-KERNEL = np.array([
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-])
+KERNEL = np.ones((3, 3), dtype=int)
 
-def step(padded_octos):
-    octos = padded_octos[1: -1, 1: -1]
-
+def step(octos):
     octos += 1
+
     flashed = np.zeros_like(octos, dtype=bool)
-    while ((flashing := octos > 9) != flashed).any():
-        new_flashes = np.argwhere(flashing & ~flashed)
-
-        for y, x in new_flashes:
-            padded_octos[y: y + 3, x: x + 3] += KERNEL
-
+    while (flashing := ((octos > 9) & ~flashed)).any():
+        octos += convolve(flashing.astype(int), KERNEL, mode="constant")
         flashed |= flashing
 
     octos[flashed] = 0
-
     return flashed.sum()
 
 def part_one():
-    padded_octos = np.pad(OCTOPI, 1)
-    return sum(step(padded_octos) for _ in range(100))
+    octos = OCTOPI.copy()
+    return sum(step(octos) for _ in range(100))
 
 def part_two():
-    padded_octos = np.pad(OCTOPI, 1)
-    octos = padded_octos[1: -1, 1: -1]
+    octos = OCTOPI.copy()
 
     for i in count():
         if (octos == 0).all():
             return i
 
-        step(padded_octos)
+        step(octos)
 
 aoc_helper.submit(11, part_one)
 aoc_helper.submit(11, part_two)
+print(part_one(), part_two())
