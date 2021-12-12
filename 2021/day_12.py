c5c81f5b2bd2e144b2a9d2eb9ebc5cd6e643bb51
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-12 01:30:20 -0600

    Slight trimming.

--- 2021/day_12.py
+++ 2021/day_12.py
@@ -3,34 +3,30 @@ import networkx as nx
 import aoc_helper
 
 RAW = aoc_helper.day(12)
-CAVE = nx.Graph(line.split("-") for line in RAW.splitlines())
+CAVES = nx.Graph(line.split("-") for line in RAW.splitlines())
 
-def npaths(current, seen=None, twice=None):
+def npaths(current, revisited, seen=()):
     if current == "end":
         return 1
 
-    if seen is None:
-        seen = set()
-
     if current.islower():
         if current not in seen:
-            seen = seen.copy()
-            seen.add(current)
-        elif twice is True:
-            twice = current
+            seen = {*seen, current}
+        elif not revisited:
+            revisited = True
 
     return sum(
-        npaths(neighbor, seen, twice)
-        for neighbor in CAVE[current]
+        npaths(neighbor, revisited, seen)
+        for neighbor in CAVES[current]
         if neighbor != "start"
-        if neighbor not in seen or twice is True
+        if neighbor not in seen or not revisited
     )
 
 def part_one():
-    return npaths("start")
+    return npaths("start", True)
 
 def part_two():
-    return npaths("start", twice=True)
+    return npaths("start", False)
 
 aoc_helper.submit(12, part_one)
 aoc_helper.submit(12, part_two)
