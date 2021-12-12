9b0b83ea57c7c1fecff9f7c0cff01fea579642bf
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-12 11:08:31 -0600

    Graph modification put into a function.

--- 2021/day_12.py
+++ 2021/day_12.py
@@ -4,19 +4,27 @@ import networkx as nx
 
 import aoc_helper
 
-CAVES = nx.MultiGraph(line.split("-") for line in aoc_helper.day(12).splitlines())
+def small_caves_multigraph():
+    """
+    For each `big_cave` in `big_caves` and for each u, v in the neighborhood of `big_cave`,
+    add an edge (u, v) to `caves`. Afterwards, remove all big caves.  The remaining
+    edges between the small caves now have multiplicity that can be used to count paths.
+    """
+    caves = nx.MultiGraph(line.split("-") for line in aoc_helper.day(12).splitlines())
 
-# For each big_cave in _BIG_CAVES and for each u, v in the neighborhood of big_cave,
-# add an edge (u, v) to CAVES. Afterwards, remove all big caves.  The remaining
-# edges between the small caves now have multiplicity that can be used to count paths.
-_BIG_CAVES = list(filter(str.isupper, CAVES))
+    big_caves = list(filter(str.isupper, caves))
 
-CAVES.add_edges_from(
-    edge
-    for big_cave in _BIG_CAVES
-    for edge in combinations_with_replacement(CAVES[big_cave], 2)
-)
-CAVES.remove_nodes_from(_BIG_CAVES)
+    caves.add_edges_from(
+        edge
+        for big_cave in big_caves
+        for edge in combinations_with_replacement(caves[big_cave], 2)
+    )
+
+    caves.remove_nodes_from(big_caves)
+
+    return caves
+
+CAVES = small_caves_multigraph()
 
 def npaths(u, revisited, seen=()):
     return u == "end" or sum(
