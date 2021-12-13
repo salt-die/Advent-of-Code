cf0ff77c355d1bff9c089a8f6027ba7befd7f275
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-12 23:42:23 -0600

    Trimming.

--- 2021/day_13.py
+++ 2021/day_13.py
@@ -4,19 +4,13 @@ import numpy as np
 
 import aoc_helper
 
-POINT_RE = re.compile(r"(\d+),(\d+)")
-INSTRUCTION_RE = re.compile(r"fold along ([xy])=(\d+)")
-
 _points, _instructions = aoc_helper.day(13).split("\n\n")
 
 PAPER = np.zeros((895, 1311), dtype=bool)
-for x, y in POINT_RE.findall(_points):
+for x, y in re.findall(r"(\d+),(\d+)", _points):
     PAPER[int(y), int(x)] = 1
 
-instructions = [
-    (axis, int(coord))
-    for axis, coord in INSTRUCTION_RE.findall(_instructions)
-]
+INSTRUCTIONS = [(axis, int(coord)) for axis, coord in re.findall(r"fold along ([xy])=(\d+)", _instructions)]
 
 def fold(paper, axis, coord):
     match axis:
@@ -26,16 +20,16 @@ def fold(paper, axis, coord):
             return paper[:coord] | paper[-1: coord: -1]
 
 def part_one():
-    return fold(PAPER, *instructions[0]).sum()
+    return fold(PAPER, *INSTRUCTIONS[0]).sum()
 
 def part_two():
     paper = PAPER
-    for axis, coord in instructions:
-        paper = fold(paper, axis, coord)
+    for instruction in INSTRUCTIONS:
+        paper = fold(paper, *instruction)
 
     aoc_helper.utils.dot_print(paper)
 
-    return "JRZBLGKH"  # After inspection
+    return "JRZBLGKH"  # after inspection
 
 aoc_helper.submit(13, part_one)
 aoc_helper.submit(13, part_two)
