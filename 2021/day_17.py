4c4795e40a9de3fb6424ff1a90d92d331d741167
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-17 09:26:48 -0600

    Refactor.

--- 2021/day_17.py
+++ 2021/day_17.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import product, starmap
 from math import inf
 
 import aoc_helper
@@ -24,21 +24,18 @@ def integrate(dx, dy):
         max_y = max(y, max_y)
 
         if XMIN <= x <= XMAX and YMIN <= y <= YMAX:
-            return max_y
+            return True
 
-    return -inf
+    return False
 
 def part_one():
     return YMIN * (YMIN + 1) // 2
 
 def part_two():
-    return sum(
-        1 if integrate(dx, dy) != -inf else 0
-        for dx, dy in product(
-            range(int((2 * XMIN)**.5), XMAX + 1),   # Solve: x * (x + 1) / 2 == XMIN => x ~~ (2 * XMIN)**.5
-            range(YMIN, abs(YMIN)),
-        )
-    )
+    LEAST_DX = int((2 * XMIN)**.5)  # Solve: x * (x + 1) / 2 == XMIN => x ~~ (2 * XMIN)**.5
+    RECT = product(range(LEAST_DX, XMAX + 1), range(YMIN, abs(YMIN)))
+
+    return sum(starmap(integrate, RECT))
 
 aoc_helper.submit(17, part_one)
 aoc_helper.submit(17, part_two)
