f5f0a98d06d915623d710d14ee4e971e5e6febc3
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-17 00:27:12 -0600

    Day 17 done!

--- 2021/day_17.py
+++ 2021/day_17.py
@@ -1,18 +1,44 @@
+from itertools import product
+from math import inf
+
 import aoc_helper
 
-RAW = aoc_helper.day(17)
-print(RAW)
+XMIN, XMAX, YMIN, YMAX = aoc_helper.utils.extract_ints(aoc_helper.day(17))
 
-def parse_raw():
-    ...
+def integrate(dx, dy):
+    x = y = 0
+    max_y = -inf
 
-DATA = parse_raw()
+    while not (
+        y < YMIN and dy <= 0
+        or dx >= 0 and x > XMAX
+        or dx <= 0 and x < XMIN
+        or dx == 0 and not (XMIN <= x <= XMAX)
+    ):
+        x += dx
+        y += dy
+
+        dx -= 1 if dx > 0 else -1 if dx < 0 else 0
+        dy -= 1
+
+        max_y = max(y, max_y)
+
+        if XMIN <= x <= XMAX and YMIN <= y <= YMAX:
+            return max_y
+
+    return -inf
 
 def part_one():
-    ...
+    return max(
+        integrate(dx, dy)
+        for dx, dy in product(range(XMAX + 1), range(YMIN, 200))
+    )
 
 def part_two():
-    ...
+    return sum(
+        1 if integrate(dx, dy) != -inf else 0
+        for dx, dy in product(range(XMAX + 1), range(YMIN, 200))
+    )
 
 aoc_helper.submit(17, part_one)
 aoc_helper.submit(17, part_two)
