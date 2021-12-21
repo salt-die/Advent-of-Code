30036ddb399d68de67c4604f0643cda3e34cdae1
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-21 10:26:37 -0600

    Day 21 done!

--- 2021/day_21.py
+++ 2021/day_21.py
@@ -1,18 +1,57 @@
+from collections import Counter
+from functools import cache
+from itertools import product
+from typing import NamedTuple
+
 import aoc_helper
+from aoc_helper.utils import shiftmod
 
-RAW = aoc_helper.day(21)
-print(RAW)
 
-def parse_raw():
-    ...
+class GameState(NamedTuple):
+    pos_0: int=9
+    pos_1: int=4
 
-DATA = parse_raw()
+    score_0: int=0
+    score_1: int=0
+
+    turn: int=0
+
+    def move(self, n):
+        pos_0, pos_1, score_0, score_1, turn = self
+
+        if turn % 2 == 0:
+            pos_0 = shiftmod(pos_0 + n, 10)
+            score_0 += pos_0
+        else:
+            pos_1 = shiftmod(pos_1 + n, 10)
+            score_1 += pos_1
+
+        return GameState(pos_0, pos_1, score_0, score_1, turn + 1)
 
 def part_one():
-    ...
+    gamestate = GameState()
+
+    while max(gamestate.score_0, gamestate.score_1) < 1000:
+        gamestate = gamestate.move(9 * gamestate.turn + 6)
+
+    return min(gamestate.score_0, gamestate.score_1) * 3 * gamestate.turn
 
 def part_two():
-    ...
+    FREQUENCIES = Counter(map(sum, product((1, 2, 3), repeat=3)))
+
+    @cache
+    def play(gamestate):
+        if gamestate.score_0 >= 21:
+            return 1
+
+        if gamestate.score_1 >= 21:
+            return 1j
+
+        return sum(freq * play(gamestate.move(n)) for n, freq in FREQUENCIES.items())
+
+    wins = play(GameState())
+
+    return max(int(wins.real), int(wins.imag))
 
 aoc_helper.submit(21, part_one)
 aoc_helper.submit(21, part_two)
