ce1356c73a3eadd827b7f14f85b43a9ea0eda25d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-25 15:31:03 -0600

    Day 25 done!

--- 2021/day_25.py
+++ 2021/day_25.py
@@ -1,18 +1,48 @@
+import numpy as np
+
 import aoc_helper
 
-RAW = aoc_helper.day(25)
-print(RAW)
+GRID = np.array(list(map(list, aoc_helper.day(25).splitlines())))
+EMPTY, EAST, SOUTH = ".>v"
 
-def parse_raw():
-    ...
+def step(universe):
+    # move east
+    where_east = universe == EAST
+    where_empty = universe == EMPTY
 
-DATA = parse_raw()
+    universe = np.where(
+        where_east & np.roll(where_empty, -1, 1),
+        EMPTY,
+        np.where(
+            where_empty & np.roll(where_east, 1, 1),
+            EAST,
+            universe,
+        ),
+    )
+
+    # move south
+    where_south = universe == SOUTH
+    where_empty = universe == EMPTY
+
+    universe = np.where(
+        where_south & np.roll(where_empty, -1, 0),
+        EMPTY,
+        np.where(
+            where_empty & np.roll(where_south, 1, 0),
+            SOUTH,
+            universe,
+        ),
+    )
+
+    return universe
 
 def part_one():
-    ...
+    universe = GRID
 
-def part_two():
-    ...
+    i = 1
+    while (universe != (universe := step(universe))).any():
+        i += 1
+
+    return i
 
 aoc_helper.submit(25, part_one)
-aoc_helper.submit(25, part_two)
