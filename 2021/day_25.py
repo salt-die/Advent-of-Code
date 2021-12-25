f2572518220c31ade7ff6324152dea211576af5c
salt-die <53280662+salt-die@users.noreply.github.com>
2021-12-25 16:18:56 -0600

    Refactor.

--- 2021/day_25.py
+++ 2021/day_25.py
@@ -2,47 +2,23 @@ import numpy as np
 
 import aoc_helper
 
-GRID = np.array(list(map(list, aoc_helper.day(25).splitlines())))
+FLOOR = np.array(list(map(list, aoc_helper.day(25).splitlines())))
 EMPTY, EAST, SOUTH = ".>v"
 
-def step(universe):
-    # move east
-    where_east = universe == EAST
-    where_empty = universe == EMPTY
+def step():
+    moving_east = (FLOOR == EAST) & np.roll(FLOOR == EMPTY, -1, 1)
 
-    universe = np.where(
-        where_east & np.roll(where_empty, -1, 1),
-        EMPTY,
-        np.where(
-            where_empty & np.roll(where_east, 1, 1),
-            EAST,
-            universe,
-        ),
-    )
+    FLOOR[moving_east] = EMPTY
+    FLOOR[np.roll(moving_east, 1, 1)] = EAST
 
-    # move south
-    where_south = universe == SOUTH
-    where_empty = universe == EMPTY
+    moving_south = (FLOOR == SOUTH) & np.roll(FLOOR == EMPTY, -1, 0)
 
-    universe = np.where(
-        where_south & np.roll(where_empty, -1, 0),
-        EMPTY,
-        np.where(
-            where_empty & np.roll(where_south, 1, 0),
-            SOUTH,
-            universe,
-        ),
-    )
+    FLOOR[moving_south] = EMPTY
+    FLOOR[np.roll(moving_south, 1, 0)] = SOUTH
 
-    return universe
+    return (moving_east | moving_south).any()
 
 def part_one():
-    universe = GRID
-
-    i = 1
-    while (universe != (universe := step(universe))).any():
-        i += 1
-
-    return i
+    return sum(iter(step, False)) + 1
 
 aoc_helper.submit(25, part_one)
