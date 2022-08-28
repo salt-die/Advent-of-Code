0c42ac67af068d66fffcf69aaec40eb25289b787
salt-die <53280662+salt-die@users.noreply.github.com>
2022-08-27 21:21:56 -0500

    Day 10!

--- 2015/day_10.py
+++ 2015/day_10.py
@@ -1,18 +1,34 @@
 import aoc_helper
+from aoc_helper.utils import ilen
 
-RAW = aoc_helper.day(10)
-print(RAW)
+def look_and_say(it):
+    curr = next(it)
+    count = 1
 
-def parse_raw():
-    ...
+    for i in it:
+        if i != curr:
+            yield count
+            yield curr
 
-DATA = parse_raw()
+            curr = i
+            count = 1
+        else:
+            count += 1
+
+    yield count
+    yield curr
+
+def len_nth_look(n):
+    it = iter(map(int, aoc_helper.day(10)))
+    for _ in range(n):
+        it = look_and_say(it)
+    return ilen(it)
 
 def part_one():
-    ...
+    return len_nth_look(40)
 
 def part_two():
-    ...
+    return len_nth_look(50)
 
 aoc_helper.submit(10, part_one)
 aoc_helper.submit(10, part_two)
