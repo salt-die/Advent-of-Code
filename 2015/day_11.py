2db7b25b1e0ea8aecd57b31be3e731de8f82e72f
salt-die <53280662+salt-die@users.noreply.github.com>
2022-08-28 00:01:35 -0500

    Trimming.

--- 2015/day_11.py
+++ 2015/day_11.py
@@ -1,22 +1,20 @@
-from string import ascii_lowercase
-
 import aoc_helper
 
 def get_next_valid(password):
     # This isn't a general solution: Since we only need to generate two valid passwords,
     # we assume no invalid characters at the start of the string, and no doubles or ascending
     # characters in the initial password (which is true for our puzzle input, by inspection).
-    as_int = list(map(ascii_lowercase.find, password))
+    as_int = list(map(ord, password))
     if as_int[3] <= as_int[4]:
         as_int[3] += 1
 
-    if (i := as_int[3]) <= 23:
-        as_int[3:] = i, i, i + 1, i + 2, i + 2  # Assume no invalids
+    if (i := as_int[3]) <= ord("x"):
+        as_int[3:] = i, i, i + 1, i + 2, i + 2
     else:
-        as_int[2] += 1  # Assume no roll-over
-        as_int[3:] = 0, 0, 1, 2, 2
+        as_int[2] += 1
+        as_int[3:] = map(ord, "aabcc")
 
-    return "".join(ascii_lowercase[i] for i in as_int)
+    return "".join(map(chr, as_int))
 
 def part_one():
     return get_next_valid(aoc_helper.day(11))
