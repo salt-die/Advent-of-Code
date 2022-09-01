29d7e23a929574861a7f3d745dbdef33ed513bf2
salt-die <53280662+salt-die@users.noreply.github.com>
2022-08-31 21:39:09 -0500

    Day 12 done!

--- 2015/day_12.py
+++ 2015/day_12.py
@@ -1,18 +1,27 @@
+import json
+
 import aoc_helper
-
-RAW = aoc_helper.day(12)
-print(RAW)
-
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+from aoc_helper.utils import extract_ints
 
 def part_one():
-    ...
+    return sum(extract_ints(aoc_helper.day(12)))
+
+def count(account):
+    match account:
+        case dict():
+            if "red" in account.values():
+                return 0
+            return sum(map(count, account.values()))
+        case list():
+            return sum(map(count, account))
+        case str():
+            return 0
+        case int():
+            return account
 
 def part_two():
-    ...
+    account = json.loads(aoc_helper.day(12))
+    return count(account)
 
 aoc_helper.submit(12, part_one)
 aoc_helper.submit(12, part_two)
