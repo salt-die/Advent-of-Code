b0e954a4b8062d40424655edf650bb3ab4e4422c
salt-die <53280662+salt-die@users.noreply.github.com>
2022-08-31 22:09:34 -0500

    Day 13 done!

--- 2015/day_13.py
+++ 2015/day_13.py
@@ -1,18 +1,37 @@
+import re
+from itertools import permutations
+
 import aoc_helper
 
-RAW = aoc_helper.day(13)
-print(RAW)
+RE = r"(.*) would (gain|lose) (.*) happiness units by sitting next to (.*)."
 
-def parse_raw():
-    ...
+G = {}
+for a, sign, happiness, b in re.findall(RE, aoc_helper.day(13)):
+    G.setdefault(a, {})[b] = int(happiness) * (1 if sign == "gain" else -1)
 
-DATA = parse_raw()
+def pairwise_cycle(iterable):
+    it = iter(iterable)
+    first = last_item = next(it)
+    for item in it:
+        yield last_item, item
+        last_item = item
+    yield last_item, first
+
+def optimal_score():
+    return max(
+        sum(G[a][b] + G[b][a] for a, b in pairwise_cycle(p))
+        for p in permutations(G)
+    )
 
 def part_one():
-    ...
+    return optimal_score()
 
 def part_two():
-    ...
+    for scores in G.values():
+        scores["salt-die"] = 0
+
+    G["salt-die"] = {name: 0 for name in G}
+    return optimal_score()
 
 aoc_helper.submit(13, part_one)
 aoc_helper.submit(13, part_two)
