f1f0270d740a4300e6e665df679fb43f321e31f3
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-03 05:00:35 -0500

    Day 14 done!

--- 2015/day_14.py
+++ 2015/day_14.py
@@ -1,18 +1,23 @@
+import numpy as np
+
 import aoc_helper
+from aoc_helper.utils import extract_ints
 
-RAW = aoc_helper.day(14)
-print(RAW)
+SPEEDS, RUN_TIMES, REST_TIMES = np.fromiter(
+    extract_ints(aoc_helper.day(14)),
+    dtype=int,
+).reshape(-1, 3).T
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+def distances(seconds):
+    n, remaining = np.divmod(seconds, RUN_TIMES + REST_TIMES)
+    return SPEEDS * (RUN_TIMES * n + np.minimum(RUN_TIMES, remaining))
 
 def part_one():
-    ...
+    return distances(2503).max()
 
 def part_two():
-    ...
+    traveled = np.array([distances(i) for i in range(1, 2504)])
+    return (traveled == traveled.max(axis=1)[:, None]).sum(axis=0).max()
 
 aoc_helper.submit(14, part_one)
 aoc_helper.submit(14, part_two)
