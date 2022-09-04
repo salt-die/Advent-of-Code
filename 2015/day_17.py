2338fad3599b17480483e3d65491ca3074099148
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-04 11:11:37 -0500

    More efficent backtracker: Escape loops earlier.

--- 2015/day_17.py
+++ 2015/day_17.py
@@ -6,16 +6,21 @@ from aoc_helper.utils import extract_ints
 def containerize():
     containers = np.array(sorted(extract_ints(aoc_helper.day(17))))
     used_containers = np.zeros_like(containers, dtype=bool)
-    ncontainers = len(containers)
 
     def _backtracker(i):
-        if (amount := containers @ used_containers) == 150:
-            yield used_containers.sum()
-        elif amount < 150:
-            for container in range(i, ncontainers):
-                used_containers[container] = True
-                yield from _backtracker(container + 1)
-                used_containers[container] = False
+        for j in range(i, len(containers)):
+            used_containers[j] = True
+
+            amount = containers @ used_containers
+            if amount < 150:
+                yield from _backtracker(j + 1)
+            elif amount == 150:
+                yield used_containers.sum()
+
+            used_containers[j] = False
+
+            if amount >= 150:
+                break
 
     yield from _backtracker(0)
 
