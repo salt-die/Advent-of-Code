e2794acf129bd01de38e6eaff1e4d77055dfb4e5
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-04 07:42:37 -0500

    Day 17 done!

--- 2015/day_17.py
+++ 2015/day_17.py
@@ -1,18 +1,31 @@
+import numpy as np
+
 import aoc_helper
+from aoc_helper.utils import extract_ints
 
-RAW = aoc_helper.day(17)
-print(RAW)
+def containerize():
+    containers = np.array(sorted(extract_ints(aoc_helper.day(17))))
+    used_containers = np.zeros_like(containers, dtype=bool)
+    ncontainers = len(containers)
 
-def parse_raw():
-    ...
+    def _backtracker(i):
+        if (amount := containers @ used_containers) == 150:
+            yield used_containers.sum()
+        elif amount < 150:
+            for container in range(i, ncontainers):
+                used_containers[container] = True
+                yield from _backtracker(container + 1)
+                used_containers[container] = False
 
-DATA = parse_raw()
+    yield from _backtracker(0)
+
+COMBINATIONS = list(containerize())
 
 def part_one():
-    ...
+    return len(COMBINATIONS)
 
 def part_two():
-    ...
+    return COMBINATIONS.count(min(COMBINATIONS))
 
 aoc_helper.submit(17, part_one)
 aoc_helper.submit(17, part_two)
