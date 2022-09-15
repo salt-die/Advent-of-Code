11b7fa7681369bfca8e810a9717fea809d120b04
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-15 01:34:12 -0500

    Rename constant.

--- 2015/day_19.py
+++ 2015/day_19.py
@@ -5,23 +5,23 @@ import aoc_helper
 
 sym_to_fruit = defaultdict(list("🍅🍇🍈🍉🍊🍋🍌🍍🍎🍐🍑🍒🍓🥑🥝🥭").pop)
 raw_fruit = re.sub(r"[A-Z][a-z]*", lambda m: sym_to_fruit[m[0]], aoc_helper.day(19))
-productions, FRUIT_BASKET = raw_fruit.split("\n\n")
+productions, FRUITS = raw_fruit.split("\n\n")
 
 PRODUCE = defaultdict(list)
-for a, b in re.findall(r"(.) => (.)", productions):
+for a, b in re.findall(r"(.) => (.*)", productions):
     PRODUCE[a].append(b)
 
 def part_one():
     seen = set()
-    for i, char in enumerate(FRUIT_BASKET):
+    for i, char in enumerate(FRUITS):
         if subs := PRODUCE.get(char):
             for sub in subs:
-                seen.add(f"{FRUIT_BASKET[:i]}{sub}{FRUIT_BASKET[i + 1:]}")
+                seen.add(f"{FRUITS[:i]}{sub}{FRUITS[i + 1:]}")
 
     return len(seen)
 
 def part_two():
-    return len(FRUIT_BASKET) - 2 * FRUIT_BASKET.count("🍒") - 2 * FRUIT_BASKET.count("🍋") - 1
+    return len(FRUITS) - 2 * FRUITS.count("🍒") - 2 * FRUITS.count("🍋") - 1
 
 aoc_helper.submit(19, part_one)
 aoc_helper.submit(19, part_two)
