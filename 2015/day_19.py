61737ffe3b424261cebb3ea9b8d2aab88d528e9e
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-14 23:40:21 -0500

    Trimming.

--- 2015/day_19.py
+++ 2015/day_19.py
@@ -3,34 +3,25 @@ from collections import defaultdict
 
 import aoc_helper
 
+sym_to_fruit = defaultdict(list("🍅🍇🍈🍉🍊🍋🍌🍍🍎🍐🍑🍒🍓🥑🥝🥭").pop)
+raw_fruit = re.sub(r"[A-Z][a-z]*", lambda m: sym_to_fruit[m[0]], aoc_helper.day(19))
+productions, FRUIT_BASKET = raw_fruit.split("\n\n")
 
-def parse_raw():
-    symbol_to_fruit = defaultdict(list("🍅🍇🍈🍉🍊🍋🍌🍍🍎🍐🍑🍒🍓🥑🥝🥭").pop)
-    def fruitize(match):
-        return symbol_to_fruit[match.group(0)]
-
-    raw = re.sub(r"([A-Z][a-z]*)", fruitize, aoc_helper.day(19))
-    replacements, molecule = raw.split("\n\n")
-    rules = {}
-    for a, b in re.findall(r"(.*) => (.*)", replacements):
-        rules.setdefault(a, []).append(b)
-
-    return rules, molecule
-
-RULES, MOLECULE = parse_raw()
-
+PRODUCE = defaultdict(list)
+for a, b in re.findall(r"(.) => (.)", productions):
+    PRODUCE[a].append(b)
 
 def part_one():
     seen = set()
-    for i, char in enumerate(MOLECULE):
-        if subs := RULES.get(char):
+    for i, char in enumerate(FRUIT_BASKET):
+        if subs := PRODUCE.get(char):
             for sub in subs:
-                seen.add(f"{MOLECULE[:i]}{sub}{MOLECULE[i + 1:]}")
+                seen.add(f"{FRUIT_BASKET[:i]}{sub}{FRUIT_BASKET[i + 1:]}")
 
     return len(seen)
 
 def part_two():
-    return len(MOLECULE) - 2 * MOLECULE.count("🍒") - 2 * MOLECULE.count("🍋") - 1
+    return len(FRUIT_BASKET) - 2 * FRUIT_BASKET.count("🍒") - 2 * FRUIT_BASKET.count("🍋") - 1
 
 aoc_helper.submit(19, part_one)
 aoc_helper.submit(19, part_two)
