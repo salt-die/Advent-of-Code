54ca1b984c181410b21ee7250ab7ef9988826f16
salt-die <53280662+salt-die@users.noreply.github.com>
2022-09-15 20:20:08 -0500

    Day 20 done!

--- 2015/day_20.py
+++ 2015/day_20.py
@@ -1,18 +1,47 @@
+from itertools import count
+
+import numpy as np
+from sympy.ntheory import divisor_sigma, divisors
+
 import aoc_helper
 
-RAW = aoc_helper.day(20)
-print(RAW)
+N = int(aoc_helper.day(20))
+STEP = 360
 
-def parse_raw():
-    ...
+def least_n(target):
+    """
+    Find the least n such that::
+        target < np.e ** np.euler_gamma * n * np.log(np.log(n))
 
-DATA = parse_raw()
+    See: https://en.wikipedia.org/wiki/Divisor_function (Robin's Theorem)
+    """
+    E_GAMMA = np.e ** np.euler_gamma
+
+    min = 5041  # Min value that theorem is valid.
+    max = target
+
+    while min != max:
+        mid = (min + max) // 2
+
+        if E_GAMMA * mid * np.log(np.log(mid)) < target:
+            min = mid + 1
+        else:
+            max = mid
+
+    return min - min % STEP  # Return a multiple of STEP. STEP has lots of small prime divisors.
+
 
 def part_one():
-    ...
+    target = N // 10
+    for i in count(least_n(target), STEP):
+        if divisor_sigma(i) >= target:
+            return i
 
 def part_two():
-    ...
+    target = N // 11
+    for i in count(least_n(target), STEP):  # Robin's Theorem will still be good enough for a lower bound.
+        if sum(p for p in divisors(i) if i // p <= 50) >= target:
+            return i
 
 aoc_helper.submit(20, part_one)
 aoc_helper.submit(20, part_two)
