d0f7c6f9917ebdbea66c7d1b19aafb695f46ba30
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 14:41:43 -0600

    Trimming.

--- 2022/day_01.nim
+++ 2022/day_01.nim
@@ -1,9 +1,6 @@
 import std/[heapqueue, sequtils, strutils]
 import nimpy
 
-template sum(sequence: untyped): untyped =
-  sequence.foldl(a + b)
-
 proc nlargest[T](iterable: openArray[T], n: int): HeapQueue[T] =
   for i in iterable:
     if result.len < n:
@@ -11,12 +8,11 @@ proc nlargest[T](iterable: openArray[T], n: int): HeapQueue[T] =
     elif i > result[0]:
       discard result.replace i
 
-let
-  CALORIES = "aoc_lube"
-    .pyImport
-    .callMethod(string, "fetch", 2022, 1)
-    .split("\n\n")
-    .mapIt(it.split.map(parseInt).sum)
+let CALORIES = "aoc_lube"
+  .pyImport
+  .callMethod(string, "fetch", 2022, 1)
+  .split("\n\n")
+  .mapIt(it.split.map(parseInt).foldl(a + b))
 
 echo "Part 1: ", CALORIES.max
-echo "Part 2: ", CALORIES.nlargest(3).sum
+echo "Part 2: ", CALORIES.nlargest(3).foldl(a + b)
