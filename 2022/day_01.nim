b48ae02853dfdfdc2fbfb747c737b8032130db2e
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 00:30:37 -0600

    Day 1 solution in nim!

---
+++ 2022/day_01.nim
@@ -0,0 +1,13 @@
+import std/[algorithm, re, strformat, sequtils, strutils, sugar]
+import nimpy
+
+proc extract_ints(s: string): seq[int] =
+  collect(for i in s.findAll(re"-?\d+"): i.parseInt)
+
+let
+  aoc_lube = nimpy.pyImport("aoc_lube")
+  RAW = nimpy.callMethod(aoc_lube, string, "fetch", 2022, 1).split("\n\n")
+  CALORIES = collect(for elf in RAW: extract_ints(elf).foldl(a + b))
+
+echo fmt"Part 1: {CALORIES.max}"
+echo fmt"Part 2: {CALORIES.sorted(cmp[int], order=SortOrder.Descending)[..2].foldl(a + b)}"
