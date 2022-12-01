b0effe9f9c4f192503fd234131667457820b46c7
salt-die <53280662+salt-die@users.noreply.github.com>
2022-11-30 23:08:51 -0600

    Day 1 finished.

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -1,18 +1,18 @@
+from heapq import nlargest
+
 import aoc_lube
+from aoc_lube.utils import extract_ints
 
-RAW = aoc_lube.fetch(year=2022, day=1)
-print(RAW)
-
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+CALORIES = [
+    sum(extract_ints(elf))
+    for elf in aoc_lube.fetch(year=2022, day=1).split("\n\n")
+]
 
 def part_one():
-    ...
+    return max(CALORIES)
 
 def part_two():
-    ...
+    return sum(nlargest(3, CALORIES))
 
 aoc_lube.submit(year=2022, day=1, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=1, part=2, solution=part_two)
