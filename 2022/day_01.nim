48bc10f07f530767c1298d0be1150c24f21afc68
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 04:07:36 -0600

    Fix linter complaints.

--- 2022/day_01.nim
+++ 2022/day_01.nim
@@ -1,4 +1,4 @@
-import std/[algorithm, re, strformat, sequtils, strutils, sugar]
+import std/[algorithm, re, sequtils, strutils, sugar]
 import nimpy
 
 proc extract_ints(s: string): seq[int] =
@@ -9,5 +9,5 @@ let
   RAW = nimpy.callMethod(aoc_lube, string, "fetch", 2022, 1).split("\n\n")
   CALORIES = collect(for elf in RAW: extract_ints(elf).foldl(a + b))
 
-echo fmt"Part 1: {CALORIES.max}"
-echo fmt"Part 2: {CALORIES.sorted(cmp[int], order=SortOrder.Descending)[..2].foldl(a + b)}"
+echo "Part 1: ", CALORIES.max
+echo "Part 2: ", CALORIES.sorted(cmp[int], order=SortOrder.Descending)[..2].foldl(a + b)
