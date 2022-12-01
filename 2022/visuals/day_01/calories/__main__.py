674f9efbeb3397865e23371a9fc38c773d9433c4
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 04:15:06 -0600

    Tidying.

--- 2022/visuals/day_01/calories/__main__.py
+++ 2022/visuals/day_01/calories/__main__.py
@@ -3,38 +3,36 @@ from pathlib import Path
 
 from nurses_2.app import App
 from nurses_2.colors import Color, ColorPair
-from nurses_2.widgets.text_widget import TextWidget
 from nurses_2.widgets.animation import Animation
 from nurses_2.widgets.image import Image
 from nurses_2.widgets.parallax import Parallax
+from nurses_2.widgets.text_widget import TextWidget
 
 import aoc_lube
 from aoc_lube.utils import extract_ints
 
 TABLE_COLORPAIR = ColorPair.from_colors(Color.from_hex("8cffff"), Color.from_hex("053d1a"))
-
 CALORIES = [list(extract_ints(elf)) for elf in aoc_lube.fetch(year=2022, day=1).split("\n\n")]
-
 ASSETS = Path(__file__).parent.parent / "assets"
 ELF = ASSETS / "elf"
 JUNGLE = ASSETS / "jungle"
-SKY = ASSETS / "Sky.png"
+SKY = ASSETS / "sky.png"
 CANDY_CANE = ASSETS / "candy_cane.png"
 
 class CalorieApp(App):
     async def on_start(self):
+        sky = Image(path=SKY, size_hint=(1.0, 1.0))
         jungle = Parallax(path=JUNGLE, size_hint=(1.0, 1.0), interpolation="linear")
         elf = Animation(path=ELF, size=(13, 26), interpolation="nearest")
-        sky = Image(path=SKY, size_hint=(1.0, 1.0))
 
         table = TextWidget(size=(17, 12), default_color_pair=TABLE_COLORPAIR)
         right_border = Image(path=CANDY_CANE, size=(17, 6))
         right_border.texture = right_border.texture[::-1, ::-1]
-        right_border.subscribe(table, "pos", lambda: setattr(right_border, "pos", (table.y, 12)))
+        right_border.subscribe(table, "pos", lambda: setattr(right_border, "pos", (table.y, table.right)))
         bottom_border = Image(path=CANDY_CANE)
         bottom_border._otexture = bottom_border._otexture.swapaxes(0, 1)[::-1, ::-1]
         bottom_border.size = (3, 15)
-        bottom_border.subscribe(table, "pos", lambda: setattr(bottom_border, "pos", (table.bottom, 0)))
+        bottom_border.subscribe(table, "pos", lambda: setattr(bottom_border, "pos", (table.bottom, table.x)))
 
         self.add_widgets(sky, jungle, elf, table, right_border, bottom_border)
 
