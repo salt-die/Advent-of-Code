1317b088400ca3c419ba431060cdfac3bbed622f
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 23:49:47 -0600

    Day 2 done!

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -1,18 +1,20 @@
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2022, day=2)
-print(RAW)
+strats = dict(
+    A=0, B=1, C=2,
+    X=0, Y=1, Z=2,
+)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+games = [
+    (strats[line[0]], strats[line[2]])
+    for line in  aoc_lube.fetch(year=2022, day=2).splitlines()
+]
 
 def part_one():
-    ...
+    return sum(b + 1 + 3 * ((b - a + 1) % 3) for a, b in games)
 
 def part_two():
-    ...
+    return sum((b - 1 + a) % 3 + 1 + 3 * b for a, b in games)
 
 aoc_lube.submit(year=2022, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=2, part=2, solution=part_two)
