edc97e0317c1faf13da0667a50ae748bb47ce4b1
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-01 20:31:33 -0600

    Doc strings added.

--- 2022/aoc.nim
+++ 2022/aoc.nim
@@ -4,17 +4,19 @@ import std/[heapqueue, re]
 import nimpy
 
 proc fetch*(year, day: int): string =
-  # Get AoC input.
+  ## Get AoC input.
   "aoc_lube".pyImport.callMethod(string, "fetch", 2022, 1)
 
 template part*(p: int, solution: untyped) =
-  # Shortcut to print solution.
+  ## Template to print solution.
   echo "Part ", p, ": ", solution
 
 proc extract_ints*(s: string): seq[int] =
+  ## Extract all integers from a string.
   s.findAll(re"-?\d+").map(parseInt)
 
 proc nlargest*[T](iterable: openArray[T], n: int): HeapQueue[T] =
+  ## Return n largest items from iterable.
   for i in iterable:
     if result.len < n:
       result.push i
@@ -22,7 +24,9 @@ proc nlargest*[T](iterable: openArray[T], n: int): HeapQueue[T] =
       discard result.replace i
 
 template sum*(it: untyped): untyped =
+  ## Sum of all items in iterable.
   it.foldl(a + b)
 
 template prod*(it: untyped): untyped =
+  ## Product of all items in iterable.
   it.foldl(a * b)
