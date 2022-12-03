c214d4cf224997e3c646227bc1db29ac03af60b0
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-03 03:13:29 -0600

    Refactor.

--- 2022/day_03.py
+++ 2022/day_03.py
@@ -1,29 +1,24 @@
-from itertools import chain
-
 import aoc_lube
 from aoc_lube.utils import chunk, split
 
 RUCKSACKS = aoc_lube.fetch(year=2022, day=3).splitlines()
 
-def priority(letter):
+def priority(s):
+    letter = next(iter(s))
     offset = ord("a") if letter.islower() else (ord("A") - 26)
     return 1 + ord(letter) - offset
 
 def part_one():
-    return sum(map(priority,
-        chain.from_iterable(
-            set(a) & set(b)
-            for a, b in map(split, RUCKSACKS)
-        )
-    ))
+    return sum(
+        priority(set(a) & set(b))
+        for a, b in map(split, RUCKSACKS)
+    )
 
 def part_two():
-    return sum(map(priority,
-        chain.from_iterable(
-            set(a) & set(b) & set(c)
-            for a, b, c in chunk(RUCKSACKS, 3)
-        )
-    ))
+    return sum(
+        priority(set(a) & set(b) & set(c))
+        for a, b, c in chunk(RUCKSACKS, 3)
+    )
 
 aoc_lube.submit(year=2022, day=3, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=3, part=2, solution=part_two)
