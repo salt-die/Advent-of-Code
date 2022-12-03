ba8f5569c3f2eeb6f2580cd152d59edd68d668a1
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-03 03:28:46 -0600

    Tidying.

--- 2022/day_03.py
+++ 2022/day_03.py
@@ -1,24 +1,25 @@
+from functools import reduce
+
 import aoc_lube
 from aoc_lube.utils import chunk, split
 
 RUCKSACKS = aoc_lube.fetch(year=2022, day=3).splitlines()
 
-def priority(s):
-    letter = next(iter(s))
+def priority(letter):
     offset = ord("a") if letter.islower() else (ord("A") - 26)
     return 1 + ord(letter) - offset
 
-def part_one():
+def sum_priorities(iterable):
     return sum(
-        priority(set(a) & set(b))
-        for a, b in map(split, RUCKSACKS)
+        priority(*reduce(set.intersection, map(set, sacks)))
+        for sacks in iterable
     )
 
+def part_one():
+    return sum_priorities(map(split, RUCKSACKS))
+
 def part_two():
-    return sum(
-        priority(set(a) & set(b) & set(c))
-        for a, b, c in chunk(RUCKSACKS, 3)
-    )
+    return sum_priorities(chunk(RUCKSACKS, 3))
 
 aoc_lube.submit(year=2022, day=3, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=3, part=2, solution=part_two)
