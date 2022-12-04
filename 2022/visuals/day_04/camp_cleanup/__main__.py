8139a56cd02be662c7331931a1774bebc31eaae8
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-04 16:02:38 -0600

    Removed white middle line.

--- 2022/visuals/day_04/camp_cleanup/__main__.py
+++ 2022/visuals/day_04/camp_cleanup/__main__.py
@@ -8,7 +8,7 @@ import parse
 from nurses_2.app import App
 from nurses_2.clamp import clamp
 from nurses_2.easings import lerp, out_elastic
-from nurses_2.colors import ColorPair, BLACK, YELLOW, GREEN, RED, WHITE_ON_BLACK, WHITE
+from nurses_2.colors import ColorPair, BLACK, YELLOW, GREEN, RED
 from nurses_2.widgets.text_widget import TextWidget
 from nurses_2.widgets.widget import Widget
 from nurses_2.widgets.video_player import VideoPlayer
@@ -18,7 +18,6 @@ SKY_PATH = Path(__file__).parent.parent / "assets" / "sky.gif"
 RED_ON_BLACK = ColorPair.from_colors(RED, BLACK)
 GREEN_ON_BLACK = ColorPair.from_colors(GREEN, BLACK)
 YELLOW_ON_BLACK = ColorPair.from_colors(YELLOW, BLACK)
-BLACK_ON_WHITE = ColorPair.from_colors(BLACK, WHITE)
 DASH = "▬"
 DURATION = 1
 
@@ -29,49 +28,52 @@ def ease(old, new, p):
 class RangeApp(App):
     async def on_start(self):
         bg = VideoPlayer(source=SKY_PATH, size_hint=(1.0, 1.0))
-        container = Widget(size=(14, 105), pos_hint=(.5, .5), anchor="center")
-
-        ranges = TextWidget(size=(3, 105), pos=(11, 0))
-        ranges.colors[0] = RED_ON_BLACK
-        ranges.colors[2] = GREEN_ON_BLACK
+        container = Widget(size=(15, 105), pos_hint=(.5, .5), anchor="center")
 
         inputs = TextWidget(size=(11, 11), pos=(0, 47))
         inputs.add_text("Assignments", underline=True)
-        inputs.colors[-1] = BLACK_ON_WHITE
+        inputs.colors[1:-1, :5] = RED_ON_BLACK
+        inputs.colors[1:-1, 6:] = GREEN_ON_BLACK
+        inputs.colors[-1, :5] = RED_ON_BLACK.reverse
+        inputs.colors[-1, 6:] = GREEN_ON_BLACK.reverse
         for i in range(9):
-            a, b, c, d = RANGES[i]
-            inputs.add_text(f"{a:>2}-{b:>2},{c:>2}-{d:>2}", row=-i-2)
+            inputs.add_text("{:>2}-{:>2},{:>2}-{:>2}".format(*RANGES[i]), row=-i - 2)
 
+        ranges = TextWidget(size=(3, 105), pos=(12, 0))
+        ranges.colors[0] = RED_ON_BLACK
+        ranges.colors[1] = YELLOW_ON_BLACK
+        ranges.colors[2] = GREEN_ON_BLACK
 
         container.add_widgets(inputs, ranges)
         self.add_widgets(bg, container)
         bg.play()
 
         def set_ranges(a, b, c, d):
-            ranges.canvas.T[:] = " ", DASH, " "
-            ranges.colors[1] = WHITE_ON_BLACK
-
+            """
+            Set the endpoints of the ranges.
+            """
+            ranges.canvas[:] = " "
+            # Top range:
             ranges.canvas[0, a + 2: b + 3] = DASH
             ranges.add_text(f"{a:>2}", column=a - 1)
             ranges.add_text(f"{b:<2}", column=b + 4)
-
+            # Bottom range:
             ranges.canvas[2, c + 2: d + 3] = DASH
             ranges.add_text(f"{c:>2}", row=2, column=c - 1)
             ranges.add_text(f"{d:<2}", row=2, column=d + 4)
-
+            # Intersection:
             if a <= d and c <= b:
                 start, end = max(a, c), min(b, d)
-                ranges.colors[1, start - 1: end + 6] = YELLOW_ON_BLACK
-                ranges.add_text(f"{start:>2} ", row=1, column=start - 1)
-                ranges.add_text(f" {end:<2}", row=1, column=end + 3)
+                ranges.canvas[1, start + 2: end + 3] = DASH
+                ranges.add_text(f"{start:>2}", row=1, column=start - 1)
+                ranges.add_text(f"{end:<2}", row=1, column=end + 4)
 
         old_a, old_b = old_c, old_d = 0, 99
         for i, (a, b, c, d) in enumerate(RANGES):
             # Roll assignments:
             inputs.canvas[2:] = inputs.canvas[1:-1]
             if i + 9 < len(RANGES):
-                top_a, top_b, top_c, top_d = RANGES[i + 9]
-                inputs.add_text(f"{top_a:>2}-{top_b:>2},{top_c:>2}-{top_d:>2}", row=1)
+                inputs.add_text("{:>2}-{:>2},{:>2}-{:>2}".format(*RANGES[i + 9]), row=1)
             else:
                 inputs.canvas[1] = " "
 
@@ -79,11 +81,12 @@ class RangeApp(App):
             end_time = monotonic() + DURATION
             while (current_time := monotonic()) < end_time:
                 p = 1 - (end_time - current_time) / DURATION
-                _a = ease(old_a, a, p)
-                _b = ease(old_b, b, p)
-                _c = ease(old_c, c, p)
-                _d = ease(old_d, d, p)
-                set_ranges(_a, _b, _c, _d)
+                set_ranges(
+                    ease(old_a, a, p),
+                    ease(old_b, b, p),
+                    ease(old_c, c, p),
+                    ease(old_d, d, p),
+                )
                 await asyncio.sleep(0)
 
             set_ranges(a, b, c, d)
