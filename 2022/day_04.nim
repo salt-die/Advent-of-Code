1b33cd735332ae104f0bfe617144a6b860e51fa7
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-04 22:07:39 -0600

    `scanf` parsing.

--- 2022/day_04.nim
+++ 2022/day_04.nim
@@ -1,15 +1,19 @@
 include aoc
 
-let RANGES = fetch(2022, 4).findAll(re"\d+").map(parseInt).chunk(4)
+let RANGES = block:
+  var a, b, c, d: int
+  collect(
+    for line in fetch(2022, 4).splitLines:
+      discard line.scanf("$i-$i,$i-$i", a, b, c, d)
+      (a, b, c, d)
+  )
 
 part 1: sum collect(
-  for r in RANGES:
-    (a, b, c, d) =* r
+  for (a, b, c, d) in RANGES:
     int a <= c and d <= b or c <= a and b <= d
 )
 
 part 2: sum collect(
-  for r in RANGES:
-    (a, b, c, d) =* r
+  for (a, b, c, d) in RANGES:
     int a <= d and c <= b
 )
