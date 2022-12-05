2b733f99bac7f47f140c3826ee8916e0f58187fb
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-05 01:46:31 -0600

    Use scanTuple.

--- 2022/day_04.nim
+++ 2022/day_04.nim
@@ -1,19 +1,15 @@
 include aoc
 
-let RANGES = block:
-  var a, b, c, d: int
-  collect(
-    for line in fetch(2022, 4).splitLines:
-      discard line.scanf("$i-$i,$i-$i", a, b, c, d)
-      (a, b, c, d)
-  )
+let RANGES = collect(for line in fetch(2022, 4).splitLines:
+  line.scanTuple("$i-$i,$i-$i")
+)
 
 part 1: sum collect(
-  for (a, b, c, d) in RANGES:
+  for (_, a, b, c, d) in RANGES:
     int a <= c and d <= b or c <= a and b <= d
 )
 
 part 2: sum collect(
-  for (a, b, c, d) in RANGES:
+  for (_, a, b, c, d) in RANGES:
     int a <= d and c <= b
 )
