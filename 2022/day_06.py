93ba618e20dfcfabd3b374168f406d16c356c598
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-06 00:00:08 -0600

    Day 6 done!

--- 2022/day_06.py
+++ 2022/day_06.py
@@ -1,18 +1,18 @@
 import aoc_lube
+from aoc_lube.utils import sliding_window
 
 RAW = aoc_lube.fetch(year=2022, day=6)
-print(RAW)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+def distinct_n(n):
+    for i, a in enumerate(sliding_window(RAW, n), start=n):
+        if len(set(a)) == n:
+            return i
 
 def part_one():
-    ...
+    return distinct_n(4)
 
 def part_two():
-    ...
+    return distinct_n(14)
 
 aoc_lube.submit(year=2022, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=6, part=2, solution=part_two)
