378d9c6eee8da744f980a2cf192813718bb60bbe
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-07 22:32:42 -0600

    Put alternative solution in separate file.

--- 2022/day_07.nim
+++ 2022/day_07.nim
@@ -51,27 +51,3 @@ part 2:
     let size = dir.size
     if size > needed: size
   ).min
-
-# ## Alternative fast solution:
-# let sizes = block:
-#   var sizes, stack: seq[int]
-
-#   proc stack_pop =
-#     sizes.add stack.pop
-#     stack[^1] += sizes[^1]
-
-#   for line in fetch(2022, 7).splitlines.mapIt(it.split):
-#     case line:
-#       of ["$", "cd", ".."]: stack_pop()
-#       of ["$", "cd", _]: stack.add 0
-#       of ["$", "ls"]: discard
-#       of ["dir", _]: discard
-#       of [@size, _]: stack[^1] += size.parseInt
-
-#   while stack.len > 1: stack_pop()
-#   sizes.add stack
-#   sizes
-
-# part 1: sum sizes.filterIt(it <= 100_000)
-
-# part 2: min sizes.filterIt(it > sizes[^1] - 40_000_000)
