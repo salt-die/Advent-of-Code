5f30b3a8d06532ee8001860cc666d5c6e53eb418
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-08 04:59:28 -0600

    Day 8 done!

--- 2022/day_08.py
+++ 2022/day_08.py
@@ -1,18 +1,41 @@
 import aoc_lube
+from aoc_lube.utils import int_grid
 
-RAW = aoc_lube.fetch(year=2022, day=8)
-print(RAW)
+import numpy as np
+import cv2
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+trees = int_grid(aoc_lube.fetch(year=2022, day=8)).astype(np.uint8)
 
 def part_one():
-    ...
+    visible = np.zeros_like(trees, bool)
+    visible[[0, -1]] = visible[:, [0, -1]] = True
+    kernel = np.array([[0, 1, 0], [0, 1, 0], [0, 0, 0]], np.uint8)
+
+    for i in range(4):
+        dilated = cv2.dilate(np.rot90(trees, i), kernel, iterations=98)
+        dilated[1:] -= dilated[:-1]
+        np.rot90(visible, i)[dilated.nonzero()] = True
+
+    return visible.sum()
 
 def part_two():
-    ...
+    kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
+    tall_trees = np.argwhere(cv2.dilate(trees, kernel) == trees) # Local maxima
+
+    score = 0
+    for y, x in tall_trees:
+        prod = 1
+        for s in (
+            np.s_[y, x - 100::-1],
+            np.s_[y, x + 1:],
+            np.s_[y - 100::-1, x],
+            np.s_[y + 1:, x],
+        ):
+            line = trees[y, x] > trees[s]
+            prod *= len(line) if line.all() else line.argmin() + 1
+        score = max(score, prod)
+
+    return score
 
 aoc_lube.submit(year=2022, day=8, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=8, part=2, solution=part_two)
