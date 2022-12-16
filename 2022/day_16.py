e7a155a9cfec19f8b0702dc9336fe8f20ff5df1e
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-16 14:52:21 -0600

    Refactor.

--- 2022/day_16.py
+++ 2022/day_16.py
@@ -9,33 +9,35 @@ def parse_raw():
         r"Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*)",
         aoc_lube.fetch(year=2022, day=16),
     )
-    G = nx.DiGraph()
-    valves = {}
+    tunnels = nx.DiGraph()
+    flows = {}
     for tunnel, flow, neighbors in parsed:
         if flow != "0":
-            valves[tunnel] = int(flow)
+            flows[tunnel] = int(flow)
         for neighbor in neighbors.split(", "):
-            G.add_edge(tunnel, neighbor)
+            tunnels.add_edge(tunnel, neighbor)
 
-    return dict(nx.all_pairs_shortest_path_length(G)), valves
+    return dict(nx.all_pairs_shortest_path_length(tunnels)), flows
 
-PATH_LENGTHS, FLOWS = parse_raw()
+DISTANCES, FLOWS = parse_raw()
+
+def _dfs(time, source, closed, leftover):
+    for target in closed:
+        if DISTANCES[source][target] < time:
+            time_left = time - DISTANCES[source][target] - 1
+            yield FLOWS[target] * time_left + dfs(time_left, target, closed - {target}, leftover)
 
 @cache
-def dfs(time, source='AA', closed=frozenset(FLOWS), elephant=False):
-    max_released = dfs(26, closed=closed) if elephant else 0
-    for target in closed:
-        if (length := PATH_LENGTHS[source][target]) < time:
-            time_left = time - length - 1
-            released = FLOWS[target] * time_left + dfs(time_left, target, closed - {target}, elephant)
-            max_released = max(max_released, released)
-    return max_released
+def dfs(time, source='AA', closed=frozenset(FLOWS), leftover=0):
+    if len(closed) == leftover:
+        return dfs(26, closed=closed)
+    return max(_dfs(time, source, closed, leftover), default=0)
 
 def part_one():
     return dfs(30)
 
 def part_two():
-    return dfs(26, elephant=True)
+    return dfs(26, leftover=9)
 
 aoc_lube.submit(year=2022, day=16, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=16, part=2, solution=part_two)
