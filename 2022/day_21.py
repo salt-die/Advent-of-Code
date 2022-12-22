54804d6971b97947d9db129cd291444594a04562
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-21 21:09:51 -0600

    Day 21 done!

--- 2022/day_21.py
+++ 2022/day_21.py
@@ -1,18 +1,46 @@
+import re
+from operator import add, mul, sub, truediv
+
 import aoc_lube
+from sympy import Equality, Symbol, solve
 
-RAW = aoc_lube.fetch(year=2022, day=21)
-print(RAW)
+NAMES = re.findall(r"(.*): (.*)", aoc_lube.fetch(year=2022, day=21))
+OPS = {"+": add, "*": mul, "-": sub, "/": truediv}
 
-def parse_raw():
-    ...
+class evaldict(dict):
+    def __getitem__(self, item):
+        value = super().__getitem__(item)
+        if not isinstance(value, tuple):
+            return value
 
-DATA = parse_raw()
+        op, lhs, rhs = value
+        value = op(self[lhs], self[rhs])
+        self[item] = value
+        return value
 
 def part_one():
-    ...
+    equations = evaldict()
+
+    for name, value in NAMES:
+        if value.isdigit():
+            equations[name] = int(value)
+        else:
+            lhs, op, rhs = value.split()
+            equations[name] = OPS[op], lhs, rhs
+
+    return round(equations["root"])
 
 def part_two():
-    ...
+    equations = evaldict()
+
+    for name, value in NAMES:
+        if value.isdigit():
+            equations[name] = Symbol("humn") if name == "humn" else int(value)
+        else:
+            lhs, op, rhs = value.split()
+            equations[name] = Equality if name == "root" else OPS[op], lhs, rhs
+
+    return round(solve(equations["root"])[0])
 
 aoc_lube.submit(year=2022, day=21, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=21, part=2, solution=part_two)
