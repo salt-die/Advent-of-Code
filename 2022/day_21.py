9fc7e60b3444a058df64f3dcf387ccc663a9251f
salt-die <53280662+salt-die@users.noreply.github.com>
2022-12-21 21:28:51 -0600

    Simplified.

--- 2022/day_21.py
+++ 2022/day_21.py
@@ -4,43 +4,27 @@ from operator import add, mul, sub, truediv
 import aoc_lube
 from sympy import Equality, Symbol, solve
 
-NAMES = re.findall(r"(.*): (.*)", aoc_lube.fetch(year=2022, day=21))
-OPS = {"+": add, "*": mul, "-": sub, "/": truediv}
+OPS = {"+": add, "*": mul, "-": sub, "/": truediv, "==": Equality}
+EQUATIONS = {
+    name: int(value) if value.isdigit() else value.split()
+    for name, value in re.findall(r"(.*): (.*)", aoc_lube.fetch(year=2022, day=21))
+}
 
-class evaldict(dict):
-    def __getitem__(self, item):
-        value = super().__getitem__(item)
-        if not isinstance(value, tuple):
-            return value
-
-        op, lhs, rhs = value
-        value = op(self[lhs], self[rhs])
-        self[item] = value
+def evaluate(name):
+    value = EQUATIONS[name]
+    if not isinstance(value, list):
         return value
 
+    lhs, op, rhs = value
+    return OPS[op](evaluate(lhs), evaluate(rhs))
+
 def part_one():
-    equations = evaldict()
-
-    for name, value in NAMES:
-        if value.isdigit():
-            equations[name] = int(value)
-        else:
-            lhs, op, rhs = value.split()
-            equations[name] = OPS[op], lhs, rhs
-
-    return round(equations["root"])
+    return round(evaluate("root"))
 
 def part_two():
-    equations = evaldict()
-
-    for name, value in NAMES:
-        if value.isdigit():
-            equations[name] = Symbol("humn") if name == "humn" else int(value)
-        else:
-            lhs, op, rhs = value.split()
-            equations[name] = Equality if name == "root" else OPS[op], lhs, rhs
-
-    return round(solve(equations["root"])[0])
+    EQUATIONS["humn"] = Symbol("humn")
+    EQUATIONS["root"][1] = "=="
+    return round(solve(evaluate("root"))[0])
 
 aoc_lube.submit(year=2022, day=21, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=21, part=2, solution=part_two)
