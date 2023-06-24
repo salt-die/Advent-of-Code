312841e15aa9a0e39995a45886d930b31aaca2e6
salt-die <53280662+salt-die@users.noreply.github.com>
2023-06-24 01:29:56 -0500

    Day 3 done.

--- 2016/day_03.py
+++ 2016/day_03.py
@@ -1,18 +1,17 @@
 import aoc_lube
+import numpy as np
 
-RAW = aoc_lube.fetch(year=2016, day=3)
-print(RAW)
+DATA = np.fromstring(aoc_lube.fetch(year=2016, day=3), dtype=int, sep=" ").reshape(-1, 3)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+def nvalid(data):
+    triangles = np.sort(data)
+    return ((triangles[:, :2].sum(axis=1) - triangles[:, 2]) > 0).sum()
 
 def part_one():
-    ...
+    return nvalid(DATA)
 
 def part_two():
-    ...
+    return nvalid(DATA.T.reshape(-1, 3))
 
 aoc_lube.submit(year=2016, day=3, part=1, solution=part_one)
 aoc_lube.submit(year=2016, day=3, part=2, solution=part_two)
