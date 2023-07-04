6b5fe29fb3bc69aab279611ff40f61e42f0c1116
salt-die <53280662+salt-die@users.noreply.github.com>
2023-07-03 20:53:28 -0500

    Day 6 finished!

--- 2016/day_06.py
+++ 2016/day_06.py
@@ -1,18 +1,21 @@
+from collections import Counter
+
 import aoc_lube
+from aoc_lube.utils import distribute
 
-RAW = aoc_lube.fetch(year=2016, day=6)
-print(RAW)
+def parse_data():
+    coded = distribute(aoc_lube.fetch(year=2016, day=6).replace("\n", ""), 8)
+    for it in coded:
+        (a, _), *_, (b, _) = Counter(it).most_common()
+        yield a, b
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
+COUNTS = list(parse_data())
 
 def part_one():
-    ...
+    return "".join(a for a, _ in COUNTS)
 
 def part_two():
-    ...
+    return "".join(b for _, b in COUNTS)
 
 aoc_lube.submit(year=2016, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2016, day=6, part=2, solution=part_two)
