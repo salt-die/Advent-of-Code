a673127b7dec90956b84d5cb7e17159c363e9692
salt-die <53280662+salt-die@users.noreply.github.com>
2023-07-14 16:40:40 -0500

    Day 11 done!

--- 2016/day_11.py
+++ 2016/day_11.py
@@ -1,18 +1,74 @@
+from collections import deque
+from itertools import combinations, product
+import re
+
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2016, day=11)
-print(RAW)
+def initial_config():
+    item_re = re.compile(r"(\w+)(-compatible microchip| generator)")
+    seen = {}
+    names = iter("abcde")
+    for line in aoc_lube.fetch(year=2016, day=11).splitlines():
+        floor = set()
+        for name, kind in item_re.findall(line):
+            if name not in seen:
+                seen[name] = next(names)
+            floor.add(seen[name] + kind[1])
+        yield frozenset(floor)
 
-def parse_raw():
-    ...
+INITIAL_CONFIG = tuple(initial_config())
 
-DATA = parse_raw()
+def is_valid_state(floors):
+    return all(
+        f"{a}g" in floor
+        for floor in floors if any(b == "g" for _, b in floor)
+        for a, b in floor if b == "c"
+    )
+
+def move_items(floors, elevator, dest, items):
+    return tuple(
+        floor ^ items if i == elevator else
+        floor | items if i == dest else
+        floor
+        for i, floor in enumerate(floors)
+    )
+
+def hash_state(floors, elevator):
+    n = elevator
+    for i, floor in enumerate(floors, start=1):
+        n += 16 ** (2 * i - 1) * len(floor)
+        n += 16 ** (2 * i) * sum(b == "g" for _, b in floor)
+    return n
+
+def nmoves(initial_state):
+    queue = deque([(initial_state, 0, 0)])
+    seen = set()
+    while queue:
+        floors, elevator, nmoves = queue.popleft()
+
+        if not any(floors[:3]):
+            return nmoves
+
+        if (hash_ := hash_state(floors, elevator)) in seen:
+            continue
+
+        seen.add(hash_)
+
+        for nitems, direction in product((1, 2), (-1, 1)):
+            for items in combinations(floors[elevator], nitems):
+                if (dest := elevator + direction) < 0 or dest >= 4:
+                    continue
+
+                new_floors = move_items(floors, elevator, dest, set(items))
+                if is_valid_state(new_floors):
+                    queue.append((new_floors, dest, nmoves + 1))
 
 def part_one():
-    ...
+    return nmoves(INITIAL_CONFIG)
 
 def part_two():
-    ...
+    a, b, c, d = INITIAL_CONFIG
+    return nmoves((a | {"yc", "yg", "zc", "zg"}, b, c, d))
 
 aoc_lube.submit(year=2016, day=11, part=1, solution=part_one)
 aoc_lube.submit(year=2016, day=11, part=2, solution=part_two)
