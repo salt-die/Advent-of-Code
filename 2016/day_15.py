91dd2421df3dbc0459318bbf53f970a170011041
salt-die <53280662+salt-die@users.noreply.github.com>
2023-07-20 19:44:36 -0500

    Cleaning.

--- 2016/day_15.py
+++ 2016/day_15.py
@@ -1,12 +1,11 @@
 import aoc_lube
-from aoc_lube.utils import extract_ints, chunk, chinese_remainder_theorem
+from aoc_lube.utils import chinese_remainder_theorem, chunk, extract_ints
 
-RAW = aoc_lube.fetch(year=2016, day=15)
-DISCS = [(b, d) for _, b, _, d in chunk(extract_ints(RAW), 4)]
+DISCS = [(b, d) for _, b, _, d in chunk(extract_ints(aoc_lube.fetch(year=2016, day=15)), 4)]
 
 def solve_discs():
     moduli = [a for a, _ in DISCS]
-    residues = [a - b - i for i, (a, b) in enumerate(DISCS, start=1)]
+    residues = [-b - i for i, (_, b) in enumerate(DISCS, start=1)]
     return chinese_remainder_theorem(moduli, residues)
 
 aoc_lube.submit(year=2016, day=15, part=1, solution=solve_discs)
