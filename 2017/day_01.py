8cc202271e12213a11eb4ae7b74b9ee50be8a786
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-30 17:55:36 -0600

    Warming up...

--- 2017/day_01.py
+++ 2017/day_01.py
@@ -1,18 +1,19 @@
 import aoc_lube
+from aoc_lube.utils import sliding_window_cycle
 
 RAW = aoc_lube.fetch(year=2017, day=1)
-print(RAW)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    return sum(int(a) if a == b else 0 for a, b in sliding_window_cycle(RAW))
+
 
 def part_two():
-    ...
+    return sum(
+        int(a) if a == b else 0
+        for a, *_, b in sliding_window_cycle(RAW, len(RAW) // 2 - 1)
+    )
+
 
 aoc_lube.submit(year=2017, day=1, part=1, solution=part_one)
 aoc_lube.submit(year=2017, day=1, part=2, solution=part_two)
