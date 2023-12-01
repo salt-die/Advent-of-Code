31e751b5e4ab13bdef5491a6b1c99e2c51a8962b
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-01 00:41:15 -0600

    Day 1 done!

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -1,18 +1,27 @@
+import re
+
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2023, day=1)
-print(RAW)
+LINES = aoc_lube.fetch(year=2023, day=1).splitlines()
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    total = 0
+    for line in LINES:
+        a, *_, b = re.findall(r"\d", 2 * line)
+        total += int(a + b)
+    return total
+
 
 def part_two():
-    ...
+    digits = "zero|one|two|three|four|five|six|seven|eight|nine"
+    to_digit = {digit: str(i) for i, digit in enumerate(digits.split("|"))}
+    total = 0
+    for line in LINES:
+        a, *_, b = re.findall(rf"(?=({digits}|\d))", 2 * line)
+        total += int(to_digit.get(a, a) + to_digit.get(b, b))
+    return total
+
 
 aoc_lube.submit(year=2023, day=1, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=1, part=2, solution=part_two)
