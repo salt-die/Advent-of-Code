c494d29bd356b8fe12ca8556d576c33c5cfac28c
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-01 06:30:17 -0600

    Visualization can now be resized.

--- 2023/visuals/01_Trebuchet/__main__.py
+++ 2023/visuals/01_Trebuchet/__main__.py
@@ -12,8 +12,7 @@ from batgrl.gadgets.texture_tools import read_texture
 AOC_BLUE = Color.from_hex("0f0f23")
 BLUISH = lerp_colors(AOC_BLUE, WHITE, 0.25)
 AOC_GREY = Color.from_hex("cccccc")
-AOC_PRIMARY = ColorPair.from_colors(AOC_GREY, AOC_BLUE)
-AOC_SECONDARY = ColorPair.from_colors(WHITE, AOC_BLUE)
+PRIMARY = ColorPair.from_colors(WHITE, AOC_BLUE)
 LINES = aoc_lube.fetch(year=2023, day=1).splitlines()
 
 
@@ -29,30 +28,36 @@ def get_trebuchet_frames():
 
 class TrebuchetApp(App):
     async def on_start(self):
-        text = Text(
-            default_color_pair=AOC_SECONDARY,
-            size=(20, max(len(line) for line in LINES) + 8),
-        )
-        text.add_str("TOTAL", pos=(-1, -14))
-        text.canvas["char"][:, -8] = "┃"
-        text.add_str("┣━━━━━━━", pos=(-2, -8))
-        text.add_str("┃     0 ", pos=(-1, -8))
+        MAX_INPUT_LENGTH = max(len(line) for line in LINES)
+        text = Text(default_color_pair=PRIMARY, size=(1, MAX_INPUT_LENGTH + 9))
 
-        treb = Animation.from_textures(
-            list(get_trebuchet_frames()), size=(20, 80), pos=(0, 20)
+        total_label = Text(
+            default_color_pair=PRIMARY,
+            size=(2, text.width),
+            pos_hint={"y_hint": 1.0, "anchor": "bottom"},
         )
+        total_label.add_str("      ┣━━━━━━━", pos=(-2, -14))
+        total_label.add_str("TOTAL ┃     0 ", pos=(-1, -14))
+
+        treb = Animation.from_textures(list(get_trebuchet_frames()))
         treb.left = text.right
         treb.play()
-        self.add_gadgets(text, treb)
+
+        def fix_size_and_pos():
+            H, W = self.root.size
+            treb.size = H, W - text.width
+            text.bottom = self.root.bottom - 2
+
+        fix_size_and_pos()
+        text.subscribe(self.root, "size", fix_size_and_pos)
+        self.add_gadgets(text, total_label, treb)
 
         digits = "zero|one|two|three|four|five|six|seven|eight|nine"
         to_digit = {digit: str(i) for i, digit in enumerate(digits.split("|"))}
         total = 0
         for line in LINES:
-            text.canvas[:-3] = text.canvas[1:-2]
-            text.colors[:-3] = text.colors[1:-2]
-            text.canvas[-3] = text.default_char
-            text.colors[-3] = AOC_PRIMARY
+            text.height += 1
+            text.y -= 1
 
             matches = [
                 (m.start(1), m.end(1), m.group(1))
@@ -62,26 +67,26 @@ class TrebuchetApp(App):
             b = matches[-1][2]
             ab = int(to_digit.get(a, a) + to_digit.get(b, b))
             total += ab
-            text.add_str(f"┃    {ab}", (-3, -8))
-            text.add_str(str(total).rjust(5), (-1, -6))
-            text.add_str(line, pos=(-3, 0))
+
+            text.add_str(f"{line:<{MAX_INPUT_LENGTH}} ┃    {ab} ", pos=(-1, 0))
+            total_label.add_str(str(total).rjust(5), pos=(-1, -6))
 
             # Color matches
             for p in range(21):
-                text.colors[-3, :-8, :3] = lerp_colors(AOC_GREY, RED, p / 20)
+                text.colors[-1, :-8, :3] = lerp_colors(WHITE, RED, p / 20)
                 for a, b, _ in matches:
-                    text.colors[-3, a:b, :3] = lerp_colors(AOC_GREY, GREEN, p / 20)
+                    text.colors[-1, a:b, :3] = lerp_colors(WHITE, GREEN, p / 20)
                 await asyncio.sleep(0.03)
 
             # Color first and last
             for p in range(21):
-                text.colors[-3, :-8, :3] = lerp_colors(RED, AOC_BLUE, p / 25)
+                text.colors[-1, :-8, :3] = lerp_colors(RED, AOC_BLUE, p / 25)
                 for a, b, _ in matches:
-                    text.colors[-3, a:b, :3] = lerp_colors(GREEN, AOC_BLUE, p / 25)
-                text.colors[-3, matches[0][0] : matches[0][1], :3] = GREEN
-                text.colors[-3, matches[-1][0] : matches[-1][1], :3] = GREEN
+                    text.colors[-1, a:b, :3] = lerp_colors(GREEN, AOC_BLUE, p / 25)
+                text.colors[-1, matches[0][0] : matches[0][1], :3] = GREEN
+                text.colors[-1, matches[-1][0] : matches[-1][1], :3] = GREEN
                 await asyncio.sleep(0.03)
 
 
 if __name__ == "__main__":
-    TrebuchetApp(title="Trebuchet?!", background_color_pair=AOC_PRIMARY).run()
+    TrebuchetApp(title="Trebuchet?!", background_color_pair=PRIMARY).run()
