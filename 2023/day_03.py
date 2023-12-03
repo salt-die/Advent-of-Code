ff61507a34336ef4239a0e069da9062fefd55983
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-03 05:53:37 -0600

    Cleaning.

--- 2023/day_03.py
+++ 2023/day_03.py
@@ -1,45 +1,38 @@
 import re
-from math import prod
 
 import aoc_lube
 
 GRID = aoc_lube.fetch(year=2023, day=3).splitlines()
-MATCHES = [list(re.finditer(r"(\d+)", line)) for line in GRID]
-SYMBOLS = {}
+H, W = len(GRID), len(GRID[0])
+MATCHES = [
+    [(int(m[1]), m.span()) for m in re.finditer(r"(\d+)", line)] for line in GRID
+]
+COORDS = {}
 
 
-def find_symbol(y, match):
+def find_symbol(y, x1, x2):
     for v in [y - 1, y, y + 1]:
-        for u in range(match.start() - 1, match.end() + 1):
-            if (
-                0 <= v < len(GRID)
-                and 0 <= u < len(GRID[0])
-                and GRID[v][u] != "."
-                and not GRID[v][u].isdigit()
-            ):
+        for u in range(x1 - 1, x2 + 1):
+            if 0 <= v < H and 0 <= u < W and GRID[v][u] not in ".0123456789":
                 return v, u
 
 
 def find_all_symbols():
     for y, line in enumerate(MATCHES):
-        for match in line:
-            if symbol := find_symbol(y, match):
-                SYMBOLS.setdefault(symbol, []).append(int(match[1]))
+        for part, (x1, x2) in line:
+            if coord := find_symbol(y, x1, x2):
+                COORDS.setdefault(coord, []).append(part)
 
 
 find_all_symbols()
 
 
 def part_one():
-    return sum(sum(parts) for parts in SYMBOLS.values())
+    return sum(sum(parts) for parts in COORDS.values())
 
 
 def part_two():
-    return sum(
-        prod(parts)
-        for (y, x), parts in SYMBOLS.items()
-        if GRID[y][x] == "*" and len(parts) == 2
-    )
+    return sum(a * b[0] for a, *b in COORDS.values() if b)
 
 
 aoc_lube.submit(year=2023, day=3, part=1, solution=part_one)
