f4f2e56eef9876caf5125535f8f218e19518f2f9
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-03 23:53:36 -0600

    Day 4 done!

--- 2023/day_04.py
+++ 2023/day_04.py
@@ -1,18 +1,26 @@
 import aoc_lube
+import numpy as np
 
-RAW = aoc_lube.fetch(year=2023, day=4)
-print(RAW)
 
 def parse_raw():
-    ...
+    for line in aoc_lube.fetch(year=2023, day=4).splitlines():
+        ints = list(aoc_lube.utils.extract_ints(line))
+        yield len(set(ints[1:11]) & set(ints[11:]))
 
-DATA = parse_raw()
+
+MATCHES = np.fromiter(parse_raw(), int)
+
 
 def part_one():
-    ...
+    return (2 ** (MATCHES[MATCHES > 0] - 1)).sum()
+
 
 def part_two():
-    ...
+    copies = np.ones_like(MATCHES)
+    for i, matches in enumerate(MATCHES):
+        copies[i + 1 : i + 1 + matches] += copies[i]
+    return copies.sum()
+
 
 aoc_lube.submit(year=2023, day=4, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=4, part=2, solution=part_two)
