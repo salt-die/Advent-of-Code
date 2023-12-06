cb42f276162bde02882bf39237820afb1b89306a
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-06 00:16:05 -0600

    Day 6 done!

--- 2023/day_06.py
+++ 2023/day_06.py
@@ -1,18 +1,28 @@
+import re
+from math import prod
+
 import aoc_lube
+from numpy import diff, roots
 
-RAW = aoc_lube.fetch(year=2023, day=6)
-print(RAW)
+DATA = re.findall(r"\d+", aoc_lube.fetch(year=2023, day=6))
+TIMES = map(int, DATA[:4])
+DISTANCES = map(int, DATA[4:])
+TIME = int("".join(DATA[:4]))
+DISTANCE = int("".join(DATA[4:]))
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def n_ways(time, distance):
+    return diff(roots([1, time, distance]).astype(int)).item()
+
 
 def part_one():
-    ...
+    return prod(map(n_ways, TIMES, DISTANCES))
+
 
 def part_two():
-    ...
+    return n_ways(TIME, DISTANCE)
 
+
+print(part_one(), part_two())
 aoc_lube.submit(year=2023, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=6, part=2, solution=part_two)
