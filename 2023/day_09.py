d5a0fc5ea91a61affca1898d547e260e425a8e37
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-09 02:53:46 -0600

    Rename function for flavor.

--- 2023/day_09.py
+++ 2023/day_09.py
@@ -8,16 +8,16 @@ DATA = [
 ]
 
 
-def predict(data):
+def extrapolate(data):
     return sum(np.diff(data, n=i)[-1] for i in range(len(data)))
 
 
 def part_one():
-    return sum(predict(datum) for datum in DATA)
+    return sum(extrapolate(datum) for datum in DATA)
 
 
 def part_two():
-    return sum(predict(datum[::-1]) for datum in DATA)
+    return sum(extrapolate(datum[::-1]) for datum in DATA)
 
 
 aoc_lube.submit(year=2023, day=9, part=1, solution=part_one)
