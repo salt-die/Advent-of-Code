6ef37140192bc878af57d7ebed6b6fe7e57f8686
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-08 23:42:52 -0600

    Day 9 done!

--- 2023/day_09.py
+++ 2023/day_09.py
@@ -1,18 +1,24 @@
 import aoc_lube
+import numpy as np
+from aoc_lube.utils import extract_ints
 
-RAW = aoc_lube.fetch(year=2023, day=9)
-print(RAW)
+DATA = [
+    np.fromiter(extract_ints(line), int)
+    for line in aoc_lube.fetch(year=2023, day=9).splitlines()
+]
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def predict(data):
+    return sum(np.diff(data, n=i)[-1] for i in range(len(data)))
+
 
 def part_one():
-    ...
+    return sum(predict(datum) for datum in DATA)
+
 
 def part_two():
-    ...
+    return sum(predict(datum[::-1]) for datum in DATA)
+
 
 aoc_lube.submit(year=2023, day=9, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=9, part=2, solution=part_two)
