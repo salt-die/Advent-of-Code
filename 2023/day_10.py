4a188360cbd07e714760e5d206702998e8544f3d
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-10 12:37:56 -0600

    No tuples.

--- 2023/day_10.py
+++ 2023/day_10.py
@@ -1,65 +1,60 @@
-from typing import NamedTuple
-
 import aoc_lube
-import networkx as nx
 
-
-class Point(NamedTuple):
-    y: int
-    x: int
-
-    def __add__(self, other):
-        return Point(self.y + other.y, self.x + other.x)
-
-
-DIRS = [N, E, S, W] = [Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1)]
-Δs = {"|": (N, S), "J": (N, W), "L": (N, E), "7": (S, W), "F": (S, E), "-": (W, E)}
+N, E, S, W = -1j, 1, 1j, -1
+Δs = {
+    "|": (N, S),
+    "J": (N, W),
+    "L": (N, E),
+    "7": (S, W),
+    "F": (S, E),
+    "-": (W, E),
+    "S": (N, E, S, W),
+    ".": (),
+}
 
 
 def parse_raw():
-    grid = [list(line) for line in aoc_lube.fetch(year=2023, day=10).splitlines()]
+    grid = {}
+    edges = set()
+    for y, line in enumerate(aoc_lube.fetch(year=2023, day=10).splitlines()):
+        for x, char in enumerate(line):
+            pos = complex(x, y)
+            grid[pos] = char
+            for Δ in Δs[char]:
+                edges.add((pos, pos + Δ))
+
+            if char == "S":
+                cycle = {pos}
+                stack = [pos]
+
+    yield y + 1, x + 1
     yield grid
 
-    edges = set()
-    for y, line in enumerate(grid):
-        for x, char in enumerate(line):
-            pos = Point(y, x)
-            if char in Δs:
-                Δ1, Δ2 = Δs[char]
-                edges.add((pos, pos + Δ1))
-                edges.add((pos, pos + Δ2))
-            elif char == "S":
-                start = pos
-                for Δ in DIRS:
-                    edges.add((pos, pos + Δ))
-
-    for pipe, (Δ1, Δ2) in Δs.items():
-        if (start + Δ1, start) in edges and (start + Δ2, start) in edges:
-            grid[start.y][start.x] = pipe
-            break
-
-    H = nx.Graph(((u, v) for u, v in edges if (v, u) in edges))
-    for component in nx.connected_components(H):
-        if start in component:
-            yield component
-            return
+    while stack:
+        pos = stack.pop()
+        for Δ in Δs[grid[pos]]:
+            if pos + Δ not in cycle:
+                cycle.add(pos + Δ)
+                stack.append(pos + Δ)
+    yield cycle
 
 
-GRID, PIPES = parse_raw()
+(H, W), GRID, CYCLE = parse_raw()
 
 
 def part_one():
-    return len(PIPES) // 2
+    return len(CYCLE) // 2
 
 
 def part_two():
     ntiles = 0
-    for y, line in enumerate(GRID):
+    for y in range(H):
         inside = False
-        for x, pipe in enumerate(line):
-            if (y, x) not in PIPES:
+        for x in range(W):
+            pos = complex(x, y)
+            if pos not in CYCLE:
                 ntiles += inside
-            elif pipe in "|F7":
+            elif GRID[pos] in "|F7":
                 inside = not inside
     return ntiles
 
