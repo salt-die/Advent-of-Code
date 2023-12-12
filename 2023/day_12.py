b2226dd24efab207d55457a7e5870cf4582ddae7
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-12 02:27:03 -0600

    Day 12 done!

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,18 +1,53 @@
+from functools import cache
+
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2023, day=12)
-print(RAW)
 
 def parse_raw():
-    ...
+    for line in aoc_lube.fetch(year=2023, day=12).splitlines():
+        springs, s = line.split()
+        yield springs, tuple(map(int, s.split(",")))
+
 
 DATA = parse_raw()
 
+
+def total_combinations(springs, groups):
+    @cache
+    def combinations(i, j, r):
+        if j == len(groups):
+            return springs.count("#", i) == 0
+        if i == len(springs):
+            return j == len(groups) - 1 and r == groups[j]
+        if springs[i] == "#":
+            return broken(i, j, r)
+        if springs[i] == ".":
+            return working(i, j, r)
+        return broken(i, j, r) + working(i, j, r)
+
+    def broken(i, j, r):
+        return combinations(i + 1, j, r + 1)
+
+    def working(i, j, r):
+        if r == groups[j]:
+            return combinations(i + 1, j + 1, 0)
+        if r == 0:
+            return combinations(i + 1, j, 0)
+        return 0
+
+    return combinations(0, 0, 0)
+
+
 def part_one():
-    ...
+    return sum(total_combinations(springs, groups) for springs, groups in DATA)
+
 
 def part_two():
-    ...
+    return sum(
+        total_combinations("?".join([springs] * 5), groups * 5)
+        for springs, groups in DATA
+    )
+
 
 aoc_lube.submit(year=2023, day=12, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=12, part=2, solution=part_two)
