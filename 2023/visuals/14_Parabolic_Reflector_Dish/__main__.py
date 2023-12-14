64e1adb5198582658bc5145b904a49199959414c
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-14 02:49:30 -0600

    Direction buttons stop auto.

--- 2023/visuals/14_Parabolic_Reflector_Dish/__main__.py
+++ 2023/visuals/14_Parabolic_Reflector_Dish/__main__.py
@@ -78,28 +78,34 @@ class ReflectorApp(App):
         direction = "e"
         button_event = asyncio.Event()
 
+        auto_button = AocToggle("AUTO", lambda _: button_event.set(), pos=(4, 1))
+
         def dir_callback(dir):
             def callback():
                 nonlocal direction
                 direction = dir
                 button_event.set()
+                auto_button.toggle_state = "off"
 
             return callback
 
         def reset():
             global GRID
+            nonlocal direction
             GRID = np.array(
                 [list(line) for line in aoc_lube.fetch(2023, 14).splitlines()]
             )
             rocks.rocks = GRID[:H, :W]
             rocks.canvas["char"] = rocks.rocks
+            direction = "e"
+            if button_event.is_set():
+                button_event.clear()
 
         north_button = AocButton("^", dir_callback("n"), pos=(0, 4))
         west_button = AocButton("<", dir_callback("w"), pos=(1, 0))
         south_button = AocButton("v", dir_callback("s"), pos=(2, 4))
         east_button = AocButton(">", dir_callback("e"), pos=(1, 8))
 
-        auto_button = AocToggle("AUTO", lambda _: button_event.set(), pos=(4, 1))
         reset_button = AocButton("RESET", reset, pos=(5, 2))
 
         button_container = Gadget(size=(6, 11), is_transparent=True)
@@ -121,7 +127,6 @@ class ReflectorApp(App):
 
             direction_label.is_enabled = False
 
-            button_event.clear()
             await button_event.wait()
 
             direction_label.is_enabled = True
@@ -129,6 +134,8 @@ class ReflectorApp(App):
             if auto_button.toggle_state == "off":
                 direction_label.add_str(f"Falling {DIR_NAMES[direction]}")
                 await rocks.roll(direction)
+
+            button_event.clear()
 
 
 if __name__ == "__main__":
