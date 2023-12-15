33e736834b81ee3a3be2a50e670d3b49b63325e3
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-14 23:43:16 -0600

    Day 15 done!

--- 2023/day_15.py
+++ 2023/day_15.py
@@ -1,18 +1,35 @@
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2023, day=15)
-print(RAW)
+INIT_SEQUENCE = aoc_lube.fetch(year=2023, day=15).split(",")
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def hash(string):
+    total = 0
+    for char in string:
+        total += ord(char)
+        total *= 17
+    return total % 256
+
 
 def part_one():
-    ...
+    return sum(map(hash, INIT_SEQUENCE))
+
 
 def part_two():
-    ...
+    boxes = [{} for _ in range(256)]
+    for s in INIT_SEQUENCE:
+        if s.endswith("-"):
+            box = s[:-1]
+            boxes[hash(box)].pop(box, None)
+        else:
+            box, n = s.split("=")
+            boxes[hash(box)][box] = int(n)
+    return sum(
+        i * j * focal
+        for i, box in enumerate(boxes, start=1)
+        for j, focal in enumerate(box.values(), start=1)
+    )
+
 
 aoc_lube.submit(year=2023, day=15, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=15, part=2, solution=part_two)
