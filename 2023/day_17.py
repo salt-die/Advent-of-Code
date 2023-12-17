6c8f482c1d7a6cebf0ad2c64b4b5177314f5f5ef
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-17 01:24:49 -0600

    Day 17 done!

--- 2023/day_17.py
+++ 2023/day_17.py
@@ -1,18 +1,37 @@
+from heapq import heappop, heappush
+
 import aoc_lube
+from aoc_lube.utils import int_grid
 
-RAW = aoc_lube.fetch(year=2023, day=17)
-print(RAW)
+GRID = int_grid(aoc_lube.fetch(year=2023, day=17))
+H, W = GRID.shape
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def min_heatloss(mn, mx):
+    q = [(0, 0, 0, 1, 0), (0, 0, 0, 0, 1)]
+    heatlosses = {}
+    while q:
+        cost, y, x, dy, dx = heappop(q)
+        if y == H - 1 and x == W - 1:
+            return cost
 
-def part_one():
-    ...
+        for dy, dx in ((-dx, dy), (dx, -dy)):
+            new_heatloss = 0
+            for d in range(1, mx + 1):
+                v = y + dy * d
+                u = x + dx * d
 
-def part_two():
-    ...
+                if not (0 <= v < H and 0 <= u < W):
+                    break
 
-aoc_lube.submit(year=2023, day=17, part=1, solution=part_one)
-aoc_lube.submit(year=2023, day=17, part=2, solution=part_two)
+                new_heatloss += GRID[v, u]
+
+                if d >= mn:
+                    new_cost = cost + new_heatloss
+                    if heatlosses.get((v, u, dy, dx), float("inf")) > new_cost:
+                        heatlosses[v, u, dy, dx] = new_cost
+                        heappush(q, (new_cost, v, u, dy, dx))
+
+
+aoc_lube.submit(2023, 17, 1, lambda: min_heatloss(1, 3))
+aoc_lube.submit(2023, 17, 2, lambda: min_heatloss(4, 10))
