97f5e00b7acf1449c2a32c6afeebfb73df1f6aa0
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-18 00:52:43 -0600

    Day 18 done!

--- 2023/day_18.py
+++ 2023/day_18.py
@@ -1,18 +1,33 @@
 import aoc_lube
+import shapely
+from aoc_lube.utils import distribute
 
-RAW = aoc_lube.fetch(year=2023, day=18)
-print(RAW)
 
 def parse_raw():
-    ...
+    to_dydx = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+    to_dydx |= {d: v for d, v in zip("3120", to_dydx.values())}
+    for line in aoc_lube.fetch(2023, 18).splitlines():
+        dir, n, color = line.split()
+        yield to_dydx[dir], int(n)
+        yield to_dydx[color[-2]], int(color[2:-2], 16)
 
-DATA = parse_raw()
 
-def part_one():
-    ...
+def points(data):
+    y = x = 0
+    yield y, x
+    for (dy, dx), n in data:
+        y += dy * n
+        x += dx * n
+        yield y, x
 
-def part_two():
-    ...
 
-aoc_lube.submit(year=2023, day=18, part=1, solution=part_one)
-aoc_lube.submit(year=2023, day=18, part=2, solution=part_two)
+PART1, PART2 = map(points, distribute(parse_raw(), 2))
+
+
+def polyarea(points):
+    poly = shapely.Polygon(points)
+    return int(poly.area + poly.length / 2 + 1)
+
+
+aoc_lube.submit(year=2023, day=18, part=1, solution=lambda: polyarea(PART1))
+aoc_lube.submit(year=2023, day=18, part=2, solution=lambda: polyarea(PART2))
