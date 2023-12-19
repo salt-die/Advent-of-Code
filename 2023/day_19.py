1e6e369829ddf92ceca015d27fec176e8e488f48
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-19 01:56:15 -0600

    Remove print.

--- 2023/day_19.py
+++ 2023/day_19.py
@@ -86,6 +86,5 @@ def part_two():
     return sum(nvalid("in", dict(zip("xmas", ((1, 4001),) * 4))))
 
 
-print(part_two())
 aoc_lube.submit(year=2023, day=19, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=19, part=2, solution=part_two)
