e492db518a236dcf65e609f2569479314174389d
salt-die <53280662+salt-die@users.noreply.github.com>
2023-12-21 03:22:40 -0600

    Day 21 done!

--- 2023/day_21.py
+++ 2023/day_21.py
@@ -1,18 +1,41 @@
+from itertools import product
+
 import aoc_lube
+from aoc_lube.utils import GRID_NEIGHBORHOODS
+from scipy.interpolate import lagrange
 
 RAW = aoc_lube.fetch(year=2023, day=21)
-print(RAW)
+GRID = RAW.splitlines()
+H = len(GRID)
+START = next((y, x) for (y, x) in product(range(H), repeat=2) if GRID[y][x] == "S")
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def step(nodes):
+    return {
+        (y + dy, x + dx)
+        for y, x in nodes
+        for dy, dx in GRID_NEIGHBORHOODS[4]
+        if GRID[(y + dy) % H][(x + dx) % H] != "#"
+    }
+
 
 def part_one():
-    ...
+    nodes = [START]
+    for _ in range(64):
+        nodes = step(nodes)
+    return len(nodes)
+
 
 def part_two():
-    ...
+    xs = [65, 65 + H, 65 + 2 * H]
+    ys = []
+    nodes = [START]
+    for i in range(xs[-1]):
+        nodes = step(nodes)
+        if i + 1 in xs:
+            ys.append(len(nodes))
+    return round(lagrange(xs, ys)(26501365))
+
 
 aoc_lube.submit(year=2023, day=21, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=21, part=2, solution=part_two)
