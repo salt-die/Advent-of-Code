0507e9609d2987dcd2017d8dbc6e37f09a5c6bbc
salt-die <salt-die@protonmail.com>
2024-11-23 22:11:26 -0600

    Prepare for 2024

---
+++ 2024/day_17.py
@@ -0,0 +1,18 @@
+import aoc_lube
+
+RAW = aoc_lube.fetch(year=2024, day=17)
+print(RAW)
+
+def parse_raw():
+    ...
+
+DATA = parse_raw()
+
+def part_one():
+    ...
+
+def part_two():
+    ...
+
+aoc_lube.submit(year=2024, day=17, part=1, solution=part_one)
+aoc_lube.submit(year=2024, day=17, part=2, solution=part_two)
