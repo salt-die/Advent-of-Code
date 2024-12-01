5bfb8b10e93984f8933448d48133714f69fc235b
salt-die <salt-die@protonmail.com>
2024-11-30 23:13:07 -0600

    Day 1 done!

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -1,18 +1,20 @@
+from collections import Counter
+
 import aoc_lube
+from aoc_lube.utils import distribute, extract_ints
 
-RAW = aoc_lube.fetch(year=2024, day=1)
-print(RAW)
+L, R = distribute(extract_ints(aoc_lube.fetch(year=2024, day=1)), 2)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    return sum(abs(i - j) for i, j in zip(sorted(L), sorted(R)))
+
 
 def part_two():
-    ...
+    a = Counter(L)
+    b = Counter(R)
+    return sum(i * a[i] * b[i] for i in a)
+
 
 aoc_lube.submit(year=2024, day=1, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=1, part=2, solution=part_two)
