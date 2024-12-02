d77a24ee69b85cf3b7316695fb002e5ff89c9b03
salt-die <salt-die@protonmail.com>
2024-12-01 23:54:22 -0600

    Day 2 done!

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -1,18 +1,28 @@
 import aoc_lube
+from aoc_lube.utils import extract_ints, sliding_window
 
-RAW = aoc_lube.fetch(year=2024, day=2)
-print(RAW)
+DATA = [
+    list(extract_ints(line)) for line in aoc_lube.fetch(year=2024, day=2).splitlines()
+]
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def is_safe(report):
+    a, b = report[:2]
+    Δ = (-1) ** (a < b)
+    return all(0 < Δ * (a - b) < 4 for a, b in sliding_window(report, 2))
+
+
+def damp_safe(report):
+    return any(is_safe(report[:i] + report[i + 1 :]) for i in range(len(report)))
+
 
 def part_one():
-    ...
+    return sum(is_safe(report) for report in DATA)
+
 
 def part_two():
-    ...
+    return sum(damp_safe(report) for report in DATA)
+
 
 aoc_lube.submit(year=2024, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=2, part=2, solution=part_two)
