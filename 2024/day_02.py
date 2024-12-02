32766327c2c4e811c429cccd26cea0258f7df7f6
salt-die <salt-die@protonmail.com>
2024-12-02 02:25:42 -0600

    Alternative solution for part 2.

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -26,3 +26,34 @@ def part_two():
 
 aoc_lube.submit(year=2024, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=2, part=2, solution=part_two)
+
+
+# Alternatively, for a O(n) solution for part 2:
+#
+#
+# def sign_of_seq(report):
+#     a, b, c, d = report[:4]
+#     return sign(sign(b - a) + sign(c - b) + sign(d - c))
+#
+#
+# def damp_safe(report):
+#     sgn = sign_of_seq(report)
+#     if sgn == 0:
+#         return False
+#     for i, (a, b) in enumerate(sliding_window(report, 2)):
+#         if sign(b - a) != sgn:
+#             return (
+#                 is_safe(
+#                     report[:i] + report[i + 1 :]
+#                 )  # Should be able to determine which of
+#                 or is_safe(
+#                     report[: i + 1] + report[i + 2 :]
+#                 )  # these to use by the magnitude of b - a
+#             )
+#     if 0 < sgn * (b - a) < 4:
+#         return is_safe(report[1:])
+#     return is_safe(report[:-1])
+#
+#
+# def part_two():
+#     return sum(damp_safe(report) for report in DATA)
