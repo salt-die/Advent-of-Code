3ddd5a0a9d1e1119278b1302330f42674403a0f2
salt-die <salt-die@protonmail.com>
2024-12-07 19:23:37 -0600

    Reduce some args.

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -16,27 +16,32 @@ def unconcat(a, b):
     return a // (10 ** (int(log10(b)) + 1))
 
 
-def solve(target, operands, part):
-    *operands, operand = operands
-    if not operands:
-        return operand == target
-    if (
-        part == 2
-        and endswith(target, operand)
-        and solve(unconcat(target, operand), operands, part)
-    ):
-        return True
-    if target % operand == 0 and solve(target // operand, operands, part):
-        return True
-    return solve(target - operand, operands, part)
+def solve(equation, part):
+    target, *operands = equation
+
+    def _solve(target, index):
+        operand = operands[index]
+        if index == 0:
+            return operand == target
+        if (
+            part == 2
+            and endswith(target, operand)
+            and _solve(unconcat(target, operand), index - 1)
+        ):
+            return True
+        if target % operand == 0 and _solve(target // operand, index - 1):
+            return True
+        return _solve(target - operand, index - 1)
+
+    return target if _solve(target, len(operands) - 1) else 0
 
 
 def part_one():
-    return sum(target for target, *operands in DATA if solve(target, operands, 1))
+    return sum(solve(equation, 1) for equation in DATA)
 
 
 def part_two():
-    return sum(target for target, *operands in DATA if solve(target, operands, 2))
+    return sum(solve(equation, 2) for equation in DATA)
 
 
 aoc_lube.submit(year=2024, day=7, part=1, solution=part_one)
