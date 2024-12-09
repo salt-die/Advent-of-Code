14deb894f4d6a2dfbfc911c740f36240729c2f18
salt-die <salt-die@protonmail.com>
2024-12-09 01:37:26 -0600

    Day 9 done!

--- 2024/day_09.py
+++ 2024/day_09.py
@@ -1,18 +1,62 @@
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2024, day=9)
-print(RAW)
+DISK = list(map(int, aoc_lube.fetch(year=2024, day=9)))
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+def get_memory():
+    used, free = [], []
+    pos = 0
+    for i, size in enumerate(DISK):
+        (free if i % 2 else used).append([pos, size, i // 2])
+        pos += size
+    return used, free
+
+
+def triangular_sum(pos, size):
+    return pos * size + (size * (size - 1) // 2)
+
+
+def checksum(used):
+    return sum(file_id * triangular_sum(pos, size) for pos, size, file_id in used)
+
 
 def part_one():
-    ...
+    used, free = get_memory()
+    fragged = []
+    for free_info in free:
+        file_info = used[-1]
+        if free_info[0] > file_info[0] + file_info[1]:
+            break
+        while free_info[1] >= file_info[1]:
+            file_info[0] = free_info[0]
+            free_info[0] += file_info[1]
+            free_info[1] -= file_info[1]
+            fragged.append(used.pop())
+            file_info = used[-1]
+
+        if free_info[1] > 0:
+            free_info[2] = file_info[2]
+            fragged.append(free_info[:])
+            file_info[1] -= free_info[1]
+            free_info[1] = 0
+
+    return checksum(used) + checksum(fragged)
+
 
 def part_two():
-    ...
+    used, free = get_memory()
+    for file_info in reversed(used):
+        for free_info in free:
+            if free_info[0] > file_info[0]:
+                break
+            if free_info[1] >= file_info[1]:
+                file_info[0] = free_info[0]
+                free_info[0] += file_info[1]
+                free_info[1] -= file_info[1]
+                break
+
+    return checksum(used)
+
 
 aoc_lube.submit(year=2024, day=9, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=9, part=2, solution=part_two)
