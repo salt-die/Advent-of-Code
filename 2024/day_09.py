c27bc524827f6a5e2210e792dd3a4880211499d5
salt-die <salt-die@protonmail.com>
2024-12-09 01:47:19 -0600

    Add separate function for moving file.

--- 2024/day_09.py
+++ 2024/day_09.py
@@ -20,26 +20,28 @@ def checksum(used):
     return sum(file_id * triangular_sum(pos, size) for pos, size, file_id in used)
 
 
+def move(free_info, file_info):
+    file_info[0] = free_info[0]
+    free_info[0] += file_info[1]
+    free_info[1] -= file_info[1]
+
+
 def part_one():
     used, free = get_memory()
+    files = reversed(used)
+    file_info = next(files)
     fragged = []
     for free_info in free:
-        file_info = used[-1]
         if free_info[0] > file_info[0]:
             break
         while free_info[1] >= file_info[1]:
-            file_info[0] = free_info[0]
-            free_info[0] += file_info[1]
-            free_info[1] -= file_info[1]
-            fragged.append(used.pop())
-            file_info = used[-1]
-
+            move(free_info, file_info)
+            file_info = next(files)
         if free_info[1] > 0:
             free_info[2] = file_info[2]
             fragged.append(free_info[:])
             file_info[1] -= free_info[1]
             free_info[1] = 0
-
     return checksum(used) + checksum(fragged)
 
 
@@ -50,14 +52,10 @@ def part_two():
             if free_info[0] > file_info[0]:
                 break
             if free_info[1] >= file_info[1]:
-                file_info[0] = free_info[0]
-                free_info[0] += file_info[1]
-                free_info[1] -= file_info[1]
+                move(free_info, file_info)
                 break
-
     return checksum(used)
 
 
-print(part_one(), part_two())
 aoc_lube.submit(year=2024, day=9, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=9, part=2, solution=part_two)
