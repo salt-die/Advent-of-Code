cc27aabcf5f6510adc6033544ad393e1e28fe85e
salt-die <salt-die@protonmail.com>
2024-12-09 23:29:46 -0600

    Day 10 done!

--- 2024/day_10.py
+++ 2024/day_10.py
@@ -1,18 +1,47 @@
 import aoc_lube
+import networkx as nx
+from aoc_lube.utils import grid_steps, ilen
 
-RAW = aoc_lube.fetch(year=2024, day=10)
-print(RAW)
 
 def parse_raw():
-    ...
+    grid = aoc_lube.fetch(year=2024, day=10).splitlines()
+    trailheads = []
+    summits = []
+    G = nx.DiGraph()
+    for y, line in enumerate(grid):
+        for x, char in enumerate(line):
+            if char == "0":
+                trailheads.append((y, x))
+            elif char == "9":
+                summits.append((y, x))
+            G.add_node((y, x), value=int(char))
 
-DATA = parse_raw()
+    nodes = G.nodes(data="value")
+    for a, b in grid_steps(4, len(grid), len(grid[0])):
+        if nodes[b] - nodes[a] == 1:
+            G.add_edge(a, b)
+    return G, trailheads, summits
+
+
+G, TRAILHEADS, SUMMITS = parse_raw()
+
 
 def part_one():
-    ...
+    total = 0
+    for a in TRAILHEADS:
+        for b in SUMMITS:
+            total += nx.has_path(G, a, b)
+    return total
+
 
 def part_two():
-    ...
+    total = 0
+    for a in TRAILHEADS:
+        for b in SUMMITS:
+            if nx.has_path(G, a, b):
+                total += ilen(nx.all_simple_edge_paths(G, a, b))
+    return total
+
 
 aoc_lube.submit(year=2024, day=10, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=10, part=2, solution=part_two)
