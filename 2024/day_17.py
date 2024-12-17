30beda53be20463d9c00ab7eb88b3898a536c9dc
salt-die <salt-die@protonmail.com>
2024-12-17 07:23:58 -0600

    Simplify

--- 2024/day_17.py
+++ 2024/day_17.py
@@ -9,7 +9,7 @@ def run_program(a):
     while a:
         b = a % 8 ^ 3
         out.append(b ^ 5 ^ (a >> b) % 8)
-        a >>= 3
+        a //= 8
     return out
 
 
@@ -17,20 +17,16 @@ def part_one():
     return ",".join(map(str, run_program(A)))
 
 
-def n(digits):
-    return sum(digit * 8 ** (len(digits) - i - 1) for i, digit in enumerate(digits))
-
-
 def part_two():
-    possible = [[]]
+    possible = [0]
     for i in range(len(CODE)):
         possible = [
-            digits + [a]
-            for digits in possible
+            n * 8 + a
+            for n in possible
             for a in range(8)
-            if CODE[-i - 1 :] == run_program(n(digits + [a]))
+            if CODE[-i - 1 :] == run_program(n * 8 + a)
         ]
-    return n(possible[0])
+    return possible[0]
 
 
 aoc_lube.submit(year=2024, day=17, part=1, solution=part_one)
