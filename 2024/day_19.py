3cc90480469d2a01411a0cd4d4098026e0c08294
salt-die <salt-die@protonmail.com>
2024-12-19 00:10:31 -0600

    Day 19 done!

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -1,18 +1,32 @@
+from functools import cache
+
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2024, day=19)
-print(RAW)
 
 def parse_raw():
-    ...
+    towels, designs = aoc_lube.fetch(year=2024, day=19).split("\n\n")
+    return towels.split(", "), designs.splitlines()
 
-DATA = parse_raw()
+
+TOWELS, DESIGNS = parse_raw()
+
+
+@cache
+def ndesigns(design):
+    return not design or sum(
+        ndesigns(design.removesuffix(towel))
+        for towel in TOWELS
+        if design.endswith(towel)
+    )
+
 
 def part_one():
-    ...
+    return sum(ndesigns(design) > 0 for design in DESIGNS)
+
 
 def part_two():
-    ...
+    return sum(ndesigns(design) for design in DESIGNS)
+
 
 aoc_lube.submit(year=2024, day=19, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=19, part=2, solution=part_two)
