c899f39b1d5376f93c5a2b2b6286eb52b0780551
salt-die <salt-die@protonmail.com>
2024-12-20 14:18:09 -0600

    Alternative solution.

--- 2024/day_20.py
+++ 2024/day_20.py
@@ -29,3 +29,34 @@ def part_two():
 
 aoc_lube.submit(year=2024, day=20, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=20, part=2, solution=part_two)
+
+# Alternative numpy solution:
+#
+# from itertools import count
+#
+# import aoc_lube
+# import numpy as np
+# from aoc_lube.utils import Vec2
+#
+# GRID = np.array([list(line) for line in aoc_lube.fetch(year=2024, day=20).splitlines()])
+# DISTANCES = np.zeros_like(GRID, int)
+# START = Vec2(*np.argwhere(GRID == "S").flatten().tolist())
+# DISTANCES[START] = 1
+#
+# u = START
+# for i in count(2):
+#     for adj in u.adj():
+#         if GRID[adj] != "#" and DISTANCES[adj] == 0:
+#             DISTANCES[adj] = i
+#             u = adj
+#             break
+#     else:
+#         break
+#
+#
+# def ncheats(cheat_duration):
+#     ys, xs = np.indices(GRID.shape, sparse=True)
+#     path_y, path_x = np.where(DISTANCES)
+#     distances = np.abs(path_y - ys[..., None]) + np.abs(path_x - xs[..., None])
+#     cheat_dist = DISTANCES[..., None] - DISTANCES[path_y, path_x] - distances
+#     return (cheat_dist[distances <= cheat_duration] >= 100).sum()
