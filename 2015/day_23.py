a9b7ba0d79fd8ecf4fc432b91341c59f43990eae
salt-die <salt-die@protonmail.com>
2024-12-20 06:47:39 -0600

    Day 23 done!

--- 2015/day_23.py
+++ 2015/day_23.py
@@ -1,19 +1,38 @@
 import aoc_lube
 
-RAW = aoc_lube.fetch(year=2015, day=23)
-print(RAW)
+INSTRUCTIONS = aoc_lube.fetch(year=2015, day=23).splitlines()
+print(INSTRUCTIONS)
 
 
-def parse_raw(): ...
+def compute(a):
+    registers = {"a": a, "b": 0}
+    ptr = 0
+    while ptr < len(INSTRUCTIONS):
+        match INSTRUCTIONS[ptr].split():
+            case "hlf", register:
+                registers[register] //= 2
+            case "tpl", register:
+                registers[register] *= 3
+            case "inc", register:
+                registers[register] += 1
+            case "jmp", amount:
+                ptr += int(amount) - 1
+            case "jie", register, amount:
+                if registers[register[0]] % 2 == 0:
+                    ptr += int(amount) - 1
+            case "jio", register, amount:
+                if registers[register[0]] == 1:
+                    ptr += int(amount) - 1
+        ptr += 1
+    return registers["b"]
 
 
-DATA = parse_raw()
+def part_one():
+    return compute(0)
 
 
-def part_one(): ...
-
-
-def part_two(): ...
+def part_two():
+    return compute(1)
 
 
 aoc_lube.submit(year=2015, day=23, part=1, solution=part_one)
