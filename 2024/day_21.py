25b75ec1a24e9696e39327599aadbaadf17732b7
salt-die <salt-die@protonmail.com>
2024-12-21 05:09:40 -0600

    Cache money

--- 2024/day_21.py
+++ 2024/day_21.py
@@ -2,35 +2,38 @@ from functools import cache
 from itertools import starmap
 
 import aoc_lube
-from aoc_lube.utils import Vec2, sliding_window
+from aoc_lube.utils import sliding_window
 
 CODES = aoc_lube.fetch(year=2024, day=21).splitlines()
 
 
 NUMPAD = {
-    key: Vec2(y, x)
+    key: (y, x)
     for y, row in enumerate(["789", "456", "123", "_0A", "<v>"])
     for x, key in enumerate(row)
 }
 NUMPAD["^"] = NUMPAD["0"]
-OO = NUMPAD["_"]
+HY, HX = NUMPAD["_"]
 
 
+@cache
 def paths(a, b):
-    u, v = NUMPAD[a], NUMPAD[b]
-    Δ = v - u
-    path = f"{'v' * Δ.y}{'^' * -Δ.y}{'>' * Δ.x}{'<' * -Δ.x}"
-    if u.x != OO.x or v.y != OO.y:
-        yield f"{path}A"
-    if all(Δ) and (u.y != OO.y or v.x != OO.x):
-        yield f"{path[::-1]}A"
+    (uy, ux), (vy, vx) = NUMPAD[a], NUMPAD[b]
+    dy, dx = vy - uy, vx - ux
+    path = f"{'v' * dy}{'^' * -dy}{'>' * dx}{'<' * -dx}"
+    paths = []
+    if ux != HX or vy != HY:
+        paths.append(f"{path}A")
+    if dy and dx and (uy != HY or vx != HX):
+        paths.append(f"{path[::-1]}A")
+    return paths
 
 
 @cache
 def nkeys(code, nrobots=2):
     all_paths = starmap(paths, sliding_window("A" + code))
     if nrobots == 0:
-        return sum(len(next(path)) for path in all_paths)
+        return sum(len(path[0]) for path in all_paths)
     return sum(min(nkeys(path, nrobots - 1) for path in paths) for paths in all_paths)
 
 
