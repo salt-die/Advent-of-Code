b9a8aa9f980ecb96ea5b9fe04d79080a301f9e80
salt-die <salt-die@protonmail.com>
2024-12-21 04:54:19 -0600

    Cleaning.

--- 2024/day_21.py
+++ 2024/day_21.py
@@ -1,4 +1,5 @@
 from functools import cache
+from itertools import starmap
 
 import aoc_lube
 from aoc_lube.utils import Vec2, sliding_window
@@ -6,34 +7,31 @@ from aoc_lube.utils import Vec2, sliding_window
 CODES = aoc_lube.fetch(year=2024, day=21).splitlines()
 
 
-def make_keys(rows):
-    return {key: Vec2(y, x) for y, row in enumerate(rows) for x, key in enumerate(row)}
+NUMPAD = {
+    key: Vec2(y, x)
+    for y, row in enumerate(["789", "456", "123", "_0A", "<v>"])
+    for x, key in enumerate(row)
+}
+NUMPAD["^"] = NUMPAD["0"]
+OO = NUMPAD["_"]
 
 
-ARROWS = make_keys(["h^A", "<v>"])
-NUMPAD = make_keys(["789", "456", "123", "h0A"])
-
-
-def paths(a, b, keys):
-    u, v, hole = keys[a], keys[b], keys["h"]
+def paths(a, b):
+    u, v = NUMPAD[a], NUMPAD[b]
     Δ = v - u
-    ys = f"{'v' * Δ.y}{'^' * -Δ.y}"
-    xs = f"{'>' * Δ.x}{'<' * -Δ.x}"
-    if u.x != hole.x or v.y != hole.y:
-        yield f"{ys}{xs}A"
-    if Δ.x != 0 != Δ.y and (u.y != hole.y or v.x != hole.x):
-        yield f"{xs}{ys}A"
+    path = f"{'v' * Δ.y}{'^' * -Δ.y}{'>' * Δ.x}{'<' * -Δ.x}"
+    if u.x != OO.x or v.y != OO.y:
+        yield f"{path}A"
+    if all(Δ) and (u.y != OO.y or v.x != OO.x):
+        yield f"{path[::-1]}A"
 
 
 @cache
-def nkeys(code, nrobots=2, robot=0):
-    keys = ARROWS if robot else NUMPAD
-    all_paths = (paths(a, b, keys) for a, b in sliding_window("A" + code))
-    if robot == nrobots:
+def nkeys(code, nrobots=2):
+    all_paths = starmap(paths, sliding_window("A" + code))
+    if nrobots == 0:
         return sum(len(next(path)) for path in all_paths)
-    return sum(
-        min(nkeys(path, nrobots, robot + 1) for path in paths) for paths in all_paths
-    )
+    return sum(min(nkeys(path, nrobots - 1) for path in paths) for paths in all_paths)
 
 
 def part_one():
