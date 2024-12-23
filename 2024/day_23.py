899251e256af673691222bf896024be1763da6e0
salt-die <salt-die@protonmail.com>
2024-12-23 06:30:52 -0600

    Day 23 done!

--- 2024/day_23.py
+++ 2024/day_23.py
@@ -1,18 +1,25 @@
 import aoc_lube
+import networkx as nx
 
-RAW = aoc_lube.fetch(year=2024, day=23)
-print(RAW)
+G = nx.Graph(
+    (line.split("-") for line in aoc_lube.fetch(year=2024, day=23).splitlines())
+)
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    total = 0
+    for clique in nx.enumerate_all_cliques(G):
+        if len(clique) < 3:
+            continue
+        elif len(clique) > 3:
+            return total
+
+        total += any(u.startswith("t") for u in clique)
+
 
 def part_two():
-    ...
+    return ",".join(sorted(nx.max_weight_clique(G, None)[0]))
+
 
 aoc_lube.submit(year=2024, day=23, part=1, solution=part_one)
 aoc_lube.submit(year=2024, day=23, part=2, solution=part_two)
