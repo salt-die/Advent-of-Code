839dbe5ad9e0a8d4fb102d25e9a1fdb37a28443b
salt-die <salt-die@protonmail.com>
2024-12-23 07:18:43 -0600

    Day 25 done!

--- 2015/day_25.py
+++ 2015/day_25.py
@@ -1,16 +1,18 @@
 import aoc_lube
+from aoc_lube.utils import extract_ints
 
-RAW = aoc_lube.fetch(year=2015, day=25)
-print(RAW)
-
-
-def parse_raw(): ...
-
-
-DATA = parse_raw()
+Y, X = extract_ints(aoc_lube.fetch(year=2015, day=25))
+INITIAL = 20151125
+EXP = 252533
+MODULUS = 33554393
 
 
-def part_one(): ...
+def nth(y, x):
+    return (y + x - 1) * (y + x - 2) // 2 + x - 1
+
+
+def part_one():
+    return INITIAL * pow(EXP, nth(Y, X), MODULUS) % MODULUS
 
 
 aoc_lube.submit(year=2015, day=25, part=1, solution=part_one)
