a0fd986b99d99c01c9233f7eddd0ed3e96e3ca71
salt-die <salt-die@protonmail.com>
2024-12-23 06:47:04 -0600

    Day 24 done!

--- 2015/day_24.py
+++ 2015/day_24.py
@@ -1,19 +1,26 @@
+from itertools import combinations
+from math import prod
+
 import aoc_lube
+from aoc_lube.utils import extract_ints
 
-RAW = aoc_lube.fetch(year=2015, day=24)
-print(RAW)
-
-
-def parse_raw(): ...
+PACKAGES = list(extract_ints(aoc_lube.fetch(year=2015, day=24)))
 
 
-DATA = parse_raw()
+def min_distribution(n):
+    target = sum(PACKAGES) // n
+    for i in range(1, len(PACKAGES)):
+        good = [prod(x) for x in combinations(PACKAGES, i) if sum(x) == target]
+        if good:
+            return min(good)
 
 
-def part_one(): ...
+def part_one():
+    return min_distribution(3)
 
 
-def part_two(): ...
+def part_two():
+    return min_distribution(4)
 
 
 aoc_lube.submit(year=2015, day=24, part=1, solution=part_one)
