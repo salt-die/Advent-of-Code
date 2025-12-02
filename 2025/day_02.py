4b8eea3b088ea7a837f2db811621bc7eb85617ae
salt-die <salt-die@protonmail.com>
2025-12-02 07:02:56 -0600

    Without using set

--- 2025/day_02.py
+++ 2025/day_02.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from itertools import combinations
+
 import aoc_lube
 from aoc_lube.utils import chunk, extract_ints
 
@@ -10,28 +13,6 @@ def is_valid(n: int):
     return s[:m] != s[m:]
 
 
-def invalids(a, b):
-    s = str(a)
-    if len(s) != len(str(b)):
-        yield from invalids(a, int("9" * len(s)))
-        yield from invalids(int("1" + "0" * len(s)), b)
-        return
-
-    seen = set()
-    for i in range(1, len(s) // 2 + 1):
-        if len(s) % i:
-            continue
-        initial = int(s[:i])
-        while True:
-            possible = int(str(initial) * (len(s) // i))
-            if possible > b:
-                break
-            initial += 1
-            if possible not in seen and possible >= a:
-                seen.add(possible)
-                yield possible
-
-
 def part_one():
     total_invalid = 0
     for a, b in DATA:
@@ -41,8 +22,45 @@ def part_one():
     return total_invalid
 
 
+def fold_div(it) -> int:
+    all_div = it[0]
+    for a in it[1:]:
+        if all_div % a:
+            all_div = 1
+        else:
+            all_div = a
+    return all_div
+
+
+def sum_of_invalids(a: int, b: int) -> int:
+    s = str(a)
+    if len(s) != len(str(b)):
+        m = int("9" * len(s))
+        return sum_of_invalids(a, m) + sum_of_invalids(m + 1, b)
+
+    seen = Counter()
+    for i in range(len(s) // 2, 0, -1):
+        if len(s) % i:
+            continue
+
+        initial = int(s[:i])
+        while True:
+            possible = int(str(initial) * (len(s) // i))
+            if possible > b:
+                break
+            if possible >= a:
+                seen[i] += possible
+            initial += 1
+
+    total = 0
+    for i in range(1, len(seen) + 1):
+        for d in combinations(seen, r=i):
+            total += seen[fold_div(d)] * (-1) ** (i + 1)
+    return total
+
+
 def part_two():
-    return sum(sum(invalids(a, b)) for a, b in DATA)
+    return sum(sum_of_invalids(a, b) for a, b in DATA)
 
 
 aoc_lube.submit(year=2025, day=2, part=1, solution=part_one)
