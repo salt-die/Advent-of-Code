e9df517d31ec54d2d764c9804ebab1428c5f5454
salt-die <salt-die@protonmail.com>
2025-12-06 00:26:34 -0600

    Day 6 done!

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -1,18 +1,33 @@
+from math import prod
+
 import aoc_lube
+import numpy as np
+from aoc_lube.utils import extract_ints, sliding_window
 
 RAW = aoc_lube.fetch(year=2025, day=6)
-print(RAW)
+*NUMBERS, _last = RAW.splitlines()
+OPERATORS = _last.split()
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    data = np.array([list(extract_ints(row)) for row in NUMBERS])
+    return sum(
+        np.prod(column) if op == "*" else column.sum()
+        for op, column in zip(OPERATORS, data.T)
+    )
+
+
+def solve(problem):
+    n = [int("".join(col)) for col in problem[:-1].T]
+    answer = sum(n) if problem[-1, 0] == "+" else prod(n)
+    return answer
+
 
 def part_two():
-    ...
+    data = np.array([list(row) for row in (RAW + "  ").splitlines()])
+    splits = [-1, *np.flatnonzero(np.all(data == " ", axis=0)).tolist(), None]
+    return sum(solve(data[:, a + 1 : b]) for a, b in sliding_window(splits, 2))
+
 
 aoc_lube.submit(year=2025, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2025, day=6, part=2, solution=part_two)
