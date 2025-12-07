bad99800abf6e4644f1d9b8b5f677411d4dee873
salt-die <salt-die@protonmail.com>
2025-12-06 23:33:07 -0600

    Day 7 done!

--- 2025/day_07.py
+++ 2025/day_07.py
@@ -1,18 +1,31 @@
+from collections import Counter
+
 import aoc_lube
 
 RAW = aoc_lube.fetch(year=2025, day=7)
-print(RAW)
+START = RAW.index("S")
+SPLITTERS = [i for row in RAW.splitlines() for i, char in enumerate(row) if char == "^"]
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    nsplits, beams = 0, {START}
+    for splitter in SPLITTERS:
+        if splitter in beams:
+            nsplits += 1
+            beams.remove(splitter)
+            beams.update((splitter - 1, splitter + 1))
+    return nsplits
+
 
 def part_two():
-    ...
+    beams = Counter({START: 1})
+    for splitter in SPLITTERS:
+        if splitter in beams:
+            count = beams.pop(splitter)
+            beams[splitter - 1] += count
+            beams[splitter + 1] += count
+    return beams.total()
+
 
 aoc_lube.submit(year=2025, day=7, part=1, solution=part_one)
 aoc_lube.submit(year=2025, day=7, part=2, solution=part_two)
