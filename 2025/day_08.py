0c92fa4dcfe3e164b65f8d806da0711eb71e5207
salt-die <salt-die@protonmail.com>
2025-12-07 23:19:55 -0600

    Day 8 done!

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -1,18 +1,29 @@
+from heapq import nlargest
+from itertools import combinations, count
+from math import dist, prod
+
 import aoc_lube
+from aoc_lube.utils import UnionFind, chunk, extract_ints
 
-RAW = aoc_lube.fetch(year=2025, day=8)
-print(RAW)
+RAW = list(chunk(extract_ints(aoc_lube.fetch(year=2025, day=8)), 3))
+UF = UnionFind(RAW)
+DISTANCES = sorted((dist(a, b), a, b) for a, b in combinations(RAW, 2))
 
-def parse_raw():
-    ...
-
-DATA = parse_raw()
 
 def part_one():
-    ...
+    for i in range(1000):
+        _, a, b = DISTANCES[i]
+        UF.merge(a, b)
+    return prod(nlargest(3, (len(component) for component in UF.components)))
+
 
 def part_two():
-    ...
+    for i in count():
+        _, a, b = DISTANCES[i]
+        UF.merge(a, b)
+        if len(UF.components) == 1:
+            return a[0] * b[0]
+
 
 aoc_lube.submit(year=2025, day=8, part=1, solution=part_one)
 aoc_lube.submit(year=2025, day=8, part=2, solution=part_two)
