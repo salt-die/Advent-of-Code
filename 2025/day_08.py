2e0946c1fdfcfba313a0d02131302c1c1e5ac1d8
salt-die <salt-die@protonmail.com>
2025-12-07 23:25:58 -0600

    Shorten

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -1,5 +1,5 @@
 from heapq import nlargest
-from itertools import combinations, count
+from itertools import combinations
 from math import dist, prod
 
 import aoc_lube
@@ -11,15 +11,13 @@ DISTANCES = sorted((dist(a, b), a, b) for a, b in combinations(RAW, 2))
 
 
 def part_one():
-    for i in range(1000):
-        _, a, b = DISTANCES[i]
+    for _, a, b in DISTANCES[:1000]:
         UF.merge(a, b)
     return prod(nlargest(3, (len(component) for component in UF.components)))
 
 
 def part_two():
-    for i in count():
-        _, a, b = DISTANCES[i]
+    for _, a, b in DISTANCES:
         UF.merge(a, b)
         if len(UF.components) == 1:
             return a[0] * b[0]
