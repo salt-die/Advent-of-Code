50e4bb0aa3b53ce80d36b6f0569429e2a2a672b5
salt-die <salt-die@protonmail.com>
2025-12-11 03:29:16 -0600

    Day 11 done!

--- 2025/day_11.py
+++ 2025/day_11.py
@@ -1,18 +1,33 @@
-import aoc_lube
+from functools import cache
 
-RAW = aoc_lube.fetch(year=2025, day=11)
-print(RAW)
+import aoc_lube
+import networkx as nx
+
 
 def parse_raw():
-    ...
+    raw = aoc_lube.fetch(year=2025, day=11)
+    lines = [line.partition(": ") for line in raw.splitlines()]
+    return nx.DiGraph({node: neighbors.split() for node, _, neighbors in lines})
 
-DATA = parse_raw()
+
+G = parse_raw()
+
 
 def part_one():
-    ...
+    return len(list(nx.all_simple_paths(G, "you", "out")))
+
+
+@cache
+def npaths(node, seen):
+    if node == "out":
+        return seen == 2
+    seen += node in {"fft", "dac"}
+    return sum(npaths(neighbor, seen) for neighbor in G[node])
+
 
 def part_two():
-    ...
+    return npaths("svr", 0)
+
 
 aoc_lube.submit(year=2025, day=11, part=1, solution=part_one)
 aoc_lube.submit(year=2025, day=11, part=2, solution=part_two)
